c788d4c309187838fe723024188b1b69bc4e15ef
steve.androulakis <steve.androulakis@174ef292-f9a9-40f3-a4db-b6760525a304>
2010-01-13 08:05:18 +0000

    Added basic comparison filters, which went missing for some reason
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/trunk@5 174ef292-f9a9-40f3-a4db-b6760525a304

---
+++ tardis_portal/templatetags/basiccomparisonfilters.py
@@ -0,0 +1,51 @@
+from django.template import Library
+from django import template
+
+def gt(value, arg):
+	"Returns a boolean of whether the value is greater than the \
+argument"
+	return value > int(arg)
+
+def lt(value, arg):
+	"Returns a boolean of whether the value is less than the argument"
+	return value < int(arg)
+
+def gte(value, arg):
+	"Returns a boolean of whether the value is greater than or equal to \
+the argument"
+	return value >= int(arg)
+
+def lte(value, arg):
+	"Returns a boolean of whether the value is less than or equal to \
+the argument"
+	return value <= int(arg)
+
+def length_gt(value, arg):
+	"Returns a boolean of whether the value's length is greater than \
+the argument"
+	return len(value) > int(arg)
+
+def length_lt(value, arg):
+	"Returns a boolean of whether the value's length is less than the \
+argument"
+	return len(value) < int(arg)
+
+def length_gte(value, arg):
+	"Returns a boolean of whether the value's length is greater than or \
+equal to the argument"
+	return len(value) >= int(arg)
+
+def length_lte(value, arg):
+	"Returns a boolean of whether the value's length is less than or \
+equal to the argument"
+	return len(value) <= int(arg)
+
+register = template.Library()
+register.filter('gt', gt)
+register.filter('lt', lt)
+register.filter('gte', gte)
+register.filter('lte', lte)
+register.filter('length_gt', length_gt)
+register.filter('length_lt', length_lt)
+register.filter('length_gte', length_gte)
+register.filter('length_lte', length_lte)
