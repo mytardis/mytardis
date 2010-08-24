76b7856bb7fd64dfe729031f66e27acb52c39f2b
Gerson Galang <gerson721@gmail.com>
2010-08-24 00:46:52 +0000

    applied PythonTidy on the following modules to make them PEP8 compliant
    git-svn-id: http://mytardis.googlecode.com/svn/trunk@159 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis_portal/templatetags/basiccomparisonfilters.py
+++ tardis_portal/templatetags/basiccomparisonfilters.py
@@ -1,44 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.template import Library
 from django import template
 
+
 def gt(value, arg):
-	"Returns a boolean of whether the value is greater than the \
-argument"
-	return value > int(arg)
+    """Returns a boolean of whether the value is greater than the argument."""
+
+    return value > int(arg)
+
 
 def lt(value, arg):
-	"Returns a boolean of whether the value is less than the argument"
-	return value < int(arg)
+    """Returns a boolean of whether the value is less than the argument."""
+
+    return value < int(arg)
+
 
 def gte(value, arg):
-	"Returns a boolean of whether the value is greater than or equal to \
-the argument"
-	return value >= int(arg)
+    """Returns a boolean of whether the value is greater than or equal to the argument."""
+
+    return value >= int(arg)
+
 
 def lte(value, arg):
-	"Returns a boolean of whether the value is less than or equal to \
-the argument"
-	return value <= int(arg)
+    """Returns a boolean of whether the value is less than or equal to the argument."""
+
+    return value <= int(arg)
+
 
 def length_gt(value, arg):
-	"Returns a boolean of whether the value's length is greater than \
-the argument"
-	return len(value) > int(arg)
+    """Returns a boolean of whether the value's length is greater than the argument."""
+
+    return len(value) > int(arg)
+
 
 def length_lt(value, arg):
-	"Returns a boolean of whether the value's length is less than the \
-argument"
-	return len(value) < int(arg)
+    """Returns a boolean of whether the value's length is less than the argument."""
+
+    return len(value) < int(arg)
+
 
 def length_gte(value, arg):
-	"Returns a boolean of whether the value's length is greater than or \
-equal to the argument"
-	return len(value) >= int(arg)
+    """Returns a boolean of whether the value's length is greater than or equal to the argument."""
+
+    return len(value) >= int(arg)
+
 
 def length_lte(value, arg):
-	"Returns a boolean of whether the value's length is less than or \
-equal to the argument"
-	return len(value) <= int(arg)
+    """Returns a boolean of whether the value's length is less than or equal to the argument."""
+
+    return len(value) <= int(arg)
+
 
 register = template.Library()
 register.filter('gt', gt)
--- tardis_portal/templatetags/experimentstats.py
+++ tardis_portal/templatetags/experimentstats.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django import template
 from tardis.tardis_portal.models import Dataset_File
 from django.db.models import Sum
 
 register = template.Library()
 
+
 @register.filter
 def experiment_file_count(value):
-	return Dataset_File.objects.filter(dataset__experiment__pk=value).count()
+    return Dataset_File.objects.filter(dataset__experiment__pk=value).count()
 
 # @register.filter
-# def experiment_file_size(value):	
-# 	return Dataset_File.objects.filter(dataset__experiment__pk=value).aggregate(Sum('size'))['size__sum']
+# def experiment_file_size(value):....
+# ....return Dataset_File.objects.filter(dataset__experiment__pk=value).aggregate(Sum('size'))['size__sum']
