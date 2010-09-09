c1db0eeab6cd7dfced3fce24103292265e30b66b
russell.sim <russell.sim@174ef292-f9a9-40f3-a4db-b6760525a304>
2010-09-09 03:24:26 +0000

    added some missing dependencies
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/trunk@194 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/test_settings.py
+++ tardis/test_settings.py
@@ -1,12 +1,18 @@
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = ':memory:'
-ROOT_URLCONF = 'tardis_portal.urls'
+ROOT_URLCONF = 'tardis.urls'
 DEBUG = True
 STATIC_DOC_ROOT = 'tardis_portal/site_media'
 ADMIN_MEDIA_STATIC_DOC_ROOT = ''
 HANDLEURL = ''
 SITE_ID = '1'
 TEMPLATE_DIRS = ['.','tardis_portal/']
+
+MIDDLEWARE_CLASSES = ('django.middleware.common.CommonMiddleware',
+                      'django.contrib.sessions.middleware.SessionMiddleware',
+                      'django.contrib.auth.middleware.AuthenticationMiddleware',
+                      'tardis.tardis_portal.minidetector.Middleware')
+
 INSTALLED_APPS = ['tardis.tardis_portal',
         'django.contrib.auth',
         'django.contrib.contenttypes',
@@ -15,5 +21,8 @@ INSTALLED_APPS = ['tardis.tardis_portal',
         'django.contrib.admin',
         'django.contrib.admindocs',
         'django_nose',
-        'tardis.tardis_portal.templatetags',]
+        'django_extensions',
+        'registration',
+        'tardis.tardis_portal.templatetags',
+        ]
 TEST_RUNNER = 'django_nose.run_tests'
