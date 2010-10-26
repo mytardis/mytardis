11eac59987f4753a8e381595e6cb69f3d7ac38d6
Ulrich Felzmann <ulrich.felzmann@gmail.com>
2010-10-26 05:48:16 +0000

    added date fields
    git-svn-id: http://mytardis.googlecode.com/svn/branches/mecat-sprint3-uli@327 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/download.py
+++ tardis/tardis_portal/download.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
-views.py
+download.py
 
 @author Steve Androulakis
 @author Ulrich Felzmann
 
 """
-
+from django.core.servers.basehttp import FileWrapper
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 
 from tardis.tardis_portal.models import *
-from tardis.tardis_portal.views import render_response_index, \
-    return_response_not_found, return_response_error, \
-    has_datafile_access, experiment_access_required
+from tardis.tardis_portal.views import return_response_not_found, \
+    return_response_error, has_datafile_access, experiment_access_required
 from tardis.tardis_portal.logger import logger
 
 from os.path import join
 
+import subprocess
 import urllib
 
 
@@ -40,7 +40,6 @@ def download_datafile(request, datafile_id):
                              datafile.url.partition('//')[2])
             try:
                 logger.debug(file_path)
-                from django.core.servers.basehttp import FileWrapper
                 wrapper = FileWrapper(file(file_path))
 
                 response = HttpResponse(wrapper,
@@ -66,21 +65,17 @@ def download_experiment(request, experiment_id):
     # Create the HttpResponse object with the appropriate headers.
     # TODO: handle no datafile, invalid filename, all http links
     # (tarfile count?)
-
-    from django.core.servers.basehttp import FileWrapper
-
     experiment = Experiment.objects.get(pk=experiment_id)
 
-    tar_command = 'tar -C ' + settings.FILE_STORE_PATH + ' -c ' + \
-        str(experiment.id) + '/'
-    logger.debug('TAR COMMAND: ' + tar_command)
+    cmd = 'tar -C %s -c %s/' % (settings.FILE_STORE_PATH,
+                                str(experiment.id))
+    #logger.debug('TAR COMMAND: ' + tar_command)
 
-    import subprocess
-
-    response = HttpResponse(FileWrapper(subprocess.Popen(tar_command,
+    response = HttpResponse(FileWrapper(subprocess.Popen(cmd,
                             stdout=subprocess.PIPE,
                             shell=True).stdout),
                             mimetype='application/x-tar')
+
     response['Content-Disposition'] = 'attachment; filename=experiment' \
         + str(experiment.id) + '-complete.tar'
 
@@ -94,59 +89,50 @@ def download_datafiles(request):
     # Create the HttpResponse object with the appropriate headers.
     # TODO: handle no datafile, invalid filename, all http links
     # (tarfile count?)
+    expid = request.POST['expid']
     protocols = []
+    fileString = ''
+    fileSize = 0
 
-    if 'datafile' in request.POST:
+    if 'datafile' or 'dataset' in request.POST:
 
-        if not len(request.POST.getlist('datafile')) == 0:
-            from django.utils.safestring import SafeUnicode
-            from django.core.servers.basehttp import FileWrapper
+        if (len(request.POST.getlist('datafile')) > 0 \
+                or len(request.POST.getlist('dataset'))) > 0:
 
-            fileString = ''
-            fileSize = 0
-            for dfid in request.POST.getlist('datafile'):
+            datasets = request.POST.getlist('dataset')
+            datafiles = request.POST.getlist('datafile')
+
+            for dsid in datasets:
+                for datafile in Dataset_File.objects.filter(dataset=dsid):
+                    if has_datafile_access(datafile.id, request.user):
+                        p = datafile.protocol
+                        if not p in protocols:
+                            protocols += [p]
+                        if datafile.url.startswith('file://'):
+                            absolute_filename = datafile.url.partition('//')[2]
+                            fileString += expid + '/' + absolute_filename + ' '
+                            fileSize += long(datafile.size)
+
+            for dfid in datafiles:
                 datafile = Dataset_File.objects.get(pk=dfid)
+                if datafile.dataset.id in datasets:
+                    continue
                 if has_datafile_access(dfid, request.user):
                     p = datafile.protocol
                     if not p in protocols:
                         protocols += [p]
                     if datafile.url.startswith('file://'):
                         absolute_filename = datafile.url.partition('//')[2]
-                        fileString + request.POST['expid'] \
-                            + '/' + absolute_filename + ' '
-                        fileSize = fileSize + long(datafile.size)
-            # FIXME
-            if len(protocols) > 1:
-                return_response_not_found(request)
-            # tarfile class doesn't work on large files being added and
-            # streamed on the fly, so going command-line-o
+                        fileString += expid + '/' + absolute_filename + ' '
+                        fileSize += long(datafile.size)
 
-            tar_command = 'tar -C ' + settings.FILE_STORE_PATH + ' -c ' \
-                + fileString
 
-            import shlex
-            import subprocess
-
-            response = \
-                HttpResponse(FileWrapper(subprocess.Popen(tar_command,
-                             stdout=subprocess.PIPE,
-                             shell=True).stdout),
-                             mimetype='application/x-tar')
-            response['Content-Disposition'] = \
-                'attachment; filename=experiment' + \
-                request.POST['expid'] + '.tar'
-            response['Content-Length'] = fileSize + 5120
-
-            return response
+        else:
+            return return_response_not_found(request)
 
     elif 'url' in request.POST:
 
         if not len(request.POST.getlist('url')) == 0:
-            from django.utils.safestring import SafeUnicode
-            from django.core.servers.basehttp import FileWrapper
-
-            fileString = ''
-            fileSize = 0
             for url in request.POST.getlist('url'):
                 datafile = \
                     Dataset_File.objects.get(url=urllib.unquote(url),
@@ -157,31 +143,41 @@ def download_datafiles(request):
                         protocols += [p]
                     if datafile.url.startswith('file://'):
                         absolute_filename = datafile.url.partition('//')[2]
-                        fileString = fileString + request.POST['expid'] \
-                            + '/' + absolute_filename + ' '
-                        fileSize = fileSize + long(datafile.size)
+                        fileString += expid + '/' + absolute_filename + ' '
+                        fileSize += long(datafile.size)
 
-            # FIXME
-            if len(protocols) > 1:
-                return_response_not_found(request)
-
-            # tarfile class doesn't work on large files being added and
-            # streamed on the fly, so going command-line-o
-            tar_command = 'tar -C ' + settings.FILE_STORE_PATH + ' -c ' + \
-                fileString
-
-            response = \
-                HttpResponse(FileWrapper(subprocess.Popen(tar_command,
-                             stdout=subprocess.PIPE,
-                             shell=True).stdout),
-                             mimetype='application/x-tar')
-            response['Content-Disposition'] = \
-                'attachment; filename=experiment' + request.POST['expid'] + \
-                '.tar'
-            response['Content-Length'] = fileSize + 5120
-
-            return response
         else:
             return return_response_not_found(request)
     else:
         return return_response_not_found(request)
+
+    # more than one download location?
+    if len(protocols) > 1:
+        response = HttpResponseNotFound()
+        response.write('<p>Different locations selected!</p>\n')
+        response.write('Please limit your selection and try again.\n')
+        return response
+
+    # redirect request if download protocol found
+    if protocols[0] != '':
+        from django.core.urlresolvers import resolve
+        view, args, kwargs = resolve('/%s%s' % (protocols[0],
+                                                request.path))
+        kwargs['request'] = request
+        return view(*args, **kwargs)
+
+    else:
+        # tarfile class doesn't work on large files being added and
+        # streamed on the fly, so going command-line-o
+        cmd = 'tar -C %s -c %s' % (settings.FILE_STORE_PATH,
+                                   fileString)
+
+        response = \
+            HttpResponse(FileWrapper(subprocess.Popen(cmd,
+                                                      stdout=subprocess.PIPE,
+                                                      shell=True).stdout),
+                         mimetype='application/x-tar')
+        response['Content-Disposition'] = \
+                'attachment; filename=experiment%s.tar' % expid
+        response['Content-Length'] = fileSize + 5120
+        return response
