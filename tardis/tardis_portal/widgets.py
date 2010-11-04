ab124379349fe436457697dd23af1570c8ada7ce
russell.sim <russell.sim@174ef292-f9a9-40f3-a4db-b6760525a304>
2010-11-04 03:27:13 +0000

    added read only printing of fields through label and span widgets
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/merc-sprint1@375 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/tardis_portal/widgets.py
+++ tardis/tardis_portal/widgets.py
@@ -1,4 +1,6 @@
-from django.forms.widgets import TextInput
+from django.utils.safestring import mark_safe
+from django.forms.util import flatatt
+from django.forms.widgets import TextInput, Widget
 
 
 class CommaSeparatedInput(TextInput):
@@ -12,3 +14,20 @@ class CommaSeparatedInput(TextInput):
                                                                      files,
                                                                      name)
         return [v.strip(' ') for v in value.split(',')]
+
+
+class Label(Widget):
+    tag = "label"
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, name=name)
+        return mark_safe(u'<%(tag)s%(attrs)s>%(value)s</%(tag)s>' %
+                         {'attrs': flatatt(final_attrs),
+                          'value': value,
+                          'tag': self.tag})
+
+
+class Span(Label):
+    tag = "span"
