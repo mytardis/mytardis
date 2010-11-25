f96c863a0fa7df5a4c1167c26700485df9f95859
Russell Sim <russell.sim@gmail.com>
2010-11-25 03:27:31 +0000

    initial authclasses
    git-svn-id: http://mytardis.googlecode.com/svn/branches/authorisation-redesign@488 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

---
+++ tardis/tardis_portal/auth/interfaces.py
@@ -0,0 +1,58 @@
+class AuthProvider:
+    def authenticate(self, request):
+        """
+        from a request authenticate try to authenticate the user.
+        return a user dict if successful.
+        """
+        raise NotImplemented()
+
+
+class UserProvider:
+    def getUserById(self, id):
+        """
+        return the user dictionary in the format of::
+
+            {"id": 123,
+            "display": "John Smith",
+            "email": "john@example.com"}
+
+        """
+        raise NotImplemented()
+
+    def searchUsers(self, request):
+        """
+        return a list of user descriptions from the auth domain.
+
+        each user is in the format of::
+
+            {"id": 123,
+            "display": "John Smith",
+            "email": "john@example.com"}
+
+        """
+        raise NotImplemented()
+
+
+class GroupProvider:
+    def getGroups(self, request):
+        """
+        return an interation of the available groups.
+        """
+        raise NotImplemented()
+
+    def getGroupById(self, id):
+        """
+        return the group associated with the id
+        """
+
+    def searchGroups(self, filter):
+        """
+        return a list of groups that match the filter
+        """
+        raise NotImplemented()
+
+    def getGroupsForEntity(self, id):
+        """
+        return a list of groups associated with a particular entity id
+        """
+        raise NotImplemented()
