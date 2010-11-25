841e99d37a79560dcf7e26f6c21ff16a82b90d80
russell.sim <russell.sim@174ef292-f9a9-40f3-a4db-b6760525a304>
2010-11-25 03:27:31 +0000

    initial authclasses
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/authorisation-redesign@488 174ef292-f9a9-40f3-a4db-b6760525a304

---
+++ tardis/tardis_portal/auth/__init__.py
@@ -0,0 +1,45 @@
+from tardis.tardis_portal.auth.interfaces import AuthProvider, UserProvider, GroupProvider
+
+
+class AuthService:
+    def authenticate(self, request):
+        """
+        Try and authenticate the user first using Django auth backends,
+        then try each AuthProvider.
+        """
+        pass
+
+    def getGroups(self, request):
+        """
+        return a list of tuples containing pluginname and group id
+        """
+        pass
+
+    def searchEntities(self, filter):
+        """
+        return a list of users and/or groups
+        """
+        pass
+
+    def searchUsers(self, filter):
+        """
+        return a list of users and/or groups
+        """
+        pass
+
+    def searchGroups(self, filter):
+        """
+        return a list of users and/or groups
+        """
+        pass
+
+    def getUser(self, user_dict):
+        """
+        return a user model based on the user dict.
+
+        This function is responsible for creating the
+        user within the Django DB and returning the resulting
+        user model.
+        """
+        pass
+
