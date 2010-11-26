195243fc65eadc03b0630c53087e314dbca617c0
Russell Sim <russell.sim@gmail.com>
2010-11-26 03:03:50 +0000

    download functions now support filenames with spaces
    git-svn-id: http://mytardis.googlecode.com/svn/branches/merc-sprint1@500 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/download.py
+++ tardis/tardis_portal/download.py
@@ -46,7 +46,7 @@ def download_datafile(request, datafile_id):
                 response = HttpResponse(wrapper,
                         mimetype='application/octet-stream')
                 response['Content-Disposition'] = \
-                    'attachment; filename=' + datafile.filename
+                    'attachment; filename="' + datafile.filename + '"'
 
                 # import os
                 # response['Content-Length'] = os.path.getsize(file_path)
