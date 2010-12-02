6546bb88063af118763f1cd7400f737ed4936c24
russell.sim <russell.sim@174ef292-f9a9-40f3-a4db-b6760525a304>
2010-12-02 00:25:11 +0000

    added authservice module loading
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/authorisation-redesign@524 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/tardis_portal/auth/__init__.py
+++ tardis/tardis_portal/auth/__init__.py
@@ -1,7 +1,40 @@
+from django.conf import settings
+from django.utils.importlib import import_module
+from django.core.exceptions import ImproperlyConfigured
+
 from tardis.tardis_portal.auth.interfaces import AuthProvider, UserProvider, GroupProvider
 
 
 class AuthService:
+    def __init__(self, settings=settings):
+        self._group_providers = []
+        self._user_providers = []
+
+        for gp in settings.GROUP_PROVIDERS:
+            self._group_providers.append(self._safe_import(gp))
+        for up in settings.USER_PROVIDERS:
+            self._user_providers.append(self._safe_import(up))
+
+    def _safe_import(self, path):
+        try:
+            dot = path.rindex('.')
+        except ValueError:
+            raise ImproperlyConfigured('%s isn\'t a middleware module' % path)
+        auth_module, auth_classname = path[:dot], path[dot + 1:]
+        try:
+            mod = import_module(auth_module)
+        except ImportError, e:
+            raise ImproperlyConfigured('Error importing auth module %s: "%s"' %
+                                       (auth_module, e))
+        try:
+            auth_class = getattr(mod, auth_classname)
+        except AttributeError:
+            raise ImproperlyConfigured('Auth module "%s" does not define a "%s" class' %
+                                       (auth_module, auth_classname))
+
+        auth_instance = auth_class()
+        return auth_instance
+
     def authenticate(self, request):
         """
         Try and authenticate the user first using Django auth backends,
