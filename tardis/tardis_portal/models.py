5a2a8374b2a86f90e52163df32eede3b54c53796
Ulrich Felzmann <ulrich.felzmann@gmail.com>
2011-01-25 01:03:52 +0000

    fixed broken download link in search_datafile_results.html
    git-svn-id: http://mytardis.googlecode.com/svn/branches/authorisation-redesign@678 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/models.py
+++ tardis/tardis_portal/models.py
@@ -216,7 +216,7 @@ class Dataset_File(models.Model):
 
     dataset = models.ForeignKey(Dataset)
     filename = models.CharField(max_length=400)
-    url = models.URLField(max_length=400)
+    url = models.CharField(max_length=400)
     size = models.CharField(blank=True, max_length=400)
     protocol = models.CharField(blank=True, max_length=10)
     created_time = models.DateTimeField(null=True, blank=True)
