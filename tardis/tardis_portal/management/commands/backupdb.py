e2fcd5b8374cdbcf24829e13348eb3f494e0fb45
ulrich.felzmann@gmail.com <ulrich.felzmann@gmail.com@174ef292-f9a9-40f3-a4db-b6760525a304>
2011-02-09 04:36:39 +0000

    added restore functionality to backupdb script for postgresql and mysql
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/SouthIntegration@809 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/tardis_portal/management/commands/backupdb.py
+++ tardis/tardis_portal/management/commands/backupdb.py
@@ -1,16 +1,22 @@
 """
  Command for backup database
 """
-from os import makedirs, system
+from os import makedirs
 from os.path import exists, join
-import popen2
+import subprocess
 import time
 
 from django.core.management.base import BaseCommand
+from optparse import make_option
 
 
 class Command(BaseCommand):
     help = "Backup database. Only Mysql and Postgresql engines are implemented"
+
+    option_list = BaseCommand.option_list + (
+        make_option('-r', '--restore', default=False,
+            help='Restore database instead of dumping it'),
+        )
 
     def handle(self, *args, **options):
         from django.conf import settings
@@ -22,6 +28,13 @@ class Command(BaseCommand):
         self.host = settings.DATABASE_HOST
         self.port = settings.DATABASE_PORT
 
+        infile = options.get('restore')
+        if infile:
+            self.restore(infile)
+        else:
+            self.backup()
+
+    def backup(self):
         backup_dir = 'backups'
         if not exists(backup_dir):
             makedirs(backup_dir)
@@ -38,6 +51,18 @@ class Command(BaseCommand):
         else:
             print 'Backup in %s engine not implemented' % self.engine
 
+    def restore(self, infile):
+        if self.engine == 'mysql':
+            print 'Doing Mysql restore to database %s from %s'\
+                % (self.db, infile)
+            self.do_mysql_restore(infile)
+        elif self.engine in ('postgresql_psycopg2', 'postgresql'):
+            print 'Doing Postgresql restore to database %s from %s'\
+                % (self.db, infile)
+            self.do_postgresql_restore(infile)
+        else:
+            print 'Restore in %s engine not implemented' % self.engine
+
     def do_mysql_backup(self, outfile):
         args = []
         if self.user:
@@ -50,7 +75,25 @@ class Command(BaseCommand):
             args += ["--port=%s" % self.port]
         args += [self.db]
 
-        system('mysqldump %s > %s' % (' '.join(args), outfile))
+        subprocess.call('mysqldump %s' % ' '.join(args),
+                        stdout=open(outfile, 'w'))
+
+    def do_mysql_restore(self, infile):
+        args = []
+        if self.user:
+            args += ["--user=%s" % self.user]
+        if self.passwd:
+            args += ["--password=%s" % self.passwd]
+        if self.host:
+            args += ["--host=%s" % self.host]
+        if self.port:
+            args += ["--port=%s" % self.port]
+        args += [self.db]
+
+        subprocess.call('mysql - %s' % ' '.join(args),
+                        stdin=open(infile),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
 
     def do_postgresql_backup(self, outfile):
         args = []
@@ -64,7 +107,44 @@ class Command(BaseCommand):
             args += ["--port=%s" % self.port]
         if self.db:
             args += [self.db]
-        pipe = popen2.Popen4('pg_dump %s > %s' % (' '.join(args), outfile))
+
+        pipe = subprocess.Popen('pg_dump %s ' % ' '.join(args),
+                                shell=True,
+                                stdout=open(outfile, 'w'),
+                                stderr=subprocess.PIPE,
+                                stdin=subprocess.PIPE)
         if self.passwd:
-            pipe.tochild.write('%s\n' % self.passwd)
-            pipe.tochild.close()
+            stdout, stderr = pipe.communicate('%s\n' % self.passwd)
+        else:
+            stdout, stderr = pipe.communicate()
+        if stdout:
+            print stdout
+        if stderr:
+            print stderr
+
+    def do_postgresql_restore(self, infile):
+        args = []
+        if self.user:
+            args += ["--username=%s" % self.user]
+        if self.passwd:
+            args += ["--password"]
+        if self.host:
+            args += ["--host=%s" % self.host]
+        if self.port:
+            args += ["--port=%s" % self.port]
+        if self.db:
+            args += ["--dbname=%s" % self.db]
+
+        pipe = subprocess.Popen('psql %s -f %s' % (' '.join(args), infile),
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                stdin=subprocess.PIPE)
+        if self.passwd:
+            stdout, stderr = pipe.communicate('%s\n' % self.passwd)
+        else:
+            stdout, stderr = pipe.communicate()
+        if stdout:
+            print stdout
+        if stderr:
+            print stderr
