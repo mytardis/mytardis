4e5bda224901728f699630d212736c98eba51012
Russell Sim <russell.sim@gmail.com>
2011-03-02 04:14:57 +0000

    fixed the inconsistencies with Author_Experiment model
    git-svn-id: http://mytardis.googlecode.com/svn/trunk@904 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/models.py
+++ tardis/tardis_portal/models.py
@@ -231,9 +231,6 @@ class Author_Experiment(models.Model):
     author = models.CharField(max_length=255)
     order = models.PositiveIntegerField()
 
-    class Meta:
-        ordering = ('order', )
-
     def __unicode__(self):
         return SafeUnicode(self.author) + ' | ' \
             + SafeUnicode(self.experiment.id) + ' | ' \
