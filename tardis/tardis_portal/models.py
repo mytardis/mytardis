6242df446989bf1fda57b04a271e728775a72955
ulrich.felzmann@gmail.com <ulrich.felzmann@gmail.com@174ef292-f9a9-40f3-a4db-b6760525a304>
2011-03-03 06:57:21 +0000

    removed equipment model from tardis_portal
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/ParameterExtension@911 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/tardis_portal/models.py
+++ tardis/tardis_portal/models.py
@@ -497,7 +497,6 @@ class ParameterName(models.Model):
     #       represented in the DB. doing it this way is just a bit wasteful.
     choices = models.CharField(max_length=500, blank=True)
 
-
     def __unicode__(self):
         return self.name
 
@@ -644,21 +643,3 @@ class XML_data(models.Model):
 
     def __unicode__(self):
         return self.xmlns
-
-
-class Equipment(models.Model):
-    key = models.CharField(unique=True, max_length=30)
-    dataset = models.ManyToManyField(Dataset, null=True, blank=True)
-    description = models.TextField(blank=True)
-    make = models.CharField(max_length=60, blank=True)
-    model = models.CharField(max_length=60, blank=True)
-    type = models.CharField(max_length=60, blank=True)
-    serial = models.CharField(max_length=60, blank=True)
-    comm = models.DateField(null=True, blank=True)
-    decomm = models.DateField(null=True, blank=True)
-    url = models.URLField(null=True, blank=True,
-                          verify_exists=False,
-                          max_length=255)
-
-    def __unicode__(self):
-        return self.key
