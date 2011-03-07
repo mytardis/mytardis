8e35f9ca2e0ad1d45839ae65083320696c65e721
Russell Sim <russell.sim@gmail.com>
2011-03-07 22:07:36 +0000

    added staging file test
    git-svn-id: http://mytardis.googlecode.com/svn/branches/metadata-extraction@958 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/tests/test_staging.py
+++ tardis/tardis_portal/tests/test_staging.py
@@ -161,3 +161,65 @@ class TestPathResolution(TestCase):
             sp = staging.calculate_relative_path('tardis',
                                                ap)
             self.assertEqual(sp, p)
+
+
+class TestStagingFiles(TestCase):
+    def setUp(self):
+        from tardis.tardis_portal import models
+        from tardis.tardis_portal.staging import calculate_relative_path
+        from tempfile import mkdtemp, mktemp
+        from django.conf import settings
+        from os import path
+
+        # Disconnect post_save signal
+        from django.db.models.signals import post_save
+        from tardis.tardis_portal.models import staging_hook, Dataset_File
+        post_save.disconnect(staging_hook, sender=Dataset_File)
+
+        from django.contrib.auth.models import User
+        user = 'tardis_user1'
+        pwd = 'secret'
+        email = ''
+        self.user = User.objects.create_user(user, email, pwd)
+
+        self.temp = mkdtemp(dir=settings.STAGING_PATH)
+
+        self.file = mktemp(dir=self.temp)
+        f = open(self.file, "w+b")
+        f.write('test file')
+        f.close()
+
+        # make datafile
+        exp = models.Experiment(title='test exp1',
+                                institution_name='monash',
+                                created_by=self.user,
+                                )
+        exp.save()
+
+        # make dataset
+        dataset = models.Dataset(description="dataset description...",
+                                 experiment=exp)
+        dataset.save()
+
+        # create datasetfile
+        df = models.Dataset_File()
+        df.dataset = dataset
+        df.filename = path.basename(self.file)
+        df.url = self.file
+        df.protocol = "staging"
+        df.save()
+        self.df = df
+
+    def tearDown(self):
+        # reconnect post_save signal
+        from django.db.models.signals import post_save
+        from tardis.tardis_portal.models import staging_hook, Dataset_File
+        post_save.connect(staging_hook, sender=Dataset_File)
+
+        from shutil import rmtree
+        rmtree(self.temp)
+
+    def test_stage_file(self):
+        from tardis.tardis_portal import staging
+
+        staging.stage_file(self.df)
