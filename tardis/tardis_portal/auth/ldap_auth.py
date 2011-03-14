3ef59c7f7a553833f98efdd81d95be6680cb2dce
Gerson Galang <gerson721@gmail.com>
2011-03-14 23:58:57 +0000

    refactored ldap auth
    git-svn-id: http://mytardis.googlecode.com/svn/branches/ldaap-auth-changes@1001 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/auth/ldap_auth.py
+++ tardis/tardis_portal/auth/ldap_auth.py
@@ -31,9 +31,9 @@
 import ldap
 
 from django.conf import settings
-from django.contrib.auth.models import User
 
-from tardis.tardis_portal.models import UserProfile, UserAuthentication
+from tardis.tardis_portal.auth.utils import get_or_create_user_with_username
+from tardis.tardis_portal.logger import logger
 
 
 auth_key = u'ldap'
@@ -56,8 +56,7 @@ def get_ldap_username_for_email(email):
         l.protocol_version = ldap.VERSION3
         result = l.search_s(settings.BASE_DN, searchScope,
                             searchFilter, retrieveAttributes)
-        l.unbind_s()
-        return result[0][1]['uid'][0]
+        return result[0][1][settings.LDAP_USER_RDN][0]
 
     except ldap.LDAPError:
         return ''
@@ -82,7 +81,7 @@ def get_ldap_email_for_user(username):
         # retrieve all attributes - again adjust to your needs
         # see documentation for more options
         retrieveAttributes = ['mail']
-        Searchfilter = 'uid=' + Username
+        searchFilter = settings.LDAP_USER_RDN + '=' + username
 
         l.protocol_version = ldap.VERSION3
 
@@ -93,40 +92,10 @@ def get_ldap_email_for_user(username):
     except ldap.LDAPError:
         return ''
     except IndexError, i:
-
         return ''
     finally:
-
         if l:
             l.unbind_s()
-
-
-def get_or_create_user(email):
-    username = get_ldap_username_for_email(email)
-    return _get_or_create_user_with_username(username)
-
-
-def _get_or_create_user_with_username(username):
-    user = None
-    try:
-        user = UserAuthentication.objects.get(username=username,
-            authenticationMethod=auth_key).userProfile.user
-    except UserAuthentication.DoesNotExist:
-        # else, create a new user with a random password
-        name = 'ldap_%s' % username[0:25]
-        user = User(username=name,
-            password=User.objects.make_random_password(),
-            email=username)
-        user.is_staff = True
-        user.save()
-
-        userProfile = UserProfile(authcate_user=True, user=user)
-        userProfile.save()
-
-        userAuth = UserAuthentication(userProfile=userProfile,
-            username=username, authenticationMethod=auth_key)
-        userAuth.save()
-    return user
 
 
 class LdapBackend():
@@ -138,35 +107,35 @@ class LdapBackend():
             return None
 
         l = None
-        l_bind = None
+
         try:
-            l = ldap.open(settings.LDAP_URL)
             searchScope = ldap.SCOPE_SUBTREE
-            # retrieve all attributes - again adjust to your needs
-            # see documentation for more options
             retrieveAttributes = None
-            searchFilter = 'uid=' + username
+            userRDN = settings.LDAP_USER_RDN + '=' + username
 
+            l = ldap.initialize(settings.LDAP_URL)
             l.protocol_version = ldap.VERSION3
-            result = l.search_s(settings.BASE_DN, searchScope,
-                                searchFilter, retrieveAttributes)
-            DN = result[0][0]
-            l_bind = ldap.open('directory.monash.edu.au')
-            l_bind.simple_bind_s(DN, password)
+            l.simple_bind(userRDN+','+settings.BASE_DN, password)
+            ldap_result_id = l.search(settings.BASE_DN, searchScope, 
+                                      userRDN, retrieveAttributes)
 
-            # check if the given username in combination with the VBL
-            # auth method is already in the UserAuthentication table
-            return _get_or_create_user_with_username(username)
+            result_type, result_data = l.result(ldap_result_id, 0)
+
+            if result_data[0][1][settings.LDAP_USER_RDN][0]==username:
+                # check if the given username in combination with the LDAP
+                # auth method is already in the UserAuthentication table
+                return get_or_create_user_with_username(request, username, auth_key)
+            return None           
 
         except ldap.LDAPError:
+            logger.error("ldap error")
             return None
         except IndexError:
+            logger.error("index error")
             return None
         finally:
             if l:
                 l.unbind_s()
-            if l_bind:
-                l_bind.unbind_s()
 
     def get_user(self, user_id):
         raise NotImplemented()
