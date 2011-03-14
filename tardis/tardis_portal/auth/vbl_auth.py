9dcf9bdd5cfa50cf24cf6bb6cbb7fee5a5542438
gerson721 <gerson721@174ef292-f9a9-40f3-a4db-b6760525a304>
2011-03-14 23:58:57 +0000

    refactored ldap auth
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/ldaap-auth-changes@1001 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/tardis_portal/auth/vbl_auth.py
+++ tardis/tardis_portal/auth/vbl_auth.py
@@ -35,12 +35,11 @@ Created on 10/12/2010
 .. moduleauthor:: Gerson Galang <gerson.galang@versi.edu.au>
 '''
 
-
-from django.contrib.auth.models import User, AnonymousUser
 from django.conf import settings
 
+from tardis.tardis_portal.auth.utils import get_or_create_user_with_username
 from tardis.tardis_portal.auth.interfaces import GroupProvider
-from tardis.tardis_portal.models import UserAuthentication, UserProfile
+from tardis.tardis_portal.models import UserAuthentication
 
 from suds.client import Client
 
@@ -173,46 +172,7 @@ class Backend():
         else:
             request.session[SOAPLoginKey] = result
 
-        isDjangoAccount = True
-
-        try:
-            # check if the given username in combination with the VBL
-            # auth method is already in the UserAuthentication table
-            user = UserAuthentication.objects.get(username=username,
-                authenticationMethod=auth_key).userProfile.user
-
-        except UserAuthentication.DoesNotExist:
-            # if request.user is not null, then we can assume that we are only
-            # calling this function to verify if the provided username and
-            # password will authenticate with this backend
-            if type(request.user) is not AnonymousUser:
-                user = request.user
-
-            # else, create a new user with a random password
-            else:
-                isDjangoAccount = False
-                name = username.partition('@')[0]
-                # length of the maximum username and the separator `_`
-                max_length = 31 - len(name)
-                name = '%s_%s' % (auth_key, name[0:max_length])
-                password = User.objects.make_random_password()
-                user = User.objects.create_user(username=name,
-                                                password=password,
-                                                email=username)
-                user.save()
-
-            try:
-                # we'll also try and check if the user already has an
-                # existing userProfile attached to his/her account
-                userProfile = UserProfile.objects.get(user=user)
-            except UserProfile.DoesNotExist:
-                userProfile = UserProfile(user=user,
-                    isDjangoAccount=isDjangoAccount)
-                userProfile.save()
-
-            userAuth = UserAuthentication(userProfile=userProfile,
-                username=username, authenticationMethod=auth_key)
-            userAuth.save()
+        user = get_or_create_user_with_username(request, username, auth_key)
 
         # result contains comma separated list of epns
         request.session[EPN_LIST] = \
