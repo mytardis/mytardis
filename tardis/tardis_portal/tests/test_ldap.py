7ebf0cac66be508b794ee5f87a768175931c976a
russell.sim@gmail.com <russell.sim@gmail.com@174ef292-f9a9-40f3-a4db-b6760525a304>
2011-03-24 23:44:11 +0000

    added basic query test for group information
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/branches/ldaap-auth-changes@1084 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/tardis_portal/tests/test_ldap.py
+++ tardis/tardis_portal/tests/test_ldap.py
@@ -48,10 +48,21 @@ class LDAPTest(TestCase):
                  {'cn': ['Test User3']})]
         self.assertEqual(res, res1)
 
+        res = l._query(settings.LDAP_GROUP_BASE, '(objectClass=*)', ['cn'])
+        res1 = [('ou=Group,dc=example,dc=com', {}),
+                ('cn=empty,ou=Group,dc=example,dc=com',
+                 {'cn': ['empty']}),
+                ('cn=full,ou=Group,dc=example,dc=com',
+                 {'cn': ['full']}),
+                ('cn=systems,ou=Group,dc=example,dc=com',
+                 {'cn': ['systems']})]
+        self.assertEqual(res, res1)
+
     def test_authenticate(self):
         from tardis.tardis_portal.auth.ldap_auth import ldap_auth
         from django.core.handlers.wsgi import WSGIRequest
 
+        # Tests Authenticate API
         l = ldap_auth()
         req = WSGIRequest({"REQUEST_METHOD": "POST"})
         req._post = {'username': 'testuser1',
