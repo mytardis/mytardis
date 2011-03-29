12ce9c3ffeab3112cba38e8b66bf90ff345cc262
Russell Sim <russell.sim@gmail.com>
2011-03-29 01:45:10 +0000

    updated to do better ldap detection and properly skip the tests
    git-svn-id: http://mytardis.googlecode.com/svn/branches/ldaap-auth-changes@1125 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/tests/test_ldap.py
+++ tardis/tardis_portal/tests/test_ldap.py
@@ -7,25 +7,6 @@ from nose.plugins.skip import SkipTest
 server = None
 
 
-def which(program):
-    import os
-
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
-
 class LDAPErrorTest(TestCase):
 
     def test_search(self):
@@ -39,7 +20,7 @@ class LDAPTest(TestCase):
         from tardis.tardis_portal.tests.ldap_ldif import test_ldif
         import slapd
         global server
-        if not which('slapd'):
+        if not slapd.Slapd.check_paths():
             raise SkipTest()
 
         server = slapd.Slapd()
