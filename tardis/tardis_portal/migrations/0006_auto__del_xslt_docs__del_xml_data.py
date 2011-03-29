07821dd9f0de37a3fea0fa89076ecc6db382e4da
Grischa Meyer <grischa@gmail.com>
2011-03-29 06:09:17 +0000

    merged with latest trunk
    git-svn-id: http://mytardis.googlecode.com/svn/branches/parameteredit@1132 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

---
+++ tardis/tardis_portal/migrations/0006_auto__del_xslt_docs__del_xml_data.py
@@ -0,0 +1,212 @@
+# encoding: utf-8
+from south.db import db
+from south.v2 import SchemaMigration
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Deleting model 'XSLT_docs'
+        db.delete_table('tardis_portal_xslt_docs')
+
+        # Deleting model 'XML_data'
+        db.delete_table('tardis_portal_xml_data')
+
+    def backwards(self, orm):
+
+        # Adding model 'XSLT_docs'
+        db.create_table('tardis_portal_xslt_docs', (
+            ('data', self.gf('django.db.models.fields.TextField')()),
+            ('xmlns', self.gf('django.db.models.fields.URLField')(max_length=255, primary_key=True)),
+        ))
+        db.send_create_signal('tardis_portal', ['XSLT_docs'])
+
+        # Adding model 'XML_data'
+        db.create_table('tardis_portal_xml_data', (
+            ('datafile', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['tardis_portal.Dataset_File'], unique=True, null=True, blank=True)),
+            ('xmlns', self.gf('django.db.models.fields.URLField')(max_length=400)),
+            ('experiment', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['tardis_portal.Experiment'], unique=True, null=True, blank=True)),
+            ('dataset', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['tardis_portal.Dataset'], unique=True, null=True, blank=True)),
+            ('data', self.gf('django.db.models.fields.TextField')()),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal('tardis_portal', ['XML_data'])
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'tardis_portal.author_experiment': {
+            'Meta': {'ordering': "['order']", 'unique_together': "(('experiment', 'author'),)", 'object_name': 'Author_Experiment'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'experiment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Experiment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'tardis_portal.datafileparameter': {
+            'Meta': {'ordering': "['id']", 'object_name': 'DatafileParameter'},
+            'datetime_value': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.ParameterName']"}),
+            'numerical_value': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'parameterset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.DatafileParameterSet']"}),
+            'string_value': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'tardis_portal.datafileparameterset': {
+            'Meta': {'ordering': "['id']", 'object_name': 'DatafileParameterSet'},
+            'dataset_file': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Dataset_File']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'schema': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Schema']"})
+        },
+        'tardis_portal.dataset': {
+            'Meta': {'object_name': 'Dataset'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'experiment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Experiment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'tardis_portal.dataset_file': {
+            'Meta': {'object_name': 'Dataset_File'},
+            'created_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'dataset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Dataset']"}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '400'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'md5sum': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'mimetype': ('django.db.models.fields.CharField', [], {'max_length': '80', 'blank': 'True'}),
+            'modification_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'protocol': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'size': ('django.db.models.fields.CharField', [], {'max_length': '400', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '400'})
+        },
+        'tardis_portal.datasetparameter': {
+            'Meta': {'ordering': "['id']", 'object_name': 'DatasetParameter'},
+            'datetime_value': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.ParameterName']"}),
+            'numerical_value': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'parameterset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.DatasetParameterSet']"}),
+            'string_value': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'tardis_portal.datasetparameterset': {
+            'Meta': {'ordering': "['id']", 'object_name': 'DatasetParameterSet'},
+            'dataset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Dataset']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'schema': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Schema']"})
+        },
+        'tardis_portal.experiment': {
+            'Meta': {'object_name': 'Experiment'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'created_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'end_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'handle': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'institution_name': ('django.db.models.fields.CharField', [], {'max_length': '400'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'start_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '400'}),
+            'update_time': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'tardis_portal.experimentacl': {
+            'Meta': {'ordering': "['experiment__id']", 'object_name': 'ExperimentACL'},
+            'aclOwnershipType': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'canDelete': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'canRead': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'canWrite': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'effectiveDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'entityId': ('django.db.models.fields.CharField', [], {'max_length': '320'}),
+            'experiment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Experiment']"}),
+            'expiryDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isOwner': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'pluginId': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        },
+        'tardis_portal.experimentparameter': {
+            'Meta': {'ordering': "['id']", 'object_name': 'ExperimentParameter'},
+            'datetime_value': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.ParameterName']"}),
+            'numerical_value': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'parameterset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.ExperimentParameterSet']"}),
+            'string_value': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'tardis_portal.experimentparameterset': {
+            'Meta': {'ordering': "['id']", 'object_name': 'ExperimentParameterSet'},
+            'experiment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Experiment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'schema': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Schema']"})
+        },
+        'tardis_portal.groupadmin': {
+            'Meta': {'object_name': 'GroupAdmin'},
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'tardis_portal.parametername': {
+            'Meta': {'object_name': 'ParameterName'},
+            'choices': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'comparison_type': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'data_type': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_searchable': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            'schema': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Schema']"}),
+            'units': ('django.db.models.fields.CharField', [], {'max_length': '60', 'blank': 'True'})
+        },
+        'tardis_portal.schema': {
+            'Meta': {'object_name': 'Schema'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'namespace': ('django.db.models.fields.URLField', [], {'max_length': '400'})
+        },
+        'tardis_portal.userauthentication': {
+            'Meta': {'object_name': 'UserAuthentication'},
+            'authenticationMethod': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'userProfile': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.UserProfile']"}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'tardis_portal.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isDjangoAccount': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['tardis_portal']
