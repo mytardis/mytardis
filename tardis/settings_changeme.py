923c148a9eb1139e9fac683e6029a7a2dbf4124c
shaun.okeefe.0@gmail.com <shaun.okeefe.0@gmail.com@174ef292-f9a9-40f3-a4db-b6760525a304>
2011-07-17 04:57:52 +0000

    added missing comma in settings_changeme.py
    git-svn-id: file:///home/uqtdettr/workspace/mytardis-svn-mirror/trunk@1659 174ef292-f9a9-40f3-a4db-b6760525a304

--- tardis/settings_changeme.py
+++ tardis/settings_changeme.py
@@ -89,7 +89,7 @@ ROOT_URLCONF = 'tardis.urls'
 TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.request',
                                'django.contrib.auth.context_processors.auth',
                                'django.core.context_processors.debug',
-                               'django.core.context_processors.i18n'
+                               'django.core.context_processors.i18n',
                                 'tardis.tardis_portal.context_processors.single_search_processor',
                                 )
 
