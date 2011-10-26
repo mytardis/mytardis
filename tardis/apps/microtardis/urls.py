568f93f9fdf7f73b71363e93800fecc39398b4bb
Joanna Huang <joanna.huitzu.huang@gmail.com>
2011-10-26 20:07:56 +1100

    MT-15

--- tardis/apps/microtardis/urls.py
+++ tardis/apps/microtardis/urls.py
@@ -2,8 +2,22 @@ from django.conf.urls.defaults import patterns
 from tardis.urls import urlpatterns as tardisurls
 
 urlpatterns = patterns('tardis.apps.microtardis.views',
-    (r'^$', 'index_microtardis'),
-    (r'^about/$', 'about_microtardis'),
+    (r'^$', 'index_mt'),
+    (r'^about/$', 'about_mt'),
+    (r'^partners/$', 'partners_mt'),
 )
 
+urlpatterns += patterns('tardis.tardis_portal.views',
+    (r'^stats/$', 'stats', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^groups/$', 'manage_groups', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^login/$', 'login', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^experiment/view/(?P<experiment_id>\d+)/publish/$', 'publish_experiment', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^experiment/view/(?P<experiment_id>\d+)/$', 'view_experiment', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^experiment/view/$', 'experiment_index', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^experiment/create/$', 'create_experiment', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^experiment/search/$', 'search_experiment', {'portal_template_name': 'portal_template_mt.html'}),
+    (r'^experiment/control_panel/$', 'control_panel', {'portal_template_name': 'portal_template_mt.html'}),
+    
+    
+)
 urlpatterns += tardisurls
--- tardis/apps/microtardis/views.py
+++ tardis/apps/microtardis/views.py
@@ -4,18 +4,23 @@ from django.http import HttpResponse
 from tardis.tardis_portal.shortcuts import render_response_index
 from tardis.tardis_portal.views import *
 
-def index_microtardis(request):
+def index_mt(request):
     status = ''
 
     c = Context({'status': status})
     return HttpResponse(render_response_index(request,
-                        'index_microtardis.html', c))
+                        'index_mt.html', c))
 
-def about_microtardis(request):
-
+def about_mt(request):
     c = Context({'subtitle': 'About',
                  'about_pressed': True,
                  'nav': [{'name': 'About', 'link': '/about/'}]})
-    return HttpResponse(render_response_index(request,
-                        'about_microtardis.html', c))
     
+    return HttpResponse(render_response_index(request,
+                        'about_mt.html', c))
+    
+def partners_mt(request):
+    c = Context({})
+    
+    return HttpResponse(render_response_index(request,
+                        'partners_mt.html', c))
