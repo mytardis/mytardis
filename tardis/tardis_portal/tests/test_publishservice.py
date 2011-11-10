6592ece6ddb2a9cc55454157729293225394c631
Veronica Luke <veronica.luke@gmail.com>
2011-11-10 05:54:58 +0000

    Added more tests that uses a default xml template in /tests/rifcs
    git-svn-id: http://mytardis.googlecode.com/svn/branches/rifcs-von@2220 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- tardis/tardis_portal/tests/test_publishservice.py
+++ tardis/tardis_portal/tests/test_publishservice.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
+from django.template import Context
+from django.conf import settings
 from tardis.tardis_portal.models import User, Experiment
 from tardis.tardis_portal.publish.provider.schemarifcsprovider import SchemaRifCsProvider
 from tardis.tardis_portal.publish.publishservice import PublishService
         
-BEAMLINE_VALUE = "myBeamline"
-SAMPLE_DESCS_VALUE = "sampleA\nsampleB\nsampleC\nsampleD\n"    
+BEAMLINE_VALUE = "myBeamline"  
 LICENSE_URL_VALUE = "http://some.uri.com"
-LICENSE_TITLE_VALUE = "myLicense"   
         
 class MockRifCsProvider(SchemaRifCsProvider):
     
@@ -16,17 +16,20 @@ class MockRifCsProvider(SchemaRifCsProvider):
     def get_beamline(self, experiment):
         return BEAMLINE_VALUE
     
-    def get_sample_description_list(self, experiment, beamline):
-        return SAMPLE_DESCS_VALUE
-    
     def get_license_uri(self, experiment):
         return LICENSE_URL_VALUE
     
-    def get_license_title(self, experiment):
-        return LICENSE_TITLE_VALUE
-    
     def get_template(self, experiment):
-        return "tardis/tardis_portal/rifcs/default.xml"
+        return "tardis/tardis_portal/tests/rifcs/default.xml"
+    
+    def get_rifcs_context(self, experiment):
+        c = Context({})
+        c['experiment'] = experiment
+        c['description'] = experiment.description
+        c['beamline'] = self.get_beamline(experiment)
+        c['license_url'] = self.get_license_uri(experiment)
+        return c
+        
 
 class PublishServiceTestCase(TestCase):
     
@@ -35,38 +38,66 @@ class PublishServiceTestCase(TestCase):
                                              email='user@test.com',
                                              password='secret')
         self.settings = ('tardis.tardis_portal.tests.test_publishservice.MockRifCsProvider',)
-        self.e1 = Experiment(id="1", title="Experiment 1", created_by=self.user, public=False)
+        self.e1 = Experiment(id="1", title="Experiment 1", 
+                             description="This is my description.",
+                             created_by=self.user, public=False)
         
     def testInitialisation(self):
         service = PublishService(self.settings, self.e1)
         self.assertEquals(self.e1, service.experiment)
         self.assertTrue(isinstance(service.provider, MockRifCsProvider))
+    
+    def testInitialisationNoProvider(self):
+        service = PublishService(None, self.e1)
+        self.assertIsNone(service.rc_providers)
+        from tardis.tardis_portal.publish.provider.rifcsprovider import RifCsProvider
+        self.assertTrue(isinstance(service.provider, RifCsProvider))
+        self.assertFalse(isinstance(service.provider, MockRifCsProvider))
         
     def testContext(self):
         service = PublishService(self.settings, self.e1)
         c = service.get_context()
         self.assertEquals(c['experiment'], self.e1)
         self.assertEquals(c['beamline'], BEAMLINE_VALUE)
-        self.assertEquals(c['sample_description_list'], SAMPLE_DESCS_VALUE)
-        self.assertEquals(c['license_title'], LICENSE_TITLE_VALUE)
-        self.assertEquals(c['license_uri'], LICENSE_URL_VALUE)     
+        self.assertEquals(c['license_url'], LICENSE_URL_VALUE)     
         
-    def testManageRifCs(self):
+    def testManageRifCsCreateAndRemove(self):
         service = PublishService(self.settings, self.e1)
         self.assertFalse(service.provider.can_publish(self.e1))
         import os
-        test_oai_path = os.path.join("tardis/tardis_portal/tests", "rifcs")
-        rifcs_output_dir = os.path.join(test_oai_path, self.e1.id)
-        service.manage_rifcs(test_oai_path)
+        service.manage_rifcs(settings.OAI_DOCS_PATH)
+        rifcs_output_dir = os.path.join(settings.OAI_DOCS_PATH, self.e1.id)
         self.assertFalse(os.path.exists(rifcs_output_dir))
         
         self.e1.public = True
-        self.assertRaises(AttributeError, service.manage_rifcs, test_oai_path)
+        service.manage_rifcs(settings.OAI_DOCS_PATH)
         self.assertTrue(os.path.exists(rifcs_output_dir))
     
         # Set to false again and see if it deletes it
         self.e1.public = False
-        service.manage_rifcs(test_oai_path)
+        service.manage_rifcs(settings.OAI_DOCS_PATH)
+        self.assertFalse(os.path.exists(rifcs_output_dir))
+        
+    def testManageRifCsCheckContent(self):
+        service = PublishService(self.settings, self.e1)
+        self.e1.public = True
+        service.manage_rifcs(settings.OAI_DOCS_PATH)
+        import os
+        rifcs_output_dir = os.path.join(settings.OAI_DOCS_PATH, self.e1.id)
+        self.assertTrue(os.path.exists(rifcs_output_dir))
+        rifcs_file = os.path.join(rifcs_output_dir, "experiment-1.xml")
+        self.assertTrue(os.path.exists(rifcs_file))
+        output = open(rifcs_file)
+        lines = output.readlines()
+        self.assertTrue("experiment title: Experiment 1\n" in lines)
+        self.assertTrue("experiment id: 1\n" in lines)
+        self.assertTrue("experiment description: This is my description.\n" in lines)
+        self.assertTrue("beamline: %s\n" % BEAMLINE_VALUE in lines)
+        self.assertTrue("license uri: %s\n" % LICENSE_URL_VALUE in lines)
+        
+        # Set to false again and see if it deletes it
+        self.e1.public = False
+        service.manage_rifcs(settings.OAI_DOCS_PATH)
         self.assertFalse(os.path.exists(rifcs_output_dir))
         
     
