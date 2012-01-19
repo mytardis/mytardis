77f3165bdd0482040fea32b1f8ffc22e81129b48
Ian Thomas <ianedwardthomas@gmail.com>
2012-01-20 09:48:18 +1100

    added hpctardis south migration

---
+++ tardis/apps/hpctardis/migrations/0003_auto__add_field_activityrecord_subject.py
@@ -0,0 +1,158 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'ActivityRecord.subject'
+        db.add_column('hpctardis_activityrecord', 'subject', self.gf('django.db.models.fields.CharField')(default='', max_length=200, blank=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'ActivityRecord.subject'
+        db.delete_column('hpctardis_activityrecord', 'subject')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'hpctardis.activitydescription': {
+            'Meta': {'object_name': 'ActivityDescription'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.ActivityRecord']"}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '80'}),
+            'value': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'})
+        },
+        'hpctardis.activitylocation': {
+            'Meta': {'object_name': 'ActivityLocation'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.ActivityRecord']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'url'", 'max_length': '80'}),
+            'value': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'})
+        },
+        'hpctardis.activitypartyrelation': {
+            'Meta': {'object_name': 'ActivityPartyRelation'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.ActivityRecord']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.PartyRecord']"}),
+            'relation': ('django.db.models.fields.CharField', [], {'default': "'isManagedBy'", 'max_length': '80'})
+        },
+        'hpctardis.activityrecord': {
+            'Meta': {'object_name': 'ActivityRecord'},
+            'activityname': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.NameParts']"}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ident': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200'}),
+            'parties': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['hpctardis.PartyRecord']", 'through': "orm['hpctardis.ActivityPartyRelation']", 'symmetrical': 'False'}),
+            'subject': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '80'})
+        },
+        'hpctardis.nameparts': {
+            'Meta': {'object_name': 'NameParts'},
+            'family': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'given': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'suffix': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'})
+        },
+        'hpctardis.partydescription': {
+            'Meta': {'object_name': 'PartyDescription'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.PartyRecord']"}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '80'}),
+            'value': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'})
+        },
+        'hpctardis.partylocation': {
+            'Meta': {'object_name': 'PartyLocation'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.PartyRecord']"}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'url'", 'max_length': '80'}),
+            'value': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'})
+        },
+        'hpctardis.partyrecord': {
+            'Meta': {'object_name': 'PartyRecord'},
+            'birthdate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'deathdate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'partyname': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'reverse'", 'to': "orm['hpctardis.NameParts']"}),
+            'subject': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'person'", 'max_length': '80'})
+        },
+        'hpctardis.publishauthevent': {
+            'Meta': {'object_name': 'PublishAuthEvent'},
+            'date_authorised': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'publish_auth': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.PublishAuthorisation']"}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'unknown'", 'max_length': '200'})
+        },
+        'hpctardis.publishauthorisation': {
+            'Meta': {'object_name': 'PublishAuthorisation'},
+            'activity_record': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.ActivityRecord']", 'null': 'True', 'blank': 'True'}),
+            'auth_key': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'authoriser': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'experiment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tardis_portal.Experiment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'party_record': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['hpctardis.PartyRecord']", 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        'tardis_portal.experiment': {
+            'Meta': {'object_name': 'Experiment'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'created_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'end_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'handle': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'institution_name': ('django.db.models.fields.CharField', [], {'default': "'RMIT University'", 'max_length': '400'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'start_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '400'}),
+            'update_time': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['hpctardis']
