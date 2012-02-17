e90ab55c70817ed216997dd20139dc52ea691461
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2012-02-17 17:30:41 +1100

    added class method for SyncManager for contacting home institutions, to be called by admin (todo) and post ingestion

--- tardis/apps/sync/managers/default_manager.py
+++ tardis/apps/sync/managers/default_manager.py
@@ -37,6 +37,7 @@ default_manager.py
 """
 import logging
 import os.path
+import urllib2
 from suds.client import Client
 
 from django.conf import settings
@@ -44,7 +45,11 @@ from django.conf import settings
 from tardis.apps.sync.site_parser import SiteParser
 from tardis.apps.sync.site_settings_parser import SiteSettingsParser
 from tardis.apps.sync.managers import SyncManagerTransferError, SyncManagerInvalidUIDError
+
 from tardis.tardis_portal.models import Experiment, ExperimentParameter
+
+# TODO surely this goes in 'forms', if not in our app?
+from tardis.tardis_portal import MultiPartForm
 
 logger = logging.getLogger('tardis.mecat')
 
@@ -122,6 +127,126 @@ class SyncManager():
             sites.append((name, url, username, password))
 
         return sites
+
+    # originally '_register_file_settings' in parser
+    #
+    # TODO change the mecat-as app etc to call this 
+    # rather than the parser.py files registration logic
+    #
+    def push_experiment_to_institutions(self, experiment, owners):
+        """
+        ingests meta-data and transfers data to experiment owner's
+        mytardis instance at the home institute
+
+        :keyword request: Django request
+        :keyword cleaned_data: cleaned form data
+        :keyword expid: id of experiment to be transfered
+        """
+
+        # sites which will receive a copy of the data
+        sites = []
+
+        # register at home institute
+
+        # owner list should be generated outside
+        #owners = re.compile(r'\s+').split(cleaned_data['experiment_owner'])
+        for owner in owners:
+        #    if owner == '':
+        #       continue
+
+            # get list of site from master tardis instance (Monash)
+            # TODO: might be better to store these sites in Django's
+            # sites table!
+            url = settings.MYTARDIS_SITES_URL
+            logger.debug('fetching mytardis sites from %s' % url)
+            #try:
+            #    sites_username = settings.MYTARDIS_SITES_USERNAME
+            #    sites_password = settings.MYTARDIS_SITES_PASSWORD
+            #except AttributeError:
+            #    # maybe we get the site list from a file...
+            #    sites_username = ''
+            #    sites_password = ''
+
+            # username and password must be set if another site is contacted
+            #sp = SiteParser(url, sites_username, sites_password)
+            
+
+            # loop over sites
+            for name in self.sp.getSiteNames():
+                url = self.sp.getSiteSettingsURL(name)
+                # fetch a MyTARDIS site's config
+                logger.debug('fetching site config for %s from %s' % (name, url))
+                try:
+                    ssp = SiteSettingsParser(url,
+                                             self.sp.getSiteSettingsUsername(name),
+                                             self.sp.getSiteSettingsPassword(name))
+                except:
+                    logger.exception('fetching site config from %s FAILED' % url)
+                    continue
+
+                # is the email domain of the experiment's owner registered
+                # by any site?
+                siteOwners = []
+                for owner in owners:
+                    for domain in ssp.getEmailEndswith():
+                        if owner.endswith(domain):
+                            siteOwners.append(owner)
+
+                # register meta-data and file transfer request at another
+                # MyTARDIS instance
+                if siteOwners:
+                    # Create the form with simple fields
+                    uid = self.sm.generate_exp_uid(experiment)
+                    mpform = MultiPartForm()
+                    mpform.add_field('username', ssp.getRegisterSetting('username'))
+                    mpform.add_field('password', ssp.getRegisterSetting('password'))
+                    mpform.add_field('from_url', request.build_absolute_uri())
+                    mpform.add_field('originid', uid)#str(expid))
+
+                    for siteOwner in siteOwners:
+                        mpform.add_field('experiment_owner', siteOwner)
+
+                    protocol = ssp.getRegisterSetting('fileProtocol')
+
+                    # export METS file
+                    filename = 'mets_expid_%i_%s' % (experiment.id, protocol)
+                    logger.debug('=== extracting mets file for experiment %i ' % uid)
+                    from tardis.tardis_portal.metsexporter import MetsExporter
+                    exporter = MetsExporter()
+                    if protocol:
+                        # translate vbl:// into tardis:// url for datafiles
+                        metsfile = exporter.export(experimentId=experiment.id,
+                                                   replace_protocols={'vbl': protocol},
+                                                   filename=filename,
+                                                   export_images=False)
+                    else:
+                        metsfile = exporter.export(experimentId=experiment.id,
+                                                   filename=filename,
+                                                   export_images=False)
+                    logger.debug('=== extraction done, filename = %s' % metsfile)
+
+                    f = open(metsfile, "r")
+                    mpform.add_file('xmldata', 'METS.xml', fileHandle=f)
+
+                    ws = ssp.getRegisterSetting('url')
+                    logger.debug('about to send register request to site %s' % ws)
+                    # build the request
+                    requestmp = urllib2.Request(ws)
+                    requestmp.add_header('User-agent', 'PyMOTW (http://www.doughellmann.com/PyMOTW/)')
+                    body = str(mpform)
+                    requestmp.add_header('Content-type', mpform.get_content_type())
+                    requestmp.add_header('Content-length', len(body))
+
+                    # logger.debug('OUTGOING DATA: ' + body)
+                    logger.debug('SERVER RESPONSE: ' + urllib2.urlopen(requestmp, body, 99999).read())
+
+                    f.close()
+                    sites.append(url)
+
+            # return a list of registered sites
+            return sites
+
+        return []
 
     def start_file_transfer(self, exp, site_settings_url, site_username, site_password):
        
