31a94cc977a513b7b6856e16484498bfe2c65628
Joanna Huang <joanna.huitzu.huang@gmail.com>
2012-02-23 16:32:58 +1100

    removed logging info in staging.py.

--- tardis/tardis_portal/staging.py
+++ tardis/tardis_portal/staging.py
@@ -130,7 +130,6 @@ class StagingHook():
         """
         instance = kwargs.get('instance')
         created = kwargs.get('created')
-        logger.debug('Test Stage file: %s ' % instance.protocol)
         if not created:
             # Don't extract on edit
             return
