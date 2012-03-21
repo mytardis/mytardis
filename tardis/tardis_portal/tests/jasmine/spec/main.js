fb69b819c64ccd30f39b149a776f0beacef4e59e
Tim Dettrick <t.dettrick@uq.edu.au>
2012-03-21 16:20:42 +1000

    Eliminated obsolete jquery-autocomplete plugin and replaced with jQuery UI autocomplete and Twitter Bootstrap typeahead.

--- tardis/tardis_portal/tests/jasmine/spec/main.js
+++ tardis/tardis_portal/tests/jasmine/spec/main.js
@@ -2,33 +2,100 @@ jasmine.getFixtures().fixturesPath = "/jasmine/fixtures/";
 
 describe("Main", function() {
 
-	describe("search box autocompletion", function() {
-	    it("should activate autocompletion for #id_q", function() {
-	        loadFixtures("search_autocomplete.html");
-	        expect($('#id_q')).toExist();
-	        activateSearchAutocomplete();
-	        waitsFor(function() {
-	        	return $('#id_q[autocomplete]').size() > 0;
-	        }, "Should be activated by now.", 500);
-	        runs(function() {
-		        expect($('#id_q')).toHaveAttr('autocomplete');
-		        expect($('#id_q')).toHaveClass('ac_input');
-	        });
-	    });
-	});
+  describe("search box typeahead", function() {
+      it("should activate typeahead for #id_q", function() {
+          loadFixtures("search_autocomplete.html");
+          expect($('#id_q')).toExist();
+          expect($('#id_q').data('typeahead')).toBeUndefined();
+          activateSearchAutocomplete();
+          waitsFor(function() {
+            return typeof($('#id_q').data('typeahead')) != 'undefined' ;
+          }, "Should be activated by now.", 500);
+          runs(function() {
+            expect($('#id_q').data('typeahead').options.items).toBe(10);
+          });
+      });
+  });
 
-	describe("hover detection", function() {
-	    it("should add/remove class on mouseover/mouseout", function() {
-	        setFixtures('<div id="hover_detect" class="ui-state-default"></div>');
-	        expect($('#hover_detect')).toExist();
-	        activateHoverDetection();
-	        expect($('#hover_detect')).not.toHaveClass('ui-state-hover');
-	        $('#hover_detect').trigger('mouseover');
-	        expect($('#hover_detect')).toHaveClass('ui-state-hover');
-	        $('#hover_detect').trigger('mouseout');
-	        expect($('#hover_detect')).not.toHaveClass('ui-state-hover');
-	    });
+  describe("hover detection", function() {
+      it("should add/remove class on mouseover/mouseout", function() {
+          setFixtures('<div id="hover_detect" class="ui-state-default"></div>');
+          expect($('#hover_detect')).toExist();
+          activateHoverDetection();
+          expect($('#hover_detect')).not.toHaveClass('ui-state-hover');
+          $('#hover_detect').trigger('mouseover');
+          expect($('#hover_detect')).toHaveClass('ui-state-hover');
+          $('#hover_detect').trigger('mouseout');
+          expect($('#hover_detect')).not.toHaveClass('ui-state-hover');
+      });
 
-	});
+  });
+
+  describe("user autocompletion handler", function() {
+    it("should search a list of users", function() {
+      var testData = [
+        {
+          "username": "testuser1",
+          "first_name": "Test",
+          "last_name": "User",
+          "email": "testuser1@example.test",
+          "auth_methods": ["testuser:localdb:Local DB"]
+        },
+        {
+          "username": "testuser2",
+          "first_name": "Test",
+          "last_name": "User",
+          "email": "",
+          "auth_methods": ["testuser2:localdb:Local DB"]
+        },
+        {
+          "username": "testuser3",
+          "first_name": "",
+          "last_name": "",
+          "email": "testuser3@example.test",
+          "auth_methods": ["testuser3:localdb:Local DB"]
+        },
+        {
+          "username": "testuser4",
+          "first_name": "",
+          "last_name": "",
+          "email": "",
+          "auth_methods": ["testuser4:localdb:Local DB"]
+        },
+        {
+          "username": "testuser5",
+          "first_name": "Test",
+          "last_name": "User",
+          "email": "testuser5@example.test",
+          "auth_methods": ["testuser5:otherdb:Other DB"]
+        }
+      ];
+      // Basic query checks
+      expect(userAutocompleteHandler("test", testData, 'localdb')).toEqual([
+        {
+          'label': 'Test User [testuser1] <testuser1@example.test>',
+          'value': 'testuser1'
+        },
+        {
+          'label': 'Test User [testuser2]',
+          'value': 'testuser2'
+        },
+        {
+          'label': '[testuser3] <testuser3@example.test>',
+          'value': 'testuser3'
+        },
+        {
+          'label': '[testuser4]',
+          'value': 'testuser4'
+        }
+      ]);
+      expect(userAutocompleteHandler("testuser1", testData, 'localdb')).toEqual([
+        {
+          'label': 'Test User [testuser1] <testuser1@example.test>',
+          'value': 'testuser1'
+        }
+      ]);
+    });
+  });
 
 });
