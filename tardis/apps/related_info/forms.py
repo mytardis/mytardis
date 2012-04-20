a6cb269cc0c7846f6f78bb0115755f3500d894ee
Tim Dettrick <t.dettrick@uq.edu.au>
2012-04-20 18:40:25 +1000

    Adding an editing UI with really poor validation.

--- tardis/apps/related_info/forms.py
+++ tardis/apps/related_info/forms.py
@@ -5,6 +5,6 @@ class RelatedInfoForm(Form):
     type = CharField( #@ReservedAssignment
                      widget=Select(choices=[('publication', 'Publication'),
                                             ('website', 'Website')]))
-    identifier = URLField()
+    identifier = URLField(required=True, max_length=2000)
     title = CharField(max_length=100, required=False)
     notes = CharField(required=False, widget=Textarea())
