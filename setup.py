d75bcbbe2b3d3f7cf600c9a1c65a6bce7608e5be
Tim Dettrick <t.dettrick@uq.edu.au>
2012-04-24 17:24:21 +1000

    Using bugfixed version of Wand to handle non-standard TIFF files.

--- setup.py
+++ setup.py
@@ -38,7 +38,12 @@ setup(
         'pytz',         # Timezone library
         'iso8601',      # ISO8601 time formatting
         'pyoai',        # For OAI-PMH provider
-        'Wand',         # For image file conversion
+        'Wand>=0.1.10',    # For image file conversion
         'django-mustachejs', # For client-side Mustache template helpers
         ],
+    dependency_links = [
+        # Until https://github.com/StyleShare/wand/pull/16 is accepted, we
+        # need this to handle strangely-formatted TIFF files.
+        'https://github.com/tjdett/wand/tarball/warning-bugfix#egg=Wand-0.1.10'
+    ]
 )
