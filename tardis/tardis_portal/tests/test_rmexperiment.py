8b17eea9f0e3aefe5df0d2ecfdffdf0cba3d3482
Stephen Crawley <s.crawley@uq.edu.au>
2012-08-23 16:40:39 +1000

    Overhauled the 'rmexperiment' management command to understand the new datamodel Added a couple of unit tests.

---
+++ tardis/tardis_portal/tests/test_rmexperiment.py
@@ -0,0 +1,133 @@
+from compare import expect
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.test.client import Client
+from django.core.management import call_command
+
+import sys
+
+from tardis.tardis_portal.models import \
+    Experiment, Dataset, Dataset_File, ExperimentACL, License, UserProfile, \
+    ExperimentParameterSet, ExperimentParameter, DatasetParameterSet, DatafileParameterSet
+    
+def _create_test_user():
+    user_ = User(username='tom',
+                first_name='Thomas',
+                last_name='Atkins',
+                email='tommy@atkins.net')
+    user_.save()
+    UserProfile(user=user_).save()
+    return user_
+
+def _create_license():
+    license_ = License(name='Creative Commons Attribution-NoDerivs 2.5 Australia',
+                       url='http://creativecommons.org/licenses/by-nd/2.5/au/',
+                       internal_description='CC BY 2.5 AU',
+                       allows_distribution=True)
+    license_.save()
+    return license_
+
+def _create_test_experiment(user, license_):
+    experiment = Experiment(title='Norwegian Blue',
+                            description='Parrot + 40kV',
+                            created_by=user)
+    experiment.public_access = Experiment.PUBLIC_ACCESS_FULL
+    experiment.license = license_
+    experiment.save()
+    experiment.author_experiment_set.create(order=0,
+                                            author="John Cleese",
+                                            url="http://nla.gov.au/nla.party-1")
+    experiment.author_experiment_set.create(order=1,
+                                            author="Michael Palin",
+                                            url="http://nla.gov.au/nla.party-2")
+    acl = ExperimentACL(experiment=experiment,
+                    pluginId='django_user',
+                    entityId=str(user.id),
+                    isOwner=True,
+                    canRead=True,
+                    canWrite=True,
+                    canDelete=True,
+                    aclOwnershipType=ExperimentACL.OWNER_OWNED)
+    acl.save()
+    return experiment
+
+def _create_test_dataset(nosDatafiles):
+    ds_ = Dataset(description='happy snaps of plumage')
+    ds_.save()
+    for i in range (0, nosDatafiles) :
+        df_ = Dataset_File(dataset=ds_, url='http://planet-python.org/' + str(_next_id()))
+        df_.save()
+    ds_.save()
+    return ds_
+
+def _create_test_data():
+    # Create 2 experiments with 3 datasets, one of which is in both experiments.
+    user_ = _create_test_user()
+    license_ = _create_license()
+    exp1_ = _create_test_experiment(user_, license_)
+    exp2_ = _create_test_experiment(user_, license_)
+    ds1_ = _create_test_dataset(1)
+    ds2_ = _create_test_dataset(2)
+    ds3_ = _create_test_dataset(3)
+    ds1_.experiments.add(exp1_);
+    ds2_.experiments.add(exp1_);
+    ds2_.experiments.add(exp2_);
+    ds3_.experiments.add(exp2_);
+    ds1_.save()
+    ds2_.save()
+    ds3_.save()
+    exp1_.save()
+    exp2_.save()
+    return (exp1_, exp2_)
+
+_counter = 1
+
+def _next_id():
+    global _counter
+    res = _counter
+    _counter += 1
+    return res
+
+class RmExperimentTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def testList(self):
+        (exp1_, exp2_) = _create_test_data()
+        expect(Dataset_File.objects.all().count()).to_be(6)
+        expect(len(exp1_.get_datafiles())).to_be(3)
+        expect(len(exp2_.get_datafiles())).to_be(5)
+        
+        # Check that --list doesn't remove anything
+        call_command('rmexperiment', exp1_.pk, list=True)
+        expect(Dataset_File.objects.all().count()).to_be(6)
+        expect(len(exp1_.get_datafiles())).to_be(3)
+        expect(len(exp2_.get_datafiles())).to_be(5)
+        
+    def testRemove(self):
+        (exp1_, exp2_) = _create_test_data()
+        expect(Dataset_File.objects.all().count()).to_be(6)
+        expect(len(exp1_.get_datafiles())).to_be(3)
+        expect(len(exp2_.get_datafiles())).to_be(5)
+        
+        # Remove first experiment and check that the shared dataset hasn't been removed
+        call_command('rmexperiment', exp1_.pk, confirmed=True)
+        expect(Dataset_File.objects.all().count()).to_be(5)
+        expect(len(exp2_.get_datafiles())).to_be(5)
+        
+        #Remove second experiment
+        call_command('rmexperiment', exp2_.pk, confirmed=True)
+        expect(Dataset_File.objects.all().count()).to_be(0)
+        
+        #Check that everything else has been removed too
+        expect(ExperimentACL.objects.all().count()).to_be(0)
+        expect(ExperimentParameterSet.objects.all().count()).to_be(0)
+        expect(ExperimentParameter.objects.all().count()).to_be(0)
+        expect(DatasetParameterSet.objects.all().count()).to_be(0)
+        expect(DatafileParameterSet.objects.all().count()).to_be(0)
+        
+    def tearDown(self):
+        pass
+
