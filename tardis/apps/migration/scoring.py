f7ea5af470fbcc55d063aa25384bacf8568eebaa
Stephen Crawley <s.crawley@uq.edu.au>
2012-11-01 18:18:21 +1000

    Snapshot ...

--- tardis/apps/migration/scoring.py
+++ tardis/apps/migration/scoring.py
@@ -1,5 +1,16 @@
+import math
+from tardis.tardis_portal.models import Dataset_File
 
 class MigrationScorer:
+    """
+    This class implements the algorithms for scoring a group of Datafiles
+    to figure which ones to migrate if we are running short of space.  The 
+    general rule is that Datafiles with the largest scores are most eligible
+    for migration to slower / cheaper storage.
+
+    A MigrationScorer instance memoizes the score contributions of users
+    and experiments and datasets.  It is therefore stateful. 
+    """
     
     def score_datafile(self, datafile):
         return self.datafile_score(datafile) * \
@@ -8,20 +19,27 @@ class MigrationScorer:
     def score_datafiles_in_dataset(self, dataset):
         ds_score = self.dataset_score(dataset)
         def score_it(datafile):
-            return (datafile.id, ds_score * self.datafile_score(datafile))
-        return map(score_it, Dataset_File.objects.filter(dataset=dataset))
+            return (datafile, ds_score * self.datafile_score(datafile))
+        datafiles = Dataset_File.objects.filter(dataset=dataset, verified=True)
+        return map(score_it, filter(Dataset_File.is_local, datafiles))
 
     def score_datafiles_in_experiment(self, experiment):
-        raise NotImplementedError
+        def score_it(datafile):
+            ds_score = self.dataset_score(datafile.dataset)
+            return (datafile, ds_score * self.datafile_score(datafile))
+        datafiles = Dataset_File.objects.\
+            filter(dataset__experiments__id=experiment.id, verified=True)
+        return map(score_it, filter(Dataset_File.is_local, datafiles))
 
-    def score_all_datafiles(self, experiment):
-        raise NotImplementedError
-
-    def score_datafiles_in_experiment(self, experiment):
-        raise NotImplementedError
+    def score_all_datafiles(self):
+        def score_it(datafile):
+            ds_score = self.dataset_score(datafile.dataset)
+            return (datafile, ds_score * self.datafile_score(datafile))
+        datafiles = Dataset_File.objects.filter(verified=True)
+        return map(score_it, filter(Dataset_File.is_local, datafiles))
 
     def datafile_score(self, datafile):
-        return math.log10(datafile.size)
+        return math.log10(float(datafile.size))
 
     def dataset_score(self, dataset):
         return 1.0
--- tardis/apps/migration/tests/test_scorer.py
+++ tardis/apps/migration/tests/test_scorer.py
@@ -11,25 +11,30 @@ from tardis.apps.migration import MigrationScorer
 
 class MigrateScorerTestCase(TestCase):
 
-    def setUp(self):
-        self.dummy_dataset = Dataset()
-        self.dummy_dataset.save()
-        self.dummy_user = self._generate_user()
-
-    def tearDown(self):
-        self.dummy_dataset.delete()
-        self.dummy_user.delete()
-
     def testScoring(self):
+        user1 = self._generate_user('ron')
+        user2 = self._generate_user('eunice')
+        exp1 = self._generate_experiment([user1, user2])
+        ds1 = self._generate_dataset([exp1])
+        df1 = self._generate_datafile('1/2/foo', 100, ds1)
+        df2 = self._generate_datafile('1/2/bar', 100, ds1, verified=False)
+        df3 = self._generate_datafile('http://foo.com/1/2/foo', 1000, ds1)
         scorer = MigrationScorer()
+        self.assertEquals(2.0, scorer.score_datafile(df1))
+        self.assertEquals([(df1, 2.0,)], 
+                          scorer.score_datafiles_in_dataset(ds1))
+        self.assertEquals([(df1, 2.0,)],
+                          scorer.score_datafiles_in_experiment(exp1))
+        self.assertEquals([(df1, 2.0,)],
+                          scorer.score_all_datafiles())
         pass
 
     def _generate_datafile(self, path, size, dataset, verified=True):
         datafile = Dataset_File()
         datafile.url = path
         datafile.mimetype = "application/unspecified"
-        datafile.filename = os.path.basename(filepath)
-        datafile.dataset_id = self.dataset.id
+        datafile.filename = os.path.basename(path)
+        datafile.dataset_id = dataset.id
         datafile.size = size
         datafile.verified = verified
         datafile.save()
@@ -38,16 +43,17 @@ class MigrateScorerTestCase(TestCase):
     def _generate_dataset(self, experiments):
         dataset = Dataset()
         dataset.save()
+        dataset.experiments.extend(experiments)
+        dataset.save()
+        return dataset
 
     def _generate_experiment(self, users):
         experiment = Experiment(created_by=users[0])
         experiment.save()
+        return experiment
 
-    def _generate_user(self):
-        user = User(username='jim',
-                    first_name='James',
-                    last_name='Spriggs',
-                    email='jim.spriggs@goonshow.com')
+    def _generate_user(self, name):
+        user = User(username=name)
         user.save()
         UserProfile(user=user).save()
         return user
