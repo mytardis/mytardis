435bd8223b99f8223cc9f11d50cefde8fc0e5dfe
Stephen Crawley <s.crawley@uq.edu.au>
2013-01-10 14:01:49 +1000

    Minor refactor.  Generalize the method for getting the free disc space

--- tardis/tardis_portal/util.py
+++ tardis/tardis_portal/util.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 import pystache
 import pytz
+import os, platform, ctypes, stat, time, struct
 
 LOCAL_TZ = pytz.timezone(settings.TIME_ZONE)
 
@@ -29,6 +30,27 @@ def get_utc_time(dt):
     if (dt.tzinfo == None):
         dt = dt.replace(tzinfo=LOCAL_TZ)
     return dt.astimezone(pytz.utc)
+
+def get_free_space(fs_dir):
+    """ Return free space on the file system holding the given directory
+    (in bytes).  This should work on Linux, BSD, Mac OSX and Windows.
+    """
+    sys_type = platform.system()
+    if sys_type == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(fs_dir), 
+                                                   None, None, 
+                                                   ctypes.pointer(free_bytes))
+        return free_bytes.value
+    elif sys_type == 'Darwin' or sys_type == 'DragonFly' or 'BSD' in sys_type:
+        st = os.statvfs(fs_dir)
+        return st.f_bfree * st.f_frsize
+    elif sys_type == 'Linux':
+        st = os.statvfs(fs_dir)
+        return st.f_bfree * st.f_bsize
+    else:
+        raise RuntimeError('Unsupported / unexpected platform type: %s' % \
+                               sys_type)
 
 def _load_template(template_name):
     from mustachejs.loading import find
