a7c764d7c62ed3b41c61b30455b3085ea24c628a
Grischa Meyer <grischa@gmail.com>
2013-05-01 17:26:43 +1000

    put registerExperiment on celery delay

--- tardis/tardis_portal/views.py
+++ tardis/tardis_portal/views.py
@@ -48,6 +48,8 @@ import logging
 import json
 from operator import itemgetter
 
+from celery.task import task
+
 from django.template import Context
 from django.conf import settings
 from django.db import transaction
@@ -82,8 +84,7 @@ from tardis.tardis_portal.errors import UnsupportedSearchQueryTypeError
 
 from tardis.tardis_portal.staging import get_full_staging_path, \
     write_uploaded_file_to_dataset, get_staging_url_and_size, \
-    staging_list
-
+    staging_list, get_sync_root
 from tardis.tardis_portal.tasks import create_staging_datafiles,\
     create_staging_datafile
 
@@ -914,9 +915,11 @@ def manage_auth_methods(request):
 
 
 # TODO removed username from arguments
+@task(name="tardis_portal._registerExperimentDocument")
 @transaction.commit_on_success
 def _registerExperimentDocument(filename, created_by, expid=None,
-                                owners=[], username=None):
+                                owners=[], username=None,
+                                sync_root=None):
     '''
     Register the experiment document and return the experiment id.
 
@@ -939,7 +942,8 @@ def _registerExperimentDocument(filename, created_by, expid=None,
     f.close()
 
     logger.debug('processing METS')
-    eid, sync_root = parseMets(filename, created_by, expid)
+    eid, sync_root = parseMets(filename, created_by, expid,
+                               sync_root=sync_root)
 
     auth_key = ''
     try:
@@ -1032,18 +1036,15 @@ def register_experiment_ws_xmldata(request):
 
             logger.info('=== processing experiment: START')
             owners = request.POST.getlist('experiment_owner')
-            try:
-                _, sync_path = _registerExperimentDocument(filename=filename,
-                                                           created_by=user,
-                                                           expid=local_id,
-                                                           owners=owners,
-                                                           username=username)
-                logger.info('=== processing experiment %s: DONE' % local_id)
-            except:
-                logger.exception('=== processing experiment %s: FAILED!' %
-                                 local_id)
-                return return_response_error(request)
-
+            sync_path = get_sync_root(prefix="%d-" % local_id)
+            taskout = _registerExperimentDocument.delay(filename=filename,
+                                                        created_by=user,
+                                                        expid=local_id,
+                                                        owners=owners,
+                                                        username=username,
+                                                        sync_path=sync_path)
+            logger.info('=== processing experiment %s: on delay' % local_id)
+            logger.info('=== task: %s' % str(taskout))
             if from_url:
                 logger.info('Sending received_remote signal')
                 from tardis.tardis_portal.signals import received_remote
