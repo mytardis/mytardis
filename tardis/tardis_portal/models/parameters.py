b4f39a4f0780bc016a5a38f86b092e96054b22e3
Sindhu Emilda <emildasindhu@gmail.com>
2013-09-19 12:41:17 +1000

    Natural key implementation for model, ExperimentParameter

--- tardis/tardis_portal/models/parameters.py
+++ tardis/tardis_portal/models/parameters.py
@@ -401,10 +401,27 @@ class DatasetParameter(Parameter):
     parameterset = models.ForeignKey('DatasetParameterSet')
     parameter_type = 'Dataset'
 
+class ExperimentParameterManager(OracleSafeManager):
+    """
+    Added by Sindhu Emilda for natural key implementation.
+    The manager for the tardis_portal's ExperimentParameter model.
+    """
+    def get_by_natural_key(self, namespace, name, nmspace, title, username):
+        return self.get(name=ParameterName.objects.get_by_natural_key(namespace, name),
+                        parameterset=ExperimentParameterSet.objects.get_by_natural_key(nmspace, title, username),
+        )
 
 class ExperimentParameter(Parameter):
     parameterset = models.ForeignKey('ExperimentParameterSet')
     parameter_type = 'Experiment'
+    
+    ''' Added by Sindhu Emilda for natural key implementation '''
+    objects = ExperimentParameterManager()
+    
+    def natural_key(self):
+        return (self.name.natural_key(),) + self.parameterset.natural_key()
+    
+    natural_key.dependencies = ['tardis_portal.ParameterName', 'tardis_portal.ExperimentParameterSet']
 
     def save(self, *args, **kwargs):
         super(ExperimentParameter, self).save(*args, **kwargs)
