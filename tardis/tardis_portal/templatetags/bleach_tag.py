f5e282243024949adb3495c0a3dd3fb5fdcd3b88
Grischa Meyer <grischa@gmail.com>
2013-10-29 17:58:27 +1100

    bleaching experiment descriptions

---
+++ tardis/tardis_portal/templatetags/bleach_tag.py
@@ -0,0 +1,16 @@
+import bleach
+
+from django import template
+from django.conf import settings
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+tags = getattr(settings, 'ALLOWED_HTML_TAGS', bleach.ALLOWED_TAGS)
+
+
+def bleach_value(value):
+    bleached_value = bleach.clean(value, tags=tags)
+    return mark_safe(bleached_value)
+
+register.filter('bleach', bleach_value)
