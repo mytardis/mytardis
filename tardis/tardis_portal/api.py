0a97a4f9afc62b68f53eaaf9516e3540bba44983
Grischa Meyer <grischa@gmail.com>
2014-06-05 14:07:25 +1000

    API additions for sftp

--- tardis/tardis_portal/api.py
+++ tardis/tardis_portal/api.py
@@ -77,7 +77,7 @@ class MyTardisAuthentication(object):
     custom tastypie authentication that works with both anonymous use and
     a number of available auth mechanisms.
     '''
-    def is_authenticated(self, request, **kwargs):
+    def is_authenticated(self, request, **kwargs):  # noqa # too complex
         '''
         handles backends explicitly so that it can return False when
         credentials are given but wrong and return Anonymous User when
@@ -129,7 +129,7 @@ default_authentication = MyTardisAuthentication()
 class ACLAuthorization(Authorization):
     '''Authorisation class for Tastypie.
     '''
-    def read_list(self, object_list, bundle):
+    def read_list(self, object_list, bundle):  # noqa # too complex
         if bundle.request.user.is_authenticated() and \
            bundle.request.user.is_superuser:
             return object_list
@@ -195,7 +195,7 @@ class ACLAuthorization(Authorization):
         else:
             return []
 
-    def read_detail(self, object_list, bundle):
+    def read_detail(self, object_list, bundle):  # noqa # too complex
         if bundle.request.user.is_authenticated() and \
            bundle.request.user.is_superuser:
             return True
@@ -240,7 +240,7 @@ class ACLAuthorization(Authorization):
     def create_list(self, object_list, bundle):
         raise NotImplementedError(type(bundle.obj))
 
-    def create_detail(self, object_list, bundle):
+    def create_detail(self, object_list, bundle):  # noqa # too complex
         if not bundle.request.user.is_authenticated():
             return False
         if bundle.request.user.is_authenticated() and \
@@ -345,7 +345,7 @@ class ACLAuthorization(Authorization):
 
         # return allowed
 
-    def update_detail(self, object_list, bundle):
+    def update_detail(self, object_list, bundle):  # noqa # too complex
         if not bundle.request.user.is_authenticated():
             return False
         if type(bundle.obj) == Experiment:
@@ -568,6 +568,7 @@ class ExperimentResource(MyTardisModelResource):
     class Meta(MyTardisModelResource.Meta):
         queryset = Experiment.objects.all()
         filtering = {
+            'id': ('exact', ),
             'title': ('exact',),
         }
 
@@ -676,6 +677,7 @@ class DatasetResource(MyTardisModelResource):
         queryset = Dataset.objects.all()
         filtering = {
             'id': ('exact', ),
+            'experiments': ALL_WITH_RELATIONS,
             'description': ('exact', ),
             'directory': ('exact', ),
         }
