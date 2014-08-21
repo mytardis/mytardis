ccb1310ca3989526e082f8fbd886f045ec8f72fe
Grischa Meyer <grischa@gmail.com>
2014-08-21 11:38:10 +1000

    generic squash ingestion parser

--- tardis/tardis_portal/storage/squashfs.py
+++ tardis/tardis_portal/storage/squashfs.py
@@ -1,18 +1,6 @@
 '''
 SquashFS Storage Box
 '''
-import errno
-import os
-import subprocess
-
-from datetime import datetime
-
-from django.conf import settings
-from django.core.exceptions import SuspiciousOperation
-from django.core.files import File
-from django.core.files.storage import Storage
-from django.utils._os import safe_join
-
 '''
 setup requirements:
 user-owned mount root dir
@@ -27,6 +15,25 @@ cd squashfuse
 make
 sudo make install
 '''
+import errno
+import os
+import subprocess
+
+from datetime import datetime
+from magic import Magic
+
+from django.conf import settings
+from django.core.exceptions import SuspiciousOperation
+from django.core.files import File
+from django.core.files.storage import Storage
+from django.utils._os import safe_join
+from django.utils.importlib import import_module
+
+from tardis.tardis_portal.models import Dataset, DataFile, DataFileObject
+from tardis.tardis_portal.util import generate_file_checksums
+
+import logging
+log = logging.getLogger(__name__)
 
 
 class SquashFSStorage(Storage):
@@ -34,7 +41,7 @@ class SquashFSStorage(Storage):
     The default mounter is squashfuse, which can be found here:
     https://github.com/vasi/squashfuse
 
-    setting:
+    settings:
     SQUASHFS_MOUNT_ROOT = '/mnt/squashfs'
     SQUASHFS_MOUNT_CMD = "/usr/local/bin/squashfuse"
     """
@@ -42,6 +49,9 @@ class SquashFSStorage(Storage):
     squashmount_root = getattr(settings,
                                'SQUASHFS_MOUNT_ROOT',
                                '/mnt/squashfs')
+    squashmount_cmd = getattr(settings,
+                              'SQUASHFSMOUNT_CMD',
+                              '/usr/local/bin/squashfuse')
 
     def __init__(self, sq_filename=None, sq_basepath=None):
         if sq_filename is None or sq_basepath is None:
@@ -60,16 +70,13 @@ class SquashFSStorage(Storage):
     def _mount(self):
         if self._mounted:
             return
-        squashmount_cmd = getattr(settings,
-                                  'SQUASHFSMOUNT_CMD',
-                                  '/usr/local/bin/squashfuse')
         try:
             os.makedirs(self.location)
         except OSError as exc:
             if not (exc.errno == errno.EEXIST and
                     os.path.isdir(self.location)):
                 raise
-        subprocess.call([squashmount_cmd,
+        subprocess.call([self.squashmount_cmd,
                          self.squashfile, self.location])
 
     def _open(self, name, mode='rb'):
@@ -116,8 +123,97 @@ class SquashFSStorage(Storage):
         if dfo.uri is None:
             return None
         sq_name = self.sq_filename.replace('.squashfs', '')
-        split_path = os.path.split(dfo.uri)
+        split_path = dfo.uri.split(os.sep)
         if split_path[0] == sq_name:
             return os.path.join(split_path[1:])
         else:
             return dfo.uri
+
+
+def dj_storage_walk(dj_storage, top='.', topdown=True, onerror=None):
+    try:
+        dirnames, filenames = dj_storage.listdir(top)
+    except os.error as err:
+        if onerror is not None:
+            onerror(err)
+        return
+    if topdown:
+        yield top, dirnames, filenames
+    for dirname in dirnames:
+        new_top = os.path.join(top, dirname)
+        for result in dj_storage_walk(dj_storage, new_top):
+            yield result
+    if not topdown:
+        yield top, dirnames, filenames
+
+
+def squashparser(exp, squash_sbox, inst, directory, filename, filepath):
+    '''
+    return matching or new datafile for given squashfs file path.
+
+    parse discipline specific if set up
+
+    by default use 'squashfs-files' as dataset name
+    '''
+    parsers = getattr(settings, 'SQUASH_PARSER', {})
+    namespaces = exp.experimentparameterset_set.all().values_list(
+        'schema__namespace', flat=True)
+    for ns in namespaces:
+        parser = parsers.get(ns)
+        if parser is not None:
+            parse_module = import_module(parser)
+            return parse_module.parse_squashfs_file(exp, squash_sbox,
+                                                    inst, directory,
+                                                    filename, filepath)
+
+    dfos = DataFileObject.objects.filter(datafile__dataset__experiments=exp,
+                                         uri__endswith=filepath)
+    if len(dfos) == 1:
+        return dfos[0]
+
+    default_name = 'squashfs-files'
+    datasets = Dataset.objects.filter(experiments=exp,
+                                      description=default_name,
+                                      storage_boxes=squash_sbox)
+    if datasets.count() >= 1:
+        dataset = datasets[0]
+    else:
+        dataset = Dataset(description=default_name)
+        dataset.save()
+        dataset.experiments.add(exp)
+        dataset.storage_boxes.add(squash_sbox)
+
+    filesize = inst.size(filepath)
+    md5, sha512, size, mimetype_buffer = generate_file_checksums(
+        inst.open(filepath))
+    mimetype = ''
+    if len(mimetype_buffer) > 0:
+        mimetype = Magic(mime=True).from_buffer(mimetype_buffer)
+    df_dict = {'dataset': dataset,
+               'filename': filename,
+               'directory': directory,
+               'size': filesize,
+               'created_time': inst.created_time(filepath),
+               'modification_time': inst.modified_time(filepath),
+               'mimetype': mimetype,
+               'md5sum': md5,
+               'sha512sum': sha512}
+    df = DataFile(**df_dict)
+    df.save()
+    return df
+
+
+def squashfs_match_experiment(exp, squash_sbox):
+    '''matches files already existing in experiment to a squashfs file
+    registered as storage box.  '''
+
+    inst = squash_sbox.get_initialised_storage_instance()
+    for basedir, dirs, files in dj_storage_walk(inst):
+        for filename in files:
+            filepath = os.path.join(basedir, filename)
+            datafile = squashparser(exp, squash_sbox, inst,
+                                    basedir, filename, filepath)
+            new_dfo = DataFileObject(datafile=datafile,
+                                     storage_box=squash_sbox,
+                                     uri=filepath)
+            new_dfo.save()
