ea05363d3fc4bee5dc70c163fda0b22f6f9ec965
James Wettenhall <james.wettenhall@monash.edu>
2014-08-31 21:44:56 +1000

    Fix for Issue #334

--- tardis/tardis_portal/models/datafile.py
+++ tardis/tardis_portal/models/datafile.py
@@ -82,7 +82,7 @@ class DataFile(models.Model):
     class Meta:
         app_label = 'tardis_portal'
         ordering = ['filename']
-        unique_together = ['directory', 'filename', 'version']
+        unique_together = ['dataset', 'directory', 'filename', 'version']
 
     @classmethod
     def sum_sizes(cls, datafiles):
