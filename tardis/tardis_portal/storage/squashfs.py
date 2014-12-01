e347795a51c85cf37d51124345cc9b61616bf461
Grischa Meyer <grischa@gmail.com>
2014-12-01 19:22:11 +1100

    moved all squash parsing to separate app

--- tardis/tardis_portal/storage/squashfs.py
+++ tardis/tardis_portal/storage/squashfs.py
@@ -142,6 +142,25 @@ class SquashFSStorage(Storage):
         else:
             return dfo.uri
 
+    def walk(self, top='.', topdown=True, onerror=None, ignore_dotfiles=True):
+        try:
+            dirnames, filenames = self.listdir(top)
+        except os.error as err:
+            if onerror is not None:
+                onerror(err)
+            return
+        if ignore_dotfiles:
+            dirnames = [d for d in dirnames if not d.startswith('.')]
+            filenames = [f for f in filenames if not f.startswith('.')]
+        if topdown:
+            yield top, dirnames, filenames
+        for dirname in dirnames:
+            new_top = os.path.join(top, dirname)
+            for result in self.walk(new_top):
+                yield result
+        if not topdown:
+            yield top, dirnames, filenames
+
 
 @task
 def parse_new_squashfiles():
@@ -184,127 +203,9 @@ def parse_squashfs_file(squashfs_file_id, parse_module, ns):
     status.save()
     parser = import_module(parse_module)
     try:
-        parser.parse_squashfs_file(squashfile)
+        parser.parse_squashfs_file(squashfile, ns)
         status.string_value = 'complete'
     except:
         status.string_value = 'parse failed'
     finally:
         status.save()
-
-
-def dj_storage_walk(dj_storage, top='.', topdown=True, onerror=None,
-                    ignore_dotfiles=True):
-    try:
-        dirnames, filenames = dj_storage.listdir(top)
-    except os.error as err:
-        if onerror is not None:
-            onerror(err)
-        return
-    if ignore_dotfiles:
-        dirnames = [d for d in dirnames if not d.startswith('.')]
-        filenames = [f for f in filenames if not f.startswith('.')]
-    if topdown:
-        yield top, dirnames, filenames
-    for dirname in dirnames:
-        new_top = os.path.join(top, dirname)
-        for result in dj_storage_walk(dj_storage, new_top):
-            yield result
-    if not topdown:
-        yield top, dirnames, filenames
-
-
-def get_parser_module(exp):
-    parsers = getattr(settings, 'SQUASH_PARSERS', {})
-    namespaces = exp.experimentparameterset_set.all().values_list(
-        'schema__namespace', flat=True)
-    for ns in namespaces:
-        parser = parsers.get(ns)
-        if parser is not None:
-            return import_module(parser)
-    return None
-
-
-def squash_parse_box_data(exp, squash_sbox, inst):
-    parser_module = get_parser_module(exp)
-    if parser_module is not None:
-        return parser_module.parse_squashfs_box_data(exp, squash_sbox, inst)
-
-
-def squash_parse_datafile(exp, squash_sbox, inst,
-                          directory, filename, filepath, box_data):
-    '''
-    return matching or new datafile for given squashfs file path.
-
-    parse discipline specific if set up
-
-    by default use 'squashfs-files' as dataset name
-    '''
-    parser_module = get_parser_module(exp)
-    if parser_module is not None:
-        return parser_module.parse_squashfs_file(exp, squash_sbox,
-                                                 inst, directory,
-                                                 filename, filepath, box_data)
-
-    exp_q = Q(datafile__dataset__experiments=exp)
-    path_part_match_q = Q(uri__endswith=filepath)
-    path_exact_match_q = Q(uri=filepath)
-    s_box_q = Q(storage_box=squash_sbox)
-    # check whether file has been registered alread, stored elsewhere:
-    dfos = DataFileObject.objects.filter(exp_q, path_part_match_q,
-                                         ~s_box_q)
-    if len(dfos) == 1:
-        return dfos[0].datafile
-    # file registered already
-    dfos = DataFileObject.objects.filter(exp_q, path_exact_match_q, s_box_q)
-    if len(dfos) == 1:
-        return dfos[0]
-
-    default_name = 'squashfs-files'
-    datasets = Dataset.objects.filter(experiments=exp,
-                                      description=default_name,
-                                      storage_boxes=squash_sbox)
-    if datasets.count() >= 1:
-        dataset = datasets[0]
-    else:
-        dataset = Dataset(description=default_name)
-        dataset.save()
-        dataset.experiments.add(exp)
-        dataset.storage_boxes.add(squash_sbox)
-
-    filesize = inst.size(filepath)
-    md5, sha512, size, mimetype_buffer = generate_file_checksums(
-        inst.open(filepath))
-    mimetype = ''
-    if len(mimetype_buffer) > 0:
-        mimetype = Magic(mime=True).from_buffer(mimetype_buffer)
-    df_dict = {'dataset': dataset,
-               'filename': filename,
-               'directory': directory,
-               'size': filesize,
-               'created_time': inst.created_time(filepath),
-               'modification_time': inst.modified_time(filepath),
-               'mimetype': mimetype,
-               'md5sum': md5,
-               'sha512sum': sha512}
-    df = DataFile(**df_dict)
-    df.save()
-    return df
-
-
-def squashfs_match_experiment(exp, squash_sbox, ignore_dotfiles=True):
-    '''matches files already existing in experiment to a squashfs file
-    registered as storage box.  '''
-
-    inst = squash_sbox.get_initialised_storage_instance()
-    box_data = squash_parse_box_data(exp, squash_sbox, inst)
-    for basedir, dirs, files in dj_storage_walk(inst):
-        for filename in files:
-            filepath = os.path.join(basedir, filename)
-            parse_result = squash_parse_datafile(exp, squash_sbox, inst,
-                                                 basedir, filename, filepath,
-                                                 box_data)
-            if type(parse_result) == DataFile:
-                new_dfo, created = DataFileObject.objects.get_or_create(
-                    datafile=parse_result,
-                    storage_box=squash_sbox,
-                    uri=filepath)
