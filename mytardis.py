32636372d12f5e2b948a322c17be3e8c2113e073
Grischa Meyer <grischa@gmail.com>
2015-02-09 11:21:27 +1100

    cleanup and robustification

--- mytardis.py
+++ mytardis.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tardis.settings")
+    custom_settings = 'tardis.settings'
+    demo_settings = 'tardis.settings_changeme'
+    if os.path.isfile(custom_settings):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", custom_settings)
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", demo_settings)
+        print('Using demo settings in "tardis/settings_changeme.py",'
+              ' please add your own settings file, '
+              '"tardis/settings.py".')
 
     from django.core.management import execute_from_command_line
 
