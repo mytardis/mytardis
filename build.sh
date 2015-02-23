afb86fe0f522deb06fb447daf042b7e7d9c8974f
Andrew Perry <perry@omgwtfgames.com>
2015-02-24 10:57:14 +1100

    Add OS X dependencies to build instructions & pip requirements list.

--- build.sh
+++ build.sh
@@ -3,16 +3,26 @@
 echo This is a guide only, please either edit or run appropriate commands manually
 exit
 
-# for ubuntu 14.04
+# for Ubuntu 14.04
 # sudo apt-get update
 # sudo apt-get install python-pip git libxml2-dev libxslt1-dev python-dev zlib1g-dev python-wand
 # sudo apt-get install python-virtualenv virtualenvwrapper python-psycopg2 python-yaml ipython
 # sudo apt-get install python-anyjson python-bs4 python-billiard python-feedparser python-html5lib
 # sudo apt-get install python-httplib2 python-pystache python-crypto python-flexmock
 
-. virtualenvwrapper.sh
-# . /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+# for OS X we need these dependencies installed via brew
+# brew install imagemagick --with-libtiff
+# brew install libmagic freetype
+# brew install postgresql
+# or for a local development server, install http://postgresapp.com/
+
+# for Ubuntu 14.04
+# source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+# for OS X
+# source /usr/local/bin/virtualenvwrapper.sh
+
 mkvirtualenv --system-site-packages mytardis
+
 pip install -U pip
 pip install -r requirements.txt
 # for OS X, but might also need some brew requirements.
