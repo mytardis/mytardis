26fa7fca1daf9286c72baf885acf558a7fcf91bc
Grischa Meyer <grischa@gmail.com>
2015-08-04 16:46:58 +1000

    rebase fix

--- tardis/tardis_portal/models/datafile.py
+++ tardis/tardis_portal/models/datafile.py
@@ -81,7 +81,6 @@ class DataFile(models.Model):
                                      storage_box=box)
             newfile.save()
             newfile.file_object = file_object
-            newfile.verify.delay()
         if len(oldobjs) > 0:
             for obj in oldobjs:
                 obj.delete()
