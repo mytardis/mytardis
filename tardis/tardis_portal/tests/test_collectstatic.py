1d339715eb71030e2a5841dfe2717f01f324a44d
Jason Rigby <jason.rigby@monash.edu>
2015-08-21 16:25:20 +1000

    specific settings given for collectstatic command

--- tardis/tardis_portal/tests/test_collectstatic.py
+++ tardis/tardis_portal/tests/test_collectstatic.py
@@ -2,25 +2,29 @@ import shutil
 from os import makedirs, path
 from django.test import TestCase
 from django.conf import settings
-from django.core import management
+from django.contrib.staticfiles.management.commands import collectstatic
 
 
 class CollectstaticTest(TestCase):
 
     def setUp(self):
-        if not path.exists(settings.STATIC_ROOT):
-            makedirs(settings.STATIC_ROOT)
+        self.STATIC_ROOT = path.abspath(path.join(path.dirname(__file__), '..', 'static_test'))
+        if not path.exists(self.STATIC_ROOT):
+            makedirs(self.STATIC_ROOT)
+        self.cmd = collectstatic.Command()
 
     def tearDown(self):
-        if path.exists(settings.STATIC_ROOT):
-            shutil.rmtree(settings.STATIC_ROOT)
+        if path.exists(self.STATIC_ROOT):
+            shutil.rmtree(self.STATIC_ROOT)
 
     def test_collectstatic(self):
-        management.call_command(
-            'collectstatic',
-            '--clear',
-            '--settings',
-            'tardis.test_settings',
-            '-v',
-            '0',
-            interactive=False)
+        with self.settings(STATIC_ROOT=self.STATIC_ROOT, STATICFILES_STORAGE='django.contrib.staticfiles.storage.CachedStaticFilesStorage'):
+            opts = {'interactive': False,
+                    'verbosity': 0,
+                    'link': False,
+                    'clear': True,
+                    'dry_run': False,
+                    'ignore_patterns': [],
+                    'use_default_ignore_patterns': True,
+                    'post_process': True}
+            self.cmd.handle(**opts)
