a1bcd7daa44753794a93a416d9e26bf5036f7add
Nick @ Work <nicholas.may@rmit.edu.au>
2015-10-07 17:54:58 +1100

    Removed directory from datafile unique key. Not allowed in MySQL.

--- tardis/tardis_portal/models/datafile.py
+++ tardis/tardis_portal/models/datafile.py
@@ -157,7 +157,7 @@ class DataFile(models.Model):
     class Meta:
         app_label = 'tardis_portal'
         ordering = ['filename']
-        unique_together = ['dataset', 'directory', 'filename', 'version']
+        unique_together = ['dataset', 'filename', 'version']
 
     @classmethod
     def sum_sizes(cls, datafiles):
