d42612ed9ff62b38790397edc4dbe252af1b4fcf
Grischa Meyer <grischa@gmail.com>
2016-01-15 15:46:36 +1100

    Adding a base.html template for registration and registration setting example

--- tardis/default_settings.py
+++ tardis/default_settings.py
@@ -340,6 +340,9 @@ AUTH_PROFILE_MODULE = 'tardis_portal.UserProfile'
 # New users are added to these groups by default.
 NEW_USER_INITIAL_GROUPS = []
 
+REGISTRATION_OPEN = True
+"""Turn on/off the self-registration link and form"""
+
 ACCOUNT_ACTIVATION_DAYS = 3
 
 AUTHENTICATION_BACKENDS = (
