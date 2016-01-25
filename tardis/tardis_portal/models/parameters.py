abd19fe43c18836253cc0b1501a970d5253a42f2
Andrew Perry <ajperry@pansapiens.com>
2016-01-25 11:32:16 +1100

    Remove index from string_value, create partial index for short strings on Postgres.

--- tardis/tardis_portal/models/parameters.py
+++ tardis/tardis_portal/models/parameters.py
@@ -430,7 +430,8 @@ class ParameterSet(models.Model, ParameterSetManagerMixin):
 
 class Parameter(models.Model):
     name = models.ForeignKey(ParameterName)
-    string_value = models.TextField(null=True, blank=True, db_index=True)
+    # string_value has a custom index created via migrations (for Postgresql)
+    string_value = models.TextField(null=True, blank=True)
     numerical_value = models.FloatField(null=True, blank=True, db_index=True)
     datetime_value = models.DateTimeField(null=True, blank=True, db_index=True)
     link_id = models.PositiveIntegerField(null=True, blank=True)
