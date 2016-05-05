14578093ec3bc2b29863bf76b28c649611e0b380
Nick.May@xTardis <nicholas.may@rmit.edu.au>
2016-05-05 08:30:32 +0000

    refactored models and migrations for MySQL compatibility.

--- tardis/tardis_portal/models/datafile.py
+++ tardis/tardis_portal/models/datafile.py
@@ -51,7 +51,7 @@ class DataFile(models.Model):
 
     dataset = models.ForeignKey(Dataset)
     filename = models.CharField(max_length=400)
-    directory = DirectoryField(blank=True, null=True)
+    directory = DirectoryField(blank=True, null=True, max_length=255)
     size = models.BigIntegerField(blank=True, null=True)
     created_time = models.DateTimeField(null=True, blank=True)
     modification_time = models.DateTimeField(null=True, blank=True)
