ab3e8653211abe1d97d0ec1a8c782b8f73233512
Grischa Meyer <grischa@gmail.com>
2016-06-14 18:02:54 +1000

    separate docker compose file for base and test

--- test.sh
+++ test.sh
@@ -4,19 +4,53 @@ if [ -v EXTRA_REQS ]; then
     pip install $EXTRA_REQS
 fi
 
-# in memory
-python test.py test $TEST_ARGS
-(( exit_status = exit_status || $? ))
+# select test to run with TEST_TYPE, memory pg mysql pylint
+# only memory will include coverage for now
 
-# on postgres
-python test.py test --settings=tardis.test_on_postgresql_settings $TEST_ARGS
-(( exit_status = exit_status || $? ))
+function run_test {
+    if [ "$2" == "coverage" ]; then
+	TEST_ARGS="--with-coverage --cover-package=tardis $TEST_ARGS"
+    fi
+    python test.py test --settings=$1 $TEST_ARGS
+    result=$?
+    if [ "$2" == "coverage" ]; then
+	if [ -n "$PULL_REQUEST_NUMBER" ]; then
+	    export TRAVIS_PULL_REQUEST=$PULL_REQUEST_NUMBER;
+	else export TRAVIS_PULL_REQUEST='false'; fi
+	coveralls
+	coverage xml
+	python-codacy-coverage -r coverage.xml
+    fi
+    return $result
+}
 
-# on mysql
-python test.py test --settings=tardis.test_on_mysql_settings $TEST_ARGS
-(( exit_status = exit_status || $? ))
-
-pylint --rcfile .pylintrc tardis
-(( exit_status = exit_status || $? ))
+case "$TEST_TYPE" in
+    memory)
+	run_test tardis.test_settings coverage
+	(( exit_status = exit_status || $? ))
+    ;;
+    pg)
+	run_test tardis.test_on_postgresql_settings
+	(( exit_status = exit_status || $? ))
+    ;;
+    mysql)
+	run_test tardis.test_on_mysql_settings
+	(( exit_status = exit_status || $? ))
+    ;;
+    pylint)
+	pylint --rcfile .pylintrc tardis
+	(( exit_status = exit_status || $? ))
+    ;;
+    *)
+	run_test tardis.test_settings coverage
+	(( exit_status = exit_status || $? ))
+	run_test tardis.test_on_postgresql_settings
+	(( exit_status = exit_status || $? ))
+	run_test tardis.test_on_mysql_settings
+	(( exit_status = exit_status || $? ))
+	pylint --rcfile .pylintrc tardis
+	(( exit_status = exit_status || $? ))
+    ;;
+esac
 
 exit $exit_status
