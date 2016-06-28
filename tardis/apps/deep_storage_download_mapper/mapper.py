bd28b2e224eeb25eecf80835f24ef944587fdf1d
Grischa Meyer <grischa@gmail.com>
2016-06-28 12:49:00 +1000

    updated test

--- tardis/apps/deep_storage_download_mapper/mapper.py
+++ tardis/apps/deep_storage_download_mapper/mapper.py
@@ -3,12 +3,14 @@ File mapper that works for files stored in deep directory structures.
 It recreates the structure as stored in the datafile directory
 '''
 import os
+from urllib import quote
 
 
 def deep_storage_mapper(datafile, rootdir):
     dataset = datafile.dataset
     exp = dataset.get_first_experiment()
-    filepath = os.path.join(dataset.directory or '', dataset.description,
+    filepath = os.path.join(dataset.directory or '',
+                            quote(dataset.description, safe=''),
                             datafile.directory or '', datafile.filename)
     if rootdir != 'datasets':
         return os.path.join(rootdir, filepath)
