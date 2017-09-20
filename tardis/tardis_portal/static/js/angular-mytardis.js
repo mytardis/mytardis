84c7c0f3b429cf1748fcc462557815fbc0e90d03
James Wettenhall <James.Wettenhall@monash.edu>
2017-09-20 13:41:41 +1000

    Injecting additional dependencies into the AngularJS app

--- tardis/tardis_portal/static/js/angular-mytardis.js
+++ tardis/tardis_portal/static/js/angular-mytardis.js
@@ -1,6 +1,7 @@
 var app = angular.module(
     'MyTardis',
-    ['ngDialog', 'ngResource', 'multipleSelect','ngTagsInput']);
+    ['ngDialog', 'ngResource', 'multipleSelect', 'ngTagsInput',
+     'ngMaterial', 'md.data.table']);
 
 app.config(function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
