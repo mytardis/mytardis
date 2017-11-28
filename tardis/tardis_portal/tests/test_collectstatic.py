b6d5a5e6f5b4c3baeade5dbbf81c0620b9ab29da
James Wettenhall <James.Wettenhall@monash.edu>
2017-11-28 11:04:07 +1100

    Angular dependencies are no longer copied into MyTardis repository. Now they are installed with "npm install" before running collectstatic.

--- tardis/tardis_portal/tests/test_collectstatic.py
+++ tardis/tardis_portal/tests/test_collectstatic.py
@@ -1,4 +1,5 @@
 import shutil
+from os import mkdir
 from os import path
 from tempfile import mkdtemp
 from django.test import TestCase
@@ -10,14 +11,22 @@ class CollectstaticTest(TestCase):
 
     def setUp(self):
         self.STATIC_ROOT = mkdtemp()
+        self.NPM_ROOT_PATH = mkdtemp()
         self.cmd = collectstatic.Command()
+
+        # Required when using django-npm's 'npm.finders.NpmFinder'
+        # in settings.STATICFILES_FINDERS:
+        mkdir(path.join(self.NPM_ROOT_PATH, 'node_modules'))
 
     def tearDown(self):
         if path.exists(self.STATIC_ROOT):
             shutil.rmtree(self.STATIC_ROOT)
 
     def test_collectstatic(self):
-        with self.settings(STATIC_ROOT=self.STATIC_ROOT, STATICFILES_STORAGE='django.contrib.staticfiles.storage.CachedStaticFilesStorage'):
+        with self.settings(
+                STATIC_ROOT=self.STATIC_ROOT,
+                STATICFILES_STORAGE='django.contrib.staticfiles.storage.CachedStaticFilesStorage',
+                NPM_ROOT_PATH=self.NPM_ROOT_PATH):
             opts = {'interactive': False,
                     'verbosity': 0,
                     'link': False,
