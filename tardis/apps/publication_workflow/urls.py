3a7e1d17df4d31b26d459e18227752346f951bec
James Wettenhall <James.Wettenhall@monash.edu>
2018-01-17 11:04:39 +1100

    The publication_workflow app replaces the publication_forms app.

--- tardis/apps/publication_workflow/urls.py
+++ tardis/apps/publication_workflow/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, include, url
 
 from .pub_form_config import PubFormConfig
 
@@ -7,9 +7,27 @@ urlpatterns = patterns(
     (r'^form/$', 'tardis.apps.publication_workflow.views.index'),
     (r'^data/fetch_experiments_and_datasets/$',
      'tardis.apps.publication_workflow.views.fetch_experiments_and_datasets'),
-    (r'^helper/pdb/(?P<pdb_id>.*)/$',
-     'tardis.apps.publication_workflow.views.pdb_helper'),
     (r'^approvals/$', 'tardis.apps.publication_workflow.views.approval_view'),
+    (r'^my_publications/$',
+     'tardis.apps.publication_workflow.views.my_publications'),
+    (r'^draft_pubs_list/$',
+     'tardis.apps.publication_workflow.views.retrieve_draft_pubs_list'),
+    (r'^scheduled_pubs_list/$',
+     'tardis.apps.publication_workflow.views.retrieve_scheduled_pubs_list'),
+    (r'^released_pubs_list/$',
+     'tardis.apps.publication_workflow.views.retrieve_released_pubs_list'),
+    (r'^tokens/(?P<experiment_id>\d+)/$',
+     'tardis.apps.publication_workflow.views.tokens'),
+    (r'^tokens_json/(?P<experiment_id>\d+)/$',
+     'tardis.apps.publication_workflow.views.retrieve_access_list_tokens_json'),
+    (r'^experiment/(?P<experiment_id>\d+)/is_publication/$',
+     'tardis.apps.publication_workflow.views.is_publication'),
+    (r'^experiment/(?P<experiment_id>\d+)/is_publication_draft/$',
+     'tardis.apps.publication_workflow.views.is_publication_draft'),
+    (r'^publication/delete/(?P<experiment_id>\d+)/$',
+     'tardis.apps.publication_workflow.views.delete_publication'),
+    (r'^publication/mint_doi/(?P<experiment_id>\d+)/$',
+      'tardis.apps.publication_workflow.views.mint_doi_and_deactivate')
 )
 
 # one-time settings check
--- tardis/tardis_portal/static/js/angular-mytardis.js
+++ tardis/tardis_portal/static/js/angular-mytardis.js
@@ -1,4 +1,6 @@
-var app = angular.module('MyTardis', ['ngDialog', 'ngResource']);
+var app = angular.module(
+    'MyTardis',
+    ['ngDialog', 'ngResource', 'ngMaterial', 'md.data.table']);
 
 app.config(function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
