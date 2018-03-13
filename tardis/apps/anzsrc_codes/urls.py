e9be588b9b6f3385a14b36dda0a3d6bdc189bbba
James Wettenhall <James.Wettenhall@monash.edu>
2018-03-13 23:57:23 +1100

    URLs used in reverse lookups require a named URL pattern which may or may not look like a fully-qualified Python method name.

--- tardis/apps/anzsrc_codes/urls.py
+++ tardis/apps/anzsrc_codes/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('tardis.apps.anzsrc_codes.views',
-    url(r'^(?P<experiment_id>\d+)/$', 'index'),
-    url(r'^experiment/(?P<experiment_id>\d+)/for-code/$',
-        'list_or_create_for_code'),
-    url(r'^experiment/(?P<experiment_id>\d+)/for-code/(?P<ps_id>\d+)$',
-        'get_or_update_or_delete_for_code'),
+from .views import index
+from .views import list_or_create_for_code
+from .views import get_or_update_or_delete_for_code
 
-)
+
+urlpatterns = [
+    url(r'^(?P<experiment_id>\d+)/$', index,
+        name='tardis.apps.anzsrc_codes.views.index'),
+    url(r'^experiment/(?P<experiment_id>\d+)/for-code/$',
+        list_or_create_for_code,
+        name='tardis.apps.anzsrc_codes.views.list_or_create_for_code'),
+    url(r'^experiment/(?P<experiment_id>\d+)/for-code/(?P<ps_id>\d+)$',
+        get_or_update_or_delete_for_code,
+        name='tardis.apps.anzsrc_codes.views.get_or_update_or_delete_for_code')
+]
--- tardis/apps/related_info/urls.py
+++ tardis/apps/related_info/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('tardis.apps.related_info.views',
-    url(r'^(?P<experiment_id>\d+)/$', 'index'),
+from .views import index
+from .views import list_or_create_related_info
+from .views import get_or_update_or_delete_related_info
+
+
+urlpatterns = [
+    url(r'^(?P<experiment_id>\d+)/$', index,
+        name='tardis.apps.related_info.views.index'),
     url(r'^experiment/(?P<experiment_id>\d+)/related-info/$',
-        'list_or_create_related_info'),
+        list_or_create_related_info,
+        name='tardis.apps.related_info.views.list_or_create_related_info'),
     url(r'^experiment/(?P<experiment_id>\d+)/related-info/(?P<ps_id>\d+)$',
-        'get_or_update_or_delete_related_info'),
-)
+        get_or_update_or_delete_related_info,
+        name='tardis.apps.related_info.views.get_or_update_or_delete_related_info'),
+]
