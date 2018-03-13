9a1c2df13fe2eaf007a42d99736d73b833ef0836
James Wettenhall <James.Wettenhall@monash.edu>
2018-03-13 23:57:23 +1100

    URLs used in reverse lookups require a named URL pattern which may or may not look like a fully-qualified Python method name.

--- tardis/search/urls.py
+++ tardis/search/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import url
 
-from tardis.search.views import SingleSearchView, retrieve_field_list, \
+from tardis.search.views import (
+    SingleSearchView,
+    retrieve_field_list,
+    search_datafile,
     search_experiment
+)
 
 urlpatterns = [
     url(r'^/?$', SingleSearchView.as_view(), name='haystack_search'),
     url(r'^parameter_field_list/$', retrieve_field_list,
         name='search_retrieve_field_list'),
-    url(r'^datafile/$', 'tardis.search.views.search_datafile'),
+    url(r'^datafile/$', search_datafile),
     url(r'^experiment/$', search_experiment, name='search_experiment'),
 
 ]
