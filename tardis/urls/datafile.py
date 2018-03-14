f78ed8a7e8465c7a88823b2af87894ff05ab3f21
James Wettenhall <James.Wettenhall@monash.edu>
2018-03-14 23:10:06 +1100

    Fixing some URL names for reverse lookups

--- tardis/urls/datafile.py
+++ tardis/urls/datafile.py
@@ -12,16 +12,18 @@ from tardis.tardis_portal.download import view_datafile
 iiif_urls = [
     url(r'^(?P<datafile_id>\d+)/(?P<region>[^\/]+)/(?P<size>[^\/]+)/'
         r'(?P<rotation>[\d\.]+)/(?P<quality>\w+)$',
-        download_image),
+        download_image,
+        name='tardis.tardis_portal.iiif.download_image'),
     url(r'^(?P<datafile_id>\d+)/(?P<region>[^\/]+)/(?P<size>[^\/]+)/'
         r'(?P<rotation>[\d\.]+)/(?P<quality>\w+).(?P<format>\w+)$',
-        download_image),
+        download_image,
+        name='tardis.tardis_portal.iiif.download_image'),
     url(r'^(?P<datafile_id>\d+)/info.(?P<format>\w+)$', download_info),
 ]
 
 datafile_urls = [
     url(r'^view/(?P<datafile_id>\d+)/$',
         view_datafile,
-        name='view_datafile'),
+        name='tardis.tardis_portal.download.view_datafile'),
     url(r'^iiif/', include(iiif_urls)),
 ]
