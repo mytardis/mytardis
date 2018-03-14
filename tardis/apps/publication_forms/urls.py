dbc1316ceb6662d37ec7572408dfea6386acc904
James Wettenhall <James.Wettenhall@monash.edu>
2018-03-14 12:20:36 +1100

    django.conf.urls.patterns() is deprecated

--- tardis/apps/publication_forms/urls.py
+++ tardis/apps/publication_forms/urls.py
@@ -1,16 +1,24 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 
 from .pub_form_config import PubFormConfig
 
-urlpatterns = patterns(
-    '',
-    (r'^form/$', 'tardis.apps.publication_forms.views.index'),
-    (r'^data/fetch_experiments_and_datasets/$',
-     'tardis.apps.publication_forms.views.fetch_experiments_and_datasets'),
-    (r'^helper/pdb/(?P<pdb_id>.*)/$',
-     'tardis.apps.publication_forms.views.pdb_helper'),
-    (r'^approvals/$', 'tardis.apps.publication_forms.views.approval_view'),
+from .views import (
+    index,
+    fetch_experiments_and_datasets,
+    pdb_helper,
+    approval_view
 )
+
+urlpatterns = [
+    url(r'^form/$', index, name='tardis.apps.publication_forms.views.index'),
+    url(r'^data/fetch_experiments_and_datasets/$',
+        fetch_experiments_and_datasets,
+        name='tardis.apps.publication_forms.views.fetch_experiments_and_datasets'),
+    url(r'^helper/pdb/(?P<pdb_id>.*)/$',
+        pdb_helper, name='tardis.apps.publication_forms.views.pdb_helper'),
+    url(r'^approvals/$', approval_view,
+        name='tardis.apps.publication_forms.views.approval_view'),
+]
 
 # one-time settings check
 PubFormConfig().do_setup()
