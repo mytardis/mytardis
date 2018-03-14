01535f525a1d0ccd16eee5643e0374c3edeefd0b
James Wettenhall <James.Wettenhall@monash.edu>
2018-03-14 16:53:24 +1100

    Split up tardis/urls.py

---
+++ tardis/urls/accounts.py
@@ -0,0 +1,22 @@
+'''
+Accounts URLs
+'''
+from django.conf.urls import include, url
+from registration.backends.default.views import RegistrationView
+
+from tardis.tardis_portal.views import (
+    login,
+    manage_user_account,
+    manage_auth_methods
+)
+
+accounts_urls = [
+    url(r'^login/$', login, name='tardis.tardis_portal.views.login'),
+    url(r'^manage$', manage_user_account,
+        name='tardis.tardis_portal.views.manage_user_account'),
+    url(r'^manage_auth_methods/$', manage_auth_methods,
+        name='tardis.tardis_portal.views.manage_auth_methods'),
+    url(r'^register/$', RegistrationView.as_view(),  # pylint: disable=E1120
+        name='tardis.tardis_portal.views.register'),
+    url(r'', include('registration.backends.default.urls')),
+]
---
+++ tardis/urls/datafile.py
@@ -0,0 +1,27 @@
+'''
+DataFile URLs
+'''
+from django.conf.urls import include, url
+
+from tardis.tardis_portal.iiif import (
+    download_image,
+    download_info
+)
+from tardis.tardis_portal.download import view_datafile
+
+iiif_urls = [
+    url(r'^(?P<datafile_id>\d+)/(?P<region>[^\/]+)/(?P<size>[^\/]+)/'
+        r'(?P<rotation>[\d\.]+)/(?P<quality>\w+)$',
+        download_image),
+    url(r'^(?P<datafile_id>\d+)/(?P<region>[^\/]+)/(?P<size>[^\/]+)/'
+        r'(?P<rotation>[\d\.]+)/(?P<quality>\w+).(?P<format>\w+)$',
+        download_image),
+    url(r'^(?P<datafile_id>\d+)/info.(?P<format>\w+)$', download_info),
+]
+
+datafile_urls = [
+    url(r'^view/(?P<datafile_id>\d+)/$',
+        view_datafile,
+        name='view_datafile'),
+    url(r'^iiif/', include(iiif_urls)),
+]
