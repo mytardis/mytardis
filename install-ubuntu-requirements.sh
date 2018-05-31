1878cc1c2f44b1346aa32d855485e37989b1c5b6
James Wettenhall <james.wettenhall@monash.edu>
2018-05-31 11:56:53 +1000

    Python's Wand module requires the MagickWand shared library

--- install-ubuntu-requirements.sh
+++ install-ubuntu-requirements.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install git ipython libldap2-dev libsasl2-dev \
+apt-get install git ipython libldap2-dev libmagickwand-dev libsasl2-dev \
   libssl-dev libxml2-dev libxslt1-dev nodejs npm \
   python-dev python-pip python-virtualenv virtualenvwrapper \
   zlib1g-dev libfreetype6-dev libjpeg-dev
