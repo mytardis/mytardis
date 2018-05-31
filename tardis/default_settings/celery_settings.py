e41c02b9b9aa33e981b2e6413096f48ae55c0fc2
James Wettenhall <james.wettenhall@monash.edu>
2018-05-31 16:12:08 +1000

    Celery task decorators should use @celery_app.task instead of @task

--- tardis/default_settings/celery_settings.py
+++ tardis/default_settings/celery_settings.py
@@ -19,3 +19,5 @@ CELERYBEAT_SCHEDULE = {
 celery_app = Celery('tardis_portal')
 celery_app.config_from_object('django.conf:settings')
 celery_app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
+
+CELERY_IMPORTS = ('tardis.tardis_portal.tasks',)
