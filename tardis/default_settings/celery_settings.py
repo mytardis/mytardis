018ccbc15e39f94bbfb7c10bec9b16384800e2b5
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-01 23:49:29 +1000

    Removing old publication forms app from Django 1.11 branch, as it has poor test coverage and will soon be replace by the new publication workflow app.

--- tardis/default_settings/celery_settings.py
+++ tardis/default_settings/celery_settings.py
@@ -6,11 +6,6 @@ CELERYBEAT_SCHEDULE = {
         "task": "tardis_portal.verify_dfos",
         "schedule": timedelta(seconds=300)
     },
-    # enable this task for the publication workflow
-    # "update-publication-records": {
-    #     "task": "apps.publication_forms.update_publication_records",
-    #     "schedule": timedelta(seconds=300)
-    # },
 }
 
 CELERY_IMPORTS = ('tardis.tardis_portal.tasks',)
