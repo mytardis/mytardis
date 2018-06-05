ef4389d6618074d86a0797d15bb1e9995a839970
Keith Schulze <keith.schulze@gmail.com>
2018-06-05 12:48:36 +1000

    Switch to installing `psycopg2-binary` via pip
    Removes dependency on system package for `psycopg2` for Ubuntu

--- install-ubuntu-requirements.sh
+++ install-ubuntu-requirements.sh
@@ -3,6 +3,6 @@ apt-get install git ipython libldap2-dev libsasl2-dev \
   libssl-dev libxml2-dev libxslt1-dev nodejs npm python-anyjson \
   python-billiard python-bs4 python-crypto python-dateutil \
   python-dev python-flexmock \
-  python-pip python-psycopg2 python-pystache \
+  python-pip python-pystache \
   python-virtualenv python-wand python-yaml virtualenvwrapper \
   zlib1g-dev libfreetype6-dev libjpeg-dev
