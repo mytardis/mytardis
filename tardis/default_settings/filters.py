140396652856f9fc8286ab4cd71b74d69c2dde60
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-08 22:47:56 +1000

    Removing protein-crystallography-specific DataFile metadata searching (for MX and SAXS beamlines).  It needs to be implemented in a more generic way or moved to an optionally installed app.
    Reverting switch from django-form-utils to django-betterforms.
    Now using a Django1.11 compatible fork of django-form-utils.

--- tardis/default_settings/filters.py
+++ tardis/default_settings/filters.py
@@ -4,10 +4,3 @@
 #     ["EXIF", "http://exif.schema"]),  # this filter requires pyexiv2
 #                                       # http://tilloy.net/dev/pyexiv2/
 #    ]
-
-# Post Save Filters
-# POST_SAVE_FILTERS = [
-#    ("tardis.tardis_portal.filters.diffractionimage.make_filter",
-#     ["DIFFRACTION", "http://www.tardis.edu.au/schemas/trdDatafile/1",
-#      "/Users/steve/Desktop/diffdump"]),  #  requires ccp4 diffdump binary
-#    ]
