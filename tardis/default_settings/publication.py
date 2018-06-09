44f3e0b90ea76ac08d19bdd23615f8f9db97a3a6
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-09 20:44:06 +1000

    This old DOI minting mechanism used an ANDS service which is no longer available.  DOI minting should now be implemented using an up-to-date DOI minting web service, such as https://mod.monash.edu/ (for real production DOIs) or https://vera186.its.monash.edu/modc/ (for test DOIs). The following Pull Request provides a way for MyTardis to use these newer DOI minting services: https://github.com/mytardis/mytardis/pull/982

--- tardis/default_settings/publication.py
+++ tardis/default_settings/publication.py
@@ -16,17 +16,6 @@ RELATED_INFO_SCHEMA_NAMESPACE = \
 RELATED_OTHER_INFO_SCHEMA_NAMESPACE = \
     'http://www.tardis.edu.au/schemas/experiment/annotation/2011/07/07'
 
-DOI_ENABLE = False
-DOI_XML_PROVIDER = 'tardis.tardis_portal.ands_doi.DOIXMLProvider'
-# DOI_TEMPLATE_DIR = path.join(
-#    TARDIS_DIR, 'tardis_portal/templates/tardis_portal/doi/')
-DOI_TEMPLATE_DIR = path.join('tardis_portal/doi/')
-DOI_APP_ID = ''
-DOI_NAMESPACE = 'http://www.tardis.edu.au/schemas/doi/2011/12/07'
-DOI_MINT_URL = 'https://services.ands.org.au/home/dois/doi_mint.php'
-DOI_RELATED_INFO_ENABLE = False
-DOI_BASE_URL = 'http://mytardis.example.com'
-
 OAIPMH_PROVIDERS = [
     'tardis.apps.oaipmh.provider.experiment.DcExperimentProvider',
     'tardis.apps.oaipmh.provider.experiment.RifCsExperimentProvider',
