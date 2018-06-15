2dc69d63abeb9928e8b980b1b3a5a73d9d67568e
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-15 15:04:33 +1000

    Getting rid of apt-get installed Python dependencies

--- install-ubuntu-requirements.sh
+++ install-ubuntu-requirements.sh
@@ -1,8 +1,5 @@
 apt-get update
 apt-get install git ipython libldap2-dev libsasl2-dev \
-  libssl-dev libxml2-dev libxslt1-dev nodejs npm python-anyjson \
-  python-billiard python-bs4 python-crypto python-dateutil \
-  python-dev python-flexmock \
-  python-pip python-pystache \
-  python-virtualenv python-wand python-yaml virtualenvwrapper \
-  zlib1g-dev libfreetype6-dev libjpeg-dev
+  libssl-dev libxml2-dev libxslt1-dev nodejs npm \
+  python-dev python-pip python-virtualenv virtualenvwrapper \
+  zlib1g-dev libfreetype6-dev libjpeg-dev libmagickwand-dev
