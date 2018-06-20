f61b03b962f009832be5066c1e3e1c8af7f38acd
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-20 14:01:08 +1000

    Removing RemovedInMyTardis310Warning, because there might not be in MyTardis v3.10

--- tardis/tardis_portal/deprecations.py
+++ tardis/tardis_portal/deprecations.py
@@ -4,10 +4,9 @@ Deprecation warnings
 import warnings
 
 warnings.simplefilter('always', category=DeprecationWarning)
-warnings.simplefilter('always', category=PendingDeprecationWarning)
 
 
-class RemovedInMyTardis40Warning(PendingDeprecationWarning):
+class RemovedInMyTardis40Warning(DeprecationWarning):
     '''
     Used to raise warnings about deprecated functionality.
 
@@ -19,23 +18,6 @@ class RemovedInMyTardis40Warning(PendingDeprecationWarning):
         "This method will be removed in MyTardis 4.0. "
         "Please use method2 instead.",
         RemovedInMyTardis40Warning
-    )
-    '''
-    pass
-
-
-class RemovedInMyTardis310Warning(DeprecationWarning):
-    '''
-    Used to raise warnings about deprecated functionality.
-
-    Usage:
-
-    import warnings
-
-    warnings.warn(
-        "This method will be removed in MyTardis 3.10. "
-        "Please use method2 instead.",
-        RemovedInMyTardis310Warning
     )
     '''
     pass
