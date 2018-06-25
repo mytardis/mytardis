8d8cdee1d3b9bf1620299e6f670cc0547dc482ce
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-25 12:34:03 +1000

    Fixed some style issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
-            all: ['js_tests/tests.html']
+            all: ["js_tests/tests.html"]
         },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.registerTask('test', ['qunit']);
-    grunt.registerTask('default', ['test']);
+    grunt.loadNpmTasks("grunt-contrib-qunit");
+    grunt.registerTask("test", ["qunit"]);
+    grunt.registerTask("default", ["test"]);
 };
