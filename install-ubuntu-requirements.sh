2d459641b9c581cb16b6c17fd0731ec0b80925dc
James Wettenhall <James.Wettenhall@monash.edu>
2018-06-25 23:38:05 +1000

    Preparing for switch from Ubuntu 14.04 to Ubuntu 16.04

--- install-ubuntu-requirements.sh
+++ install-ubuntu-requirements.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install git ipython libldap2-dev libmagickwand-dev libsasl2-dev \
-  libssl-dev libxml2-dev libxslt1-dev nodejs npm \
+  libssl-dev libxml2-dev libxslt1-dev libmagic-dev nodejs npm \
   python-dev python-pip python-virtualenv virtualenvwrapper \
   zlib1g-dev libfreetype6-dev libjpeg-dev
