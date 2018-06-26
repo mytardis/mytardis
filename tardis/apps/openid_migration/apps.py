89312a264faa7dee1e2cda35bd2533b31b969311
manishkumr <rishimanish123@gmail.com>
2018-06-26 15:40:37 +1000

    adding app for openid migration

---
+++ tardis/apps/openid_migration/apps.py
@@ -0,0 +1,6 @@
+from tardis.app_config import AbstractTardisAppConfig
+
+
+class OpenidMigrationConfig(AbstractTardisAppConfig):
+    name = 'tardis.apps.openid_migration'
+    verbose_name = 'OpenID migrations'
