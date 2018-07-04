2aedfe8aa1d195abedbf1aee0d5711989cf8bfdc
manishkumr <rishimanish123@gmail.com>
2018-07-04 11:13:18 +1000

    moving migration related code to openid migration app

--- tardis/apps/openid_migration/views.py
+++ tardis/apps/openid_migration/views.py
@@ -1,6 +1,19 @@
-def migrate_accounts(request):
-    from tardis.tardis_portal.auth.authentication import add_auth_method, \
-        merge_auth_method, remove_auth_method, edit_auth_method, \
-        list_auth_methods
+from .migration import do_migration, add_auth_method
 
+
+def migrate_accounts(request):
+    """
+
+    :param request:
+    :return:
+    """
+    from .migration import list_auth_methods
+
+    if request.method == 'POST':
+        operation = request.POST['operation']
+        if operation == 'addAuth':
+            return add_auth_method(request)
+        elif operation == 'mergeAuth':
+            return do_migration(request)
+    # if GET, we'll just give the initial list of auth methods for the user
     return list_auth_methods(request)
