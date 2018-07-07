2ce56e07b75a7d9d2250eb8a9ef97cb4ee6e9e10
James Wettenhall <James.Wettenhall@monash.edu>
2018-07-07 11:20:26 +1000

    Using newer npm version for CI builds, ensuring that npm test result is reflected in CI build status

--- test.sh
+++ test.sh
@@ -42,8 +42,8 @@ case "$TEST_TYPE" in
 	(( exit_status = exit_status || $? ))
     ;;
     behave)
-    npm install && npm test
-    python manage.py behave --settings=tardis.test_settings
+        npm install && npm install phantomjs-prebuilt && npm test \
+        && python manage.py behave --settings=tardis.test_settings
 	(( exit_status = exit_status || $? ))
     ;;
     *)
