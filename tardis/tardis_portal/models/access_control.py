920885a051615603a899faf4e1e5e60ee757513e
manishkumr <rishimanish123@gmail.com>
2018-07-09 15:59:10 +1000

    adding approved field to user authentication model

--- tardis/tardis_portal/models/access_control.py
+++ tardis/tardis_portal/models/access_control.py
@@ -99,6 +99,7 @@ class UserAuthentication(models.Model):
     userProfile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     authenticationMethod = models.CharField(max_length=30, choices=CHOICES)
+    approved = models.BooleanField(default=True)
 
     class Meta:
         app_label = 'tardis_portal'
