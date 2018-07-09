c489751d3a4a805e5fff3b194192cb47d669b342
manishkumr <rishimanish123@gmail.com>
2018-07-09 15:59:10 +1000

    adding approved field to user authentication model

--- tardis/apps/openid_migration/utils.py
+++ tardis/apps/openid_migration/utils.py
@@ -9,23 +9,23 @@ def rollback_migration(user_migration_obj):
 
     # get object ACls updated by this migration
     migrated_acls_records = user_migration_obj.openidaclmigration_set.all()
-    #for each migrated ACLs revert it back to old user and delete acl migration record
+    # for each migrated ACLs revert it back to old user and delete acl migration record
 
     for migrated_acls_record in migrated_acls_records:
-        #get migrated ACL
+        # get migrated ACL
         acl = migrated_acls_record.acl_id
         acl.entityId = old_user.id
         acl.save()
         migrated_acls_record.delete()
 
-    #set new user to inactive and old user to active
+    # set new user to inactive and old user to active
     new_user.is_active = False
     old_user.is_active = True
-    #set user_migration status to false
+    # set user_migration status to false
     user_migration_obj.migration_status = False
     user_migration_obj.save()
 
-    #roll back group changes
+    # roll back group changes
     groups = Group.objects.filter(user=new_user)
     for group in groups:
         old_user.groups.add(group)
