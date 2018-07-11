e4e4eebc54082d0d5d6d718d6993f85f0db24a03
manishkumr <rishimanish123@gmail.com>
2018-07-11 12:12:25 +1000

    Added test case for model

---
+++ tardis/apps/openid_migration/tests/test_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from ..models import  OpenidUserMigration
+from django.contrib.auth.models import User
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.old_user = User.objects.create_user('abc',
+                                            email='abc@example.com',
+                                            password='abc')
+        self.new_user = User.objects.create_user('def',
+                                            email='def@example.com',
+                                            password='def')
+
+    def create_OpenidUserMigration(self):
+
+        return OpenidUserMigration.objects.create(old_user=self.old_user,
+                                                  new_user=self.new_user,
+                                                  old_user_auth_method='ldap',
+                                                  new_user_auth_method='AAF')
+
+    def test_OpenidUserMigration_creation(self):
+        openidUserMigration_obj = self.create_OpenidUserMigration()
+        self.assertTrue(isinstance(openidUserMigration_obj, OpenidUserMigration))
+        self.assertEqual(openidUserMigration_obj.__unicode__(), 'abc | def')
