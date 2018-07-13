ff045f5d26f358f7b81836bbab1efc9d2d4d38dc
manishkumr <rishimanish123@gmail.com>
2018-07-13 16:49:37 +1000

    Show message if user is not aaproved by admin

---
+++ tardis/tardis_portal/templatetags/approved_user_tags.py
@@ -0,0 +1,22 @@
+from django import template
+from tardis.tardis_portal.models import User,UserAuthentication
+
+register = template.Library()
+
+
+@register.filter
+def check_if_user_not_approved(request):
+    """
+    Custom template filter to identify whether a user is a
+    facility manager.
+    """
+    if (request.user.is_authenticated()):
+        user = request.user
+        try:
+            user_auth = UserAuthentication.objects.get(username=user.username, authenticationMethod='Google')
+            if not user_auth.approved:
+                return True
+        except UserAuthentication.DoesNotExist:
+            return False
+
+    return False
