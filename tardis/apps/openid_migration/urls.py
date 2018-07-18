f3166217388466f0959042f6e32dba4df231b3d3
manishkumr <rishimanish123@gmail.com>
2018-07-19 07:06:35 +1000

    fixing urls to make it compatible with Django 1.11

--- tardis/apps/openid_migration/urls.py
+++ tardis/apps/openid_migration/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^migrate-accounts/$', views.migrate_accounts),
+    url(r'^migrate-accounts/$', views.migrate_accounts,
+        name='tardis.apps.openid_migration.views.migrate_accounts'),
 ]
