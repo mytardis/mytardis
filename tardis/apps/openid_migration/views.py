7ee23443321b3d981f5fa977d5492da42583f0c9
manishkumr <rishimanish123@gmail.com>
2018-07-18 10:38:06 +1000

    pylint fixes

--- tardis/apps/openid_migration/views.py
+++ tardis/apps/openid_migration/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.decorators import login_required, \
+    permission_required
+
 from .migration import do_migration, \
     openid_migration_method, \
     confirm_migration
 
 
+@permission_required('tardis_portal.change_userauthentication')
+@login_required()
 def migrate_accounts(request):
-    """
-    :param request:
-    :return:
-    """
+    '''
+    Manage user migration using AJAX.
+    '''
 
     if request.method == 'POST':
         operation = request.POST['operation']
@@ -16,5 +20,3 @@ def migrate_accounts(request):
         if operation == 'migrateAccount':
             return do_migration(request)
     return openid_migration_method(request)
-
-
