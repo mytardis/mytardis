1e9f41787ef8fedd5d40f084067fa4691f7d23c4
manishkumr <rishimanish123@gmail.com>
2018-07-31 07:32:33 +1000

    some more code refractoring to move social auth into separate app

---
+++ tardis/apps/openid_migration/context_processor.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+
+def openid_migration_processor(request):
+    """
+    adds context for openid_migration
+    """
+    def is_openid_migration_enabled():
+        try:
+            if 'tardis.apps.openid_migration' in settings.INSTALLED_APPS:
+                return getattr(settings, 'OPENID_MIGRATION_ENABLED', True)
+        except AttributeError:
+            pass
+        return False
+    return {'openid_migration_enabled': is_openid_migration_enabled()}
