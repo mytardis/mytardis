a810c50f5bcb418c2f4f502c7b44fe3140541fe3
James Wettenhall <James.Wettenhall@monash.edu>
2018-08-20 22:55:50 +1000

    Trying to prevent username and password inputs from bleeding out past the right-hand border of the Verify My Account login box and trying to make it look more like a standard bootstrap form (with button in a control-group).

--- tardis/apps/openid_migration/forms.py
+++ tardis/apps/openid_migration/forms.py
@@ -1,3 +1,5 @@
+import six
+
 from django import forms
 
 
@@ -12,6 +14,9 @@ def openid_user_migration_form():
     fields['password'] = forms.CharField(
         required=True, widget=forms.PasswordInput(),
         label='Password')
+
+    for _, field in six.iteritems(fields):
+        field.widget.attrs['style'] = "width: 80%; max-width: 150px;"
 
     return type('openid_user_migration_form', (forms.BaseForm, ),
                 {'base_fields': fields})
