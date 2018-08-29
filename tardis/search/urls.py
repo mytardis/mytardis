9bfcd1b6fa7d97868ed993e84d0864b6e514ea32
James Wettenhall <James.Wettenhall@monash.edu>
2018-08-29 22:22:37 +1000

    Search improvements

--- tardis/search/urls.py
+++ tardis/search/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from tardis.search.views import (
-    SingleSearchView,
     retrieve_field_list,
     search_experiment
 )
 
+# from tardis.search.views import SingleSearchView,
+
 urlpatterns = [
-    url(r'^\?$', SingleSearchView.as_view(), name='haystack_search'),
+    # url(r'^\?$', SingleSearchView.as_view(), name='haystack_search'),
+    url(r'', include('haystack.urls')),
     url(r'^parameter_field_list/$', retrieve_field_list,
         name='search_retrieve_field_list'),
     url(r'^experiment/$', search_experiment,
