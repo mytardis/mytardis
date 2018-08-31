9236a12521c56476e2a1a98c0c819d4eeac39430
manishkumr <rishimanish123@gmail.com>
2018-08-31 15:46:43 +1000

    adding migration menu at the top of menu items

--- tardis/apps/openid_migration/user_menu_modifiers.py
+++ tardis/apps/openid_migration/user_menu_modifiers.py
@@ -16,15 +16,8 @@ def add_migrate_account_menu_item(request, user_menu):
 
     migrate_menu_item = dict(
         url=reverse('tardis.apps.openid_migration.views.migrate_accounts'),
-        icon='fa fa-tags',
+        icon='fa fa-user',
         label='Migrate My Account'
     )
-    item_index = -1
-    for menu_item in user_menu:
-        if 'label' in menu_item and menu_item['label'] == "Link Accounts":
-            item_index = user_menu.index(menu_item)
-            menu_item.update(migrate_menu_item)
-    if item_index == -1:
-        item_index = len(user_menu) - 1
-        user_menu.insert(item_index, migrate_menu_item)
+    user_menu.insert(0, migrate_menu_item)
     return user_menu
