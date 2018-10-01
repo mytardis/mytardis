517afffa75d661f2a03b54db432c5794c546e46f
James Wettenhall <James.Wettenhall@monash.edu>
2018-10-01 13:23:44 +1000

    Changed default BROKER_URL to use RabbitMQ, because in-memory broker only works for a single process (now that we're not using django-celery).
    Removed CELERY_IMPORTS because this is handled by autodiscover_tasks
    in tardis/celery.py
    
    Moved CELERYBEAT_SCHEDULE default configuration into tardis/celery.py
    
    Added some notes on installing ImageMagick and optionally RabbitMQ via
    Homebrew

--- tardis/celery.py
+++ tardis/celery.py
@@ -1,7 +1,15 @@
 from __future__ import absolute_import
+from datetime import timedelta
 from celery import Celery  # pylint: disable=import-error
 from django.apps import apps  # pylint: disable=wrong-import-order
 
 tardis_app = Celery('tardis')
 tardis_app.config_from_object('django.conf:settings')
 tardis_app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
+
+tardis_app.conf.CELERYBEAT_SCHEDULE = {
+    "verify-files": {
+        "task": "tardis_portal.verify_dfos",
+        "schedule": timedelta(seconds=300)
+    },
+}
--- tardis/default_settings/celery_settings.py
+++ tardis/default_settings/celery_settings.py
@@ -1,18 +1,8 @@
 # Celery queue
-from datetime import timedelta
 
-CELERYBEAT_SCHEDULE = {
-    "verify-files": {
-        "task": "tardis_portal.verify_dfos",
-        "schedule": timedelta(seconds=300)
-    },
-}
-
-CELERY_IMPORTS = ('tardis.tardis_portal.tasks',)
-
-# Use a real broker (e.g. RabbitMQ) for production, but memory is OK for
+# Use a strong password for production, but guest credentials are OK for
 # local development:
-BROKER_URL = 'memory://'
+BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 
 # For local development, you can force Celery tasks to run synchronously:
 # CELERY_ALWAYS_EAGER = True
