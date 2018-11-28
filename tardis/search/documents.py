5cc4284606e49ea682b55f3e0192e16d3ca3e033
manishkumr <rishimanish123@gmail.com>
2018-11-28 11:16:46 +1100

    removing unused imports

--- tardis/search/documents.py
+++ tardis/search/documents.py
@@ -1,14 +1,9 @@
 import logging
 import datetime
-import elasticsearch_dsl as search
 
 from django.conf import settings
 
-#from haystack import indexes
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
 from django_elasticsearch_dsl import DocType, Index
-from elasticsearch_dsl.connections import connections
 
 from tardis.tardis_portal.models import Dataset, Experiment
 
