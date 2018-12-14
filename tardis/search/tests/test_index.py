afd2c5ec3d2ef48f7385f4a0db59326a52905854
manishkumr <rishimanish123@gmail.com>
2018-12-14 15:00:24 +1100

    updated test case to delete indices before creating

--- tardis/search/tests/test_index.py
+++ tardis/search/tests/test_index.py
@@ -1,5 +1,6 @@
-from django.conf import settings
+from django.core.management import call_command
 from django.contrib.auth.models import User
+from django.utils.six import StringIO
 from django.test import TestCase
 
 from tardis.tardis_portal.models import Dataset, Experiment
@@ -14,6 +15,9 @@ class IndexExperimentTestCase(TestCase):
         pwd = 'secret'
         email = ''
         self.user = User.objects.create_user(user, email, pwd)
+        self.out = StringIO()
+        call_command('search_index', stdout=self.out,
+                     action='delete', models=['tardis_portal.experiment'], force=True)
 
     def test_create_index(self):
         exp1 = Experiment(title='test exp1',
@@ -29,7 +33,7 @@ class IndexExperimentTestCase(TestCase):
         # get search instance
         search = ExperimentDocument.search()
         # query for title(exact matching)
-        query = search.query("term", title='test exp1')
+        query = search.query("match", title='test exp1')
         result = query.execute(ignore_cache=True)
         self.assertEqual(result.hits[0].title, 'test exp1')
         # query for description
