c1fabb21eb5713de2cccd2b43b971f4b7a3b9337
manishkumr <rishimanish123@gmail.com>
2019-01-14 13:17:16 +1100

    adding filter to query by time range

--- tardis/apps/search_v2/views.py
+++ tardis/apps/search_v2/views.py
@@ -2,6 +2,7 @@
 views relevant to search
 """
 import logging
+import datetime
 
 from django.views.generic.base import TemplateView
 
@@ -167,12 +168,14 @@ class SearchV2View(TemplateView):
     def get_context_data(self, **kwargs):
         c = super(SearchV2View, self).get_context_data(**kwargs)
         query_text = self.request.GET.dict().get('q', '')
+        query_date_gt = self.request.GET.dict().get('dt_start', datetime.datetime(1970, 01, 01).strftime('%d%m%Y'))
+        query_date_lt = self.request.GET.dict().get('dt_end', datetime.datetime.today().strftime('%d%m%Y'))
         s = Search()
         search = s.query(
             "multi_match",
             query=query_text,
             fields=["title", "description", "filename"]
-        )
+        ).filter('range', created_time={'gte': query_date_gt, 'lt': query_date_lt, 'format': 'ddMMyyyy||yyyy'})
         results = search.execute()
         c['results'] = results
         c['hits'] = results.hits
