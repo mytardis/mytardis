111a6f66ee3b001b94a6f0f44569a1e6560c7932
James Wettenhall <james.wettenhall@gmail.com>
2019-01-18 11:11:10 +1100

    Fixes #1425

--- tardis/tardis_portal/download.py
+++ tardis/tardis_portal/download.py
@@ -37,6 +37,7 @@ from django.contrib.auth.decorators import login_required
 from tardis.analytics.tracker import IteratorTracker
 from .models import Dataset
 from .models import DataFile
+from .models import DataFileObject
 from .models import Experiment
 from .auth.decorators import has_datafile_download_access
 from .auth.decorators import experiment_download_required
@@ -426,8 +427,10 @@ def streaming_download_experiment(request, experiment_id, comptype='tgz',
     rootdir = get_filesystem_safe_experiment_name(experiment)
     filename = '%s-complete.tar' % rootdir
 
-    datafiles = DataFile.objects.filter(
-        dataset__experiments__id=experiment_id)
+    df_ids = DataFileObject.objects.filter(
+        datafile__dataset__experiments__id=experiment_id, verified=True) \
+                .values('datafile_id').distinct()
+    datafiles = DataFile.objects.filter(id__in=df_ids)
     return _streaming_downloader(request, datafiles, rootdir, filename,
                                  comptype, organization)
 
@@ -439,7 +442,10 @@ def streaming_download_dataset(request, dataset_id, comptype='tgz',
     rootdir = get_filesystem_safe_dataset_name(dataset)
     filename = '%s-complete.tar' % rootdir
 
-    datafiles = DataFile.objects.filter(dataset=dataset)
+    df_ids = DataFileObject.objects.filter(
+        datafile__dataset=dataset, verified=True) \
+        .values('datafile_id').distinct()
+    datafiles = DataFile.objects.filter(id__in=df_ids)
     return _streaming_downloader(request, datafiles, rootdir, filename,
                                  comptype, organization)
 
