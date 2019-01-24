b3f6acf88d3823381b13f1f30c1cec9317362f08
James Wettenhall <james.wettenhall@gmail.com>
2019-01-24 16:36:52 +1100

    A previous commit had removed the 0015_dataset_created_time migration which was released in v4.0.0.  This commit restores that migration and modifies the "0016_add_timestamps" migration (now called "0015_add_timestamps", so that it can be applied onto an existing MyTardis 4.0.0 deployment.

--- tardis/tardis_portal/migrations/0016_add_timestamps.py
+++ tardis/tardis_portal/migrations/0016_add_timestamps.py
@@ -9,21 +9,11 @@ import django.utils.timezone
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tardis_portal', '0014_auto_20181002_1154'),
+        ('tardis_portal', '0015_dataset_created_time'),
     ]
 
     operations = [
         # add timestamp fields with null values
-        migrations.AddField(
-            model_name='dataset',
-            name='created_time',
-            field=models.DateTimeField(blank=True, null=True),
-        ),
-        migrations.AddField(
-            model_name='dataset',
-            name='modified_time',
-            field=models.DateTimeField(blank=True, null=True),
-        ),
         migrations.AddField(
             model_name='facility',
             name='created_time',
