7070870aa44b0f50011d865e628c03fa2476f0d0
manishkumr <rishimanish123@gmail.com>
2019-03-05 11:11:02 +1100

    splitchunk optimization for bundled js and css

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     context: __dirname,
@@ -11,15 +12,42 @@ module.exports = {
         path: path.resolve('./assets/bundles/'),
         filename: '[name]-[hash].js'
     },
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    },
     plugins: [
         new BundleTracker({
             path: __dirname,
-            filename: 'webpack-stats.json'
+            filename: 'webpack-stats.json',
         }),
         new CleanWebpackPlugin(
             ['assets/bundles/*']
         ),
-        new MiniCssExtractPlugin('bundle.styles.css'),
+        new MiniCssExtractPlugin({
+            filename: 'bundle-[hash].styles.css',
+            chunkFilename: "[id].css"
+        }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
@@ -29,15 +57,27 @@ module.exports = {
     module: {
         rules: [
             {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
-            {test: /\.css$/, loader: 'style-loader!css-loader',},
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/bundles/',
+                            publicPath: '../static/bundles/'
+                        }
+                    }, "css-loader"
+                ]
+            },
             {
                 test: /\.(woff|woff2|)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: 'url-loader?limit=10000&mimetype=application/font-woff',
                 options: {
-                        name: '[name].[ext]',
-                        outputPath: 'static/bundles/',
-                        publicPath: '../static/bundles/'
-                    }
+                    name: '[name].[ext]',
+                    outputPath: 'static/bundles/',
+                    publicPath: '../static/bundles/'
+                }
             },
             {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader'},
             {test: /backbone.js/, loader: 'imports-loader?define=>false'},
@@ -55,13 +95,11 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: [{
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader"
-                }, {
-                    loader: "less-loader"
-                }]
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "less-loader"}
+                ]
             },
 
         ]
