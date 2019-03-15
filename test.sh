c2b6dad3cdc73bc142a5685c82f86386a003a025
James Wettenhall <james.wettenhall@gmail.com>
2019-03-15 12:29:58 +1100

    Upgrade to grunt-contrib-qunit 3.1 which uses Chromium via Puppeteer instead of PhantomJS.

--- test.sh
+++ test.sh
@@ -42,7 +42,7 @@ case "$TEST_TYPE" in
 	(( exit_status = exit_status || $? ))
     ;;
     behave)
-        npm install && npm install phantomjs-prebuilt && npm test \
+        npm install && npm test \
         && python test.py behave
 	(( exit_status = exit_status || $? ))
     ;;
