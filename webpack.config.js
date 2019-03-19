e43ba3bab6a7b7d742c4ae603ed8eb736b60fa75
manishkumr <rishimanish123@gmail.com>
2019-03-19 16:27:10 +1100

    creating entrypoint for my_data and hsared tabs

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
         tardis_portal_view_experiment: glob.sync('./assets/js/tardis_portal/view_experiment/*.js'),
         tardis_portal_view_dataset: glob.sync('./assets/js/tardis_portal/view_dataset/**/*.js'),
         tardis_portal_manage_group_members: glob.sync('./assets/js/tardis_portal/manage_group_members/**/*.js'),
+        tardis_portal_my_data: glob.sync('./assets/js/tardis_portal/my_data/**/*.js'),
+        tardis_portal_shared: glob.sync('./assets/js/tardis_portal/shared/**/*.js'),
         tardis_portal_facility_view: './assets/js/tardis_portal/facility_view/index.js',
         lib: glob.sync('./assets/js/lib/**/*.js'),
     },
