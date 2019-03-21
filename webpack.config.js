4e19c6a619990891e876e22d4df607e2e4397018
manishkumr <rishimanish123@gmail.com>
2019-03-21 17:03:31 +1100

    removing angular js optimization by terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,48 @@
-const path = require('path');
-const webpack = require('webpack');
-const BundleTracker = require('webpack-bundle-tracker');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const BundleTracker = require("webpack-bundle-tracker");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 const glob = require("glob");
 
 module.exports = {
     context: __dirname,
     entry: {
-        main: './assets/js/index.js',
-        tardis_portal: glob.sync('./assets/js/tardis_portal/*.js'),
-        tardis_portal_view_experiment_init: glob.sync('./assets/js/tardis_portal/view_experiment/init/init.js'),
-        tardis_portal_view_experiment_share: glob.sync('./assets/js/tardis_portal/view_experiment/share/share.js'),
-        tardis_portal_view_experiment: glob.sync('./assets/js/tardis_portal/view_experiment/*.js'),
-        tardis_portal_view_dataset: glob.sync('./assets/js/tardis_portal/view_dataset/**/*.js'),
-        tardis_portal_manage_group_members: glob.sync('./assets/js/tardis_portal/manage_group_members/**/*.js'),
-        tardis_portal_my_data: glob.sync('./assets/js/tardis_portal/my_data/**/*.js'),
-        tardis_portal_shared: glob.sync('./assets/js/tardis_portal/shared/**/*.js'),
-        tardis_portal_facility_view: './assets/js/tardis_portal/facility_view/index.js',
-        lib: glob.sync('./assets/js/lib/**/*.js'),
+        main: "./assets/js/index.js",
+        tardis_portal: glob.sync("./assets/js/tardis_portal/*.js"),
+        tardis_portal_view_experiment_init: glob.sync("./assets/js/tardis_portal/view_experiment/init/init.js"),
+        tardis_portal_view_experiment_share: glob.sync("./assets/js/tardis_portal/view_experiment/share/share.js"),
+        tardis_portal_view_experiment: glob.sync("./assets/js/tardis_portal/view_experiment/*.js"),
+        tardis_portal_view_dataset: glob.sync("./assets/js/tardis_portal/view_dataset/**/*.js"),
+        tardis_portal_manage_group_members: glob.sync("./assets/js/tardis_portal/manage_group_members/**/*.js"),
+        tardis_portal_my_data: glob.sync("./assets/js/tardis_portal/my_data/**/*.js"),
+        tardis_portal_shared: glob.sync("./assets/js/tardis_portal/shared/**/*.js"),
+        tardis_portal_facility_view: "./assets/js/tardis_portal/facility_view/index.js",
+        lib: glob.sync("./assets/js/lib/**/*.js"),
     },
     output: {
-        path: path.resolve('./assets/bundles/'),
-        filename: '[name]-[hash].js'
+        path: path.resolve("./assets/bundles/"),
+        filename: "[name]-[hash].js"
     },
     optimization: {
-        minimizer: [new TerserPlugin()],
+        minimizer: [
+            new TerserPlugin({
+                chunkFilter: (chunk) => {
+                    if (chunk.name === "tardis_portal_facility_view") {
+                        return false;
+                    }
+                    return true;
+                }
+                })],
         splitChunks: {
-            chunks: 'async',
+            chunks: "async",
             minSize: 30000,
             maxSize: 0,
             minChunks: 1,
             maxAsyncRequests: 5,
             maxInitialRequests: 3,
-            automaticNameDelimiter: '~',
+            automaticNameDelimiter: "~",
             name: true,
             cacheGroups: {
                 vendors: {
@@ -52,60 +60,61 @@ module.exports = {
     plugins: [
         new BundleTracker({
             path: __dirname,
-            filename: 'webpack-stats.json',
+            filename: "webpack-stats.json",
         }),
         new CleanWebpackPlugin(
-            ['assets/bundles/*']
+            ["assets/bundles/*"]
         ),
         new MiniCssExtractPlugin({
-            filename: '[name]-[hash].styles.css',
+            filename: "[name]-[hash].styles.css",
         }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
-            'window.jQuery': 'jquery',
-            'async': 'async',
-            'main': 'main',
-            'backbonemodels' : 'backbonemodels',
+            "window.jQuery": "jquery",
+            "async": "async",
+            "main": "main",
+            "backbonemodels": "backbonemodels",
         }),
     ],
     module: {
         rules: [
-            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"},
             {
                 test: /\.css$/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            outputPath: 'static/bundles/',
-                            publicPath: '../static/bundles/'
+                            outputPath: "static/bundles/",
+                            publicPath: "../static/bundles/"
                         }
                     }, "css-loader"
                 ]
             },
             {
                 test: /\.(woff|woff2|)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+                loader: "url-loader?limit=10000&mimetype=application/font-woff",
                 options: {
-                    name: '[name].[ext]',
-                    outputPath: 'static/bundles/',
-                    publicPath: '../static/bundles/'
+                    name: "[name].[ext]",
+                    outputPath: "static/bundles/",
+                    publicPath: "../static/bundles/"
                 }
             },
-            {   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader',
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader",
                 options: {
-                    name: '[name].[ext]',
-                    publicPath: '/bundles/'
+                    name: "[name].[ext]",
+                    publicPath: "/bundles/"
                 }
             },
-            {test: /backbone.js/, loader: 'imports-loader?define=>false'},
+            {test: /backbone.js/, loader: "imports-loader?define=>false"},
             {
                 test: /\.(gif|png|jpe?g)$/i,
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
-                    name: '[name].[ext]',
+                    name: "[name].[ext]",
                 }
             },
             {
@@ -120,13 +129,13 @@ module.exports = {
         ]
     },
     resolve: {
-        modules: ['node_modules', 'bower_components'],
-        extensions: ['*', '.js', '.jsx'],
-        alias:{
-            'jquery': __dirname + '/node_modules/jquery',
-            'main': __dirname + '/assets/js/tardis_portal/main',
-            'async': __dirname + '/assets/js/lib/async.min',
-            'backbonemodels': __dirname + '/assets/js/tardis_portal/backbone-models',
+        modules: ["node_modules", "bower_components"],
+        extensions: ["*", ".js", ".jsx"],
+        alias: {
+            "jquery": __dirname + "/node_modules/jquery",
+            "main": __dirname + "/assets/js/tardis_portal/main",
+            "async": __dirname + "/assets/js/lib/async.min",
+            "backbonemodels": __dirname + "/assets/js/tardis_portal/backbone-models",
         },
     }
-}
+};
