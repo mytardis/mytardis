51e3a6fcc2a0870f3de4a78c890edb71a0e83e55
manishkumr <rishimanish123@gmail.com>
2019-04-28 20:29:15 +1000

    moving from class component to functional component and using hooks

--- assets/js/app/search/search.js
+++ assets/js/app/search/search.js
@@ -1,57 +1,85 @@
-import React, {Component} from "react";
+import React, { useState, } from "react";
+import { Component, } from "react";
 import {TypeTags, IntrumentTags} from "./tags.js" ;
 import SearchDatePicker from "./datepicker";
 
-class Search extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-10">
-                        <div className="input-group" id={"adv-search"}>
-                            <input type="text" className="form-control"
-                                   placeholder="Search for Experiments, Datasets, Datafiles"/>
-                            <div className="input-group-btn">
-                                <div className="btn-group" role="group">
-                                    <div className="dropdown dropdown-lg">
-                                        <button type="button" className="btn btn-default dropdown-toggle"
-                                                data-toggle="dropdown" aria-expanded="false"><span
-                                            className="caret"></span></button>
-                                        <div className="dropdown-menu dropdown-menu-right" role="menu">
-                                            <form className="form-horizontal" role="form">
-                                                <div className="form-group">
-                                                    <label htmlFor="filter">Search </label>
-                                                    <input type="text" className="form-control"></input>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label htmlFor="filter">Select Date Range</label>
-                                                     <SearchDatePicker className="form-control"></SearchDatePicker>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label htmlFor="contain">Search In</label>
-                                                    <TypeTags className="form-control"></TypeTags>
-                                                </div>
-                                                <div className="form-group">
-                                                    <label htmlFor="contain">Instrument</label>
-                                                    <IntrumentTags className="form-group"></IntrumentTags>
-                                                </div>
-                                                <button type="submit" className="btn btn-primary"><span
-                                                    className="glyphicon glyphicon-search" aria-hidden="true"></span>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <button type="button" className="btn btn-primary"><span
-                                        className="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+function Search() {
+    const [simpleSearch, setSimpleSerach] = useState("")
+    const [advanceSearchVisible, setAdvanceSearchVisible ] = useState(false)
+    const toggleAdvanceSearch = () => setAdvanceSearchVisible(!advanceSearchVisible)
+    return (
+        <main>
+            <SimpleSearchForm></SimpleSearchForm>
+                <button type="button"
+                        onClick={toggleAdvanceSearch}
+                        className="btn btn-default dropdown-toggle"
+                        data-toggle="dropdown" aria-expanded="false">
+                    <span className="caret"></span>
+                </button>
+            {advanceSearchVisible ? (<AdvanceSearchForm></AdvanceSearchForm>) :
+                (<button type="submit" className="simple-search btn btn-primary">
+                    <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </button>)}
+        </main>
+    )
+}
 
+function SimpleSearchForm() {
+    const [simpleSearchText, setSimpleSearchText] = useState("");
+    const handleSimpleSearchSubmit = e => {
+        e.preventDefault();
+        //form validation
+        console.log(simpleSearchText)
     }
+    const showAdvanceSearchForm = e => {
+        e.preventDefault();
+    }
+    return (
+        <form onSubmit={handleSimpleSearchSubmit} id={"simple-search"}>
+                <input type="text"
+                       name="simple_search_text"
+                       onChange={event => setSimpleSearchText(event.target.value)}
+                       value={simpleSearchText}
+                       className="form-control"
+                       placeholder="Search for Experiments, Datasets, Datafiles">
+                </input>
+        </form>
+    )
+}
+
+function AdvanceSearchForm() {
+    const [advanceSearchText, setAdvanceSeacrhText] = useState("");
+    const handleAdvanceSearchSubmit = e => {
+        e.preventDefault();
+        //form validation
+        console.log(advanceSeacrhText)
+    }
+    const handleAdvanceSearchTextChange = e => {
+        e.preventDefault();
+        //form validation
+        console.log(advanceSeacrhText)
+    }
+    return (
+        <form id="adv-search-form" onSubmit={handleAdvanceSearchSubmit} className="form-horizontal" role="form">
+            <div className="form-group" id={"adv-search"}>
+                <label htmlFor="filter">Search </label>
+                <input type="text" name="adv_search_text" value={advanceSearchText}
+                       onChange={handleAdvanceSearchTextChange}
+                       className="form-control">
+                </input>
+                <label htmlFor="filter">Select Date Range</label>
+                <SearchDatePicker className="form-control"></SearchDatePicker>
+                <label htmlFor="contain">Search In</label>
+                <TypeTags className="form-control"></TypeTags>
+                <label htmlFor="contain">Instrument</label>
+                <IntrumentTags className="form-group"></IntrumentTags>
+                <button type="submit" className="btn btn-primary">
+                    <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </button>
+            </div>
+        </form>
+    )
+
 }
 
 export default Search;
