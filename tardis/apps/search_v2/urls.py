9ca64a45063e1c2996f325f5731c6104ecbc4f3c
manishkumr <rishimanish123@gmail.com>
2019-06-17 14:40:06 +1000

    removing legacy serach view and urls

--- tardis/apps/search_v2/urls.py
+++ tardis/apps/search_v2/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 
-from tardis.apps.search_v2.views import SearchV2View, search_experiment
+from tardis.apps.search_v2.views import SearchV2View
 
 urlpatterns = [
     url(r'^\?$', SearchV2View.as_view(), name='search_v2'),
-    url(r'^experiment/$', search_experiment,
-        name='tardis.search.views.search_experiment'),
 
 ]
