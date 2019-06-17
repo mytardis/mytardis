364851e4cf8eb3f970db41086e3be8cbd9c18b93
manishkumr <rishimanish123@gmail.com>
2019-06-17 16:39:26 +1000

    removing unused imports and adding default settings

--- tardis/apps/search_v2/views.py
+++ tardis/apps/search_v2/views.py
@@ -2,11 +2,9 @@
 views relevant to search
 """
 import logging
-import datetime
 
 from django.views.generic.base import TemplateView
 
-from django_elasticsearch_dsl.search import Search
 
 logger = logging.getLogger(__name__)
 
@@ -14,4 +12,3 @@ logger = logging.getLogger(__name__)
 class SearchV2View(TemplateView):
 
     template_name = 'searchV2.html'
-
