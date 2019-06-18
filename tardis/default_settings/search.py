78735629553d4e0cc0912ee7efca914f4d6770d6
manishkumr <rishimanish123@gmail.com>
2019-06-18 13:56:35 +1000

    some pylint fixes

--- tardis/default_settings/search.py
+++ tardis/default_settings/search.py
@@ -4,11 +4,11 @@ from .apps import INSTALLED_APPS
 
 SINGLE_SEARCH_V2_ENABLED = False
 if SINGLE_SEARCH_V2_ENABLED:
-    INSTALLED_APPS += 'django_elasticsearch_dsl',
+    INSTALLED_APPS += ('django_elasticsearch_dsl',)
     ELASTICSEARCH_DSL = {
         'default': {
             'hosts': os.environ.get('ELASTICSEARCH_URL', 'http://localhost:9200')
-        },
+        }
     }
     ELASTICSEARCH_DSL_INDEX_SETTINGS = {
         'number_of_shards': 1
