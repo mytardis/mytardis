8da578400ea1c16c24eb31775c58f6a8123165fb
manishkumr <rishimanish123@gmail.com>
2019-06-26 14:59:17 +1000

    updated search url

--- tardis/apps/search/urls.py
+++ tardis/apps/search/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from tardis.apps.search.views import SearchView
 
 urlpatterns = [
-    url(r'^\?$', SearchView.as_view(), name='search'),
+    url(r'^$', SearchView.as_view(), name='search'),
 ]
