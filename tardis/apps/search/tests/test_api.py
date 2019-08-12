08f9456fc529e83be0f362e518878d804e577197
manishkumr <rishimanish123@gmail.com>
2019-08-12 17:08:41 +1000

    Adding assertions for dataset and datafile

--- tardis/apps/search/tests/test_api.py
+++ tardis/apps/search/tests/test_api.py
@@ -4,9 +4,10 @@ import unittest
 
 from django.test import modify_settings, override_settings
 from django.core.management import call_command
+from django.conf import settings
 from django.utils.six import StringIO
 
-from tardis.tardis_portal.models import Experiment
+from tardis.tardis_portal.models import Experiment, Dataset, DataFile
 from tardis.tardis_portal.tests.api import MyTardisResourceTestCase
 
 
@@ -33,14 +34,21 @@ class SimpleSearchTest(MyTardisResourceTestCase):
                      action='rebuild', force=True)
 
     def test_simple_search(self):
-        self.exp1 = Experiment(title='test exp1',
-                               institution_name='monash',
-                               description='Test Description',
-                               created_by=self.user)
-        self.exp1.save()
 
+        # add dataset and datafile to experiment
+        self.dataset1 = Dataset(description='test_dataset')
+        self.dataset1.save()
+        self.dataset1.experiments.add(self.testexp)
+        self.dataset1.save()
+
+        settings.REQUIRE_DATAFILE_SIZES = False
+        settings.REQUIRE_DATAFILE_CHECKSUMS = False
+        self.datafile = DataFile(dataset=self.dataset1, filename='test.txt')
+        self.datafile.save()
         response = self.api_client.get('/api/v1/search_simple-search/?query=test',
                                        authentication=self.get_credentials())
-        data = json.loads(response.content)
 
+        data = json.loads(response.content)
         self.assertEqual(len(data['objects'][0]['hits']['experiments']), 1)
+        self.assertEqual(len(data['objects'][0]['hits']['datasets']), 1)
+        self.assertEqual(len(data['objects'][0]['hits']['datafiles']), 1)
