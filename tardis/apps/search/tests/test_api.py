a373f5883feb8cbaf3feafec702017fa99595807
manishkumr <rishimanish123@gmail.com>
2019-08-12 16:24:42 +1000

    updating simple search test case

--- tardis/apps/search/tests/test_api.py
+++ tardis/apps/search/tests/test_api.py
@@ -1,14 +1,12 @@
 import os
+import json
 import unittest
 
-from django.test import TestCase, modify_settings, override_settings
+from django.test import modify_settings, override_settings
 from django.core.management import call_command
-from django.contrib.auth.models import User
 from django.utils.six import StringIO
-from django.test.client import Client
 
-from tardis.tardis_portal.models import Experiment, Dataset, DataFile
-
+from tardis.tardis_portal.models import Experiment
 from tardis.tardis_portal.tests.api import MyTardisResourceTestCase
 
 
@@ -18,26 +16,31 @@ from tardis.tardis_portal.tests.api import MyTardisResourceTestCase
 })
 @override_settings(ELASTICSEARCH_DSL={
         'default': {
-            'hosts': 'http://localhost:9200'
+            'hosts': os.environ.get('ELASTICSEARCH_URL', None)
         },
 })
-class SimpleSearchTestCase(TestCase):
+@unittest.skipUnless(
+        os.environ.get('ELASTICSEARCH_URL', None),
+        "--elasticsearch not set"
+    )
+class SimpleSearchTest(MyTardisResourceTestCase):
     def setUp(self):
-        user = 'tardis_user1'
-        pwd = 'secret'
-        email = ''
-        self.user = User.objects.create_user(user, email, pwd)
+        super(SimpleSearchTest, self).setUp()
         self.out = StringIO()
         call_command('search_index', stdout=self.out,
                      action='delete', force=True)
+        call_command('search_index', stdout=self.out,
+                     action='rebuild', force=True)
+
+    def test_simple_search(self):
         self.exp1 = Experiment(title='test exp1',
                                institution_name='monash',
                                description='Test Description',
                                created_by=self.user)
         self.exp1.save()
 
-    def test_simple_search(self):
-        client = Client()
-        client.login(username=self.user.username, password=self.user.password)
-        response = client.post('/api/v1/search_advance-search/schema/')
-        self.assertEqual(response.status_code, 200)
+        response = self.api_client.get('/api/v1/search_simple-search/?query=test',
+                                       authentication=self.get_credentials())
+        data = json.loads(response.content)
+
+        self.assertEqual(len(data['objects'][0]['hits']['experiments']), 1)
