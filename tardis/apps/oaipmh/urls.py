c3eb5dc6643095db4ebd45ab167660cd300d6992
James Wettenhall <james.wettenhall@gmail.com>
2019-09-22 22:16:41 +1000

    Remove unused apps which aren't Python 3 compatible

--- tardis/apps/oaipmh/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from .views import endpoint
-
-
-urlpatterns = [
-    url(r'^$', endpoint, name="oaipmh-endpoint"),
-]
--- tardis/default_settings/publication.py
+++ tardis/default_settings/publication.py
@@ -11,12 +11,3 @@ RIFCS_TEMPLATE_DIR = path.join(
     'tardis_portal/templates/tardis_portal/rif-cs/profiles/')
 RIFCS_GROUP = "MyTARDIS Default Group"
 RIFCS_KEY = "keydomain.example"
-RELATED_INFO_SCHEMA_NAMESPACE = \
-    'http://www.tardis.edu.au/schemas/related_info/2011/11/10'
-RELATED_OTHER_INFO_SCHEMA_NAMESPACE = \
-    'http://www.tardis.edu.au/schemas/experiment/annotation/2011/07/07'
-
-OAIPMH_PROVIDERS = [
-    'tardis.apps.oaipmh.provider.experiment.DcExperimentProvider',
-    'tardis.apps.oaipmh.provider.experiment.RifCsExperimentProvider',
-]
