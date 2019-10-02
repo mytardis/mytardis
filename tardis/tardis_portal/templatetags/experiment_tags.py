76a31b96ce5573a208f6d646e7dbb017844d04fa
James Wettenhall <james.wettenhall@gmail.com>
2019-10-02 21:10:23 +1000

    Avoid duplicating experiment authors markup in templates

--- tardis/tardis_portal/templatetags/experiment_tags.py
+++ tardis/tardis_portal/templatetags/experiment_tags.py
@@ -84,6 +84,16 @@ def experiment_badges(experiment, **kwargs):
     }
 
 
+@register.inclusion_tag('tardis_portal/experiment_tags/experiment_authors.html')
+def experiment_authors(experiment, **kwargs):
+    """
+    Displays an experiment's authors in an experiment list view
+    """
+    return {
+        'experiment': experiment
+    }
+
+
 @register.inclusion_tag('tardis_portal/experiment_tags/experiment_download_link.html')
 def experiment_download_link(experiment, **kwargs):
     """
