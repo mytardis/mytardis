9f96d0dd70d95d7da98f0ee0dea012558d0a3a85
James Wettenhall <james.wettenhall@gmail.com>
2019-10-29 11:26:50 +1100

    Remove interactive createuser test which doesn't work in Django 2.0

--- tardis/tardis_portal/tests/management/test_createuser.py
+++ tardis/tardis_portal/tests/management/test_createuser.py
@@ -11,24 +11,3 @@ class CreateUserTestCase(TestCase):
         without any runtime exceptions
         '''
         call_command('createuser', username='testuser1', email='testuser1@example.com', interactive=False)
-
-    def testInteractive(self):
-        '''
-        Just test that we can run
-        ./manage.py createuser
-        without any runtime exceptions by mocking the raw_input username and
-        email entry
-        '''
-
-        def test_get_username(_):
-            return 'testuser2'
-
-        def test_get_email():
-            return 'testuser2@example.com'
-
-        def test_get_password(_):
-            return 'Open Sesame!'
-
-        call_command(
-            'createuser', interactive=True, get_username=test_get_username,
-            get_email=test_get_email, get_password=test_get_password)
