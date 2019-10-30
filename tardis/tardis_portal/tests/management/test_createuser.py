68851e7c02783adc035739af3d0089ae4ab175b0
James Wettenhall <james.wettenhall@gmail.com>
2019-10-30 21:42:21 +1100

    Add some missing tests

--- tardis/tardis_portal/tests/management/test_createuser.py
+++ tardis/tardis_portal/tests/management/test_createuser.py
@@ -11,3 +11,29 @@ class CreateUserTestCase(TestCase):
         without any runtime exceptions
         '''
         call_command('createuser', username='testuser1', email='testuser1@example.com', interactive=False)
+
+    def testInteractive(self):
+        '''
+        Just test that we can run
+        ./manage.py createuser
+        without any runtime exceptions by mocking the raw_input username and
+        email entry
+        '''
+        from tardis.tardis_portal.management.commands.createuser import Command
+
+        cmd = Command()
+
+        def test_get_username(_):
+            return 'testuser2'
+
+        def test_get_email():
+            return 'testuser2@example.com'
+
+        def test_get_password(_):
+            return 'Open Sesame!'
+
+        args = []
+        options = dict(
+            interactive=True, get_username=test_get_username,
+            get_email=test_get_email, get_password=test_get_password)
+        cmd.handle(*args, **options)
