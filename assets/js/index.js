83773feca777f961cf7c61b44de7c56fdb62dad2
James Wettenhall <james.wettenhall@gmail.com>
2019-12-16 16:02:06 +1100

    Fix facility overview styles - fonts were too small

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,4 +27,5 @@ require("bootstrap/dist/css/bootstrap.css");
 require("../css/jquery-ui/jquery-ui-1.8.18.custom.css");
 require("font-awesome/css/font-awesome.css");
 require("../css/default.css");
+require("../css/facility-overview.css");
 require("blueimp-file-upload/css/jquery.fileupload.css");
