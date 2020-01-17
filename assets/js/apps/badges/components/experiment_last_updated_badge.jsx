3392ab0ab8fa58007f02128b7204ed6222633f46
manishkumr <rishimanish123@gmail.com>
2020-01-17 15:34:15 +1100

    eslint fixes

--- assets/js/apps/badges/components/experiment_last_updated_badge.jsx
+++ assets/js/apps/badges/components/experiment_last_updated_badge.jsx
@@ -1,22 +1,27 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 
-import Badge from 'react-bootstrap/Badge'
-import {fetchExperimentData} from './FetchData'
+import Badge from 'react-bootstrap/Badge';
+import PropTypes from 'prop-types';
+import { fetchExperimentData } from './FetchData';
 
-const ExperimentLastUpdatedBadge = ({experimentID}) => {
-  const [lastUpdatedTime, setLastUpdatedTime] = useState("");
+const ExperimentLastUpdatedBadge = ({ experimentID }) => {
+  const [lastUpdatedTime, setLastUpdatedTime] = useState('');
 
   useEffect(() => {
     fetchExperimentData(experimentID).then((data) => {
-        setLastUpdatedTime(new Date(data.update_time).toDateString())
-      })
-  },[]);
-  return(
-    <Badge variant="info" content={"test"} title={"test"}>
-      <i className="fa fa-clock-o"></i>&nbsp;
+      setLastUpdatedTime(new Date(data.update_time).toDateString());
+    });
+  }, []);
+  return (
+    <Badge variant="info" content="test" title="test">
+      <i className="fa fa-clock-o" />
+&nbsp;
       {lastUpdatedTime}
     </Badge>
-  )
+  );
 };
 
+ExperimentLastUpdatedBadge.propTypes = {
+  experimentID: PropTypes.string.isRequired,
+};
 export default ExperimentLastUpdatedBadge;
