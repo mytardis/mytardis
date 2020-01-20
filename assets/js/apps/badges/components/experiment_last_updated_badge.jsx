f1aa516e61cb9720c800062c14704ae5232f7c7f
manishkumr <rishimanish123@gmail.com>
2020-01-20 11:33:57 +1100

    updating last updated badge component to display humanized date and content in ISO format

--- assets/js/apps/badges/components/experiment_last_updated_badge.jsx
+++ assets/js/apps/badges/components/experiment_last_updated_badge.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
-
+import moment from "moment";
 import Badge from 'react-bootstrap/Badge';
 import PropTypes from 'prop-types';
 import { fetchExperimentData } from './FetchData';
+import {naturalDay} from "./humanize";
 
 const ExperimentLastUpdatedBadge = ({ experimentID }) => {
   const [lastUpdatedTime, setLastUpdatedTime] = useState('');
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     fetchExperimentData(experimentID).then((data) => {
-      setLastUpdatedTime(new Date(data.update_time).toDateString());
+      const date = new Date(data.update_time);
+      setLastUpdatedTime(naturalDay(date));
+      setContent(date.toISOString());
+      setTitle("Last updated: "+moment(date, "DD-MM-YYYY").format("llll"))
     });
   }, []);
   return (
-    <Badge variant="info" content="test" title="test">
+    <Badge variant="info" content={content} title={title}>
       <i className="fa fa-clock-o" />
 &nbsp;
       {lastUpdatedTime}
