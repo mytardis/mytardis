ec7b61e882e4af4bf5822dd553bb23653dd7038b
manishkumr <rishimanish123@gmail.com>
2020-01-20 11:33:21 +1100

    custom method to return humanized days
    This method will take a date object and will calculate number of days from current date and reutrn today/tomorrow or a week ago accordingly

---
+++ assets/js/apps/badges/components/humanize.js
@@ -0,0 +1,24 @@
+import moment from "moment";
+
+
+export const naturalDay = (date) => {
+  //get moment data from passed date
+  const momentDate = moment(date);
+  if (!momentDate.isValid()){
+    //passed date is not valid
+    return date
+  }
+  //calculate difference in days from today
+  const diffDays = moment().diff(moment(date),'days');
+  const durationDays = moment.duration(diffDays, 'days').asDays();
+  if(durationDays === 0){
+    return "today"
+  } else if(durationDays === 1 ){
+    return "yesterday"
+  } else if (durationDays > 1 && durationDays < 7){
+    return 'a week ago'
+  } else {
+    return momentDate.format('do MMMM YYYY');
+  }
+  
+};
