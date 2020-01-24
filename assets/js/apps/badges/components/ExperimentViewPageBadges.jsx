bd09bfc2db55fccec5ce8a93ecb868c3c867f6cf
manishkumr <rishimanish123@gmail.com>
2020-01-24 15:15:03 +1100

    render experiment view page badge as a component

--- assets/js/apps/badges/components/ExperimentViewPageBadges.jsx
+++ assets/js/apps/badges/components/ExperimentViewPageBadges.jsx
@@ -1,27 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+import PropTypes from 'prop-types';
 import ExperimentLastUpdatedBadge from './ExperimentLastUpdateBadge';
 import PublicAccessBadge from './PublicAccessBadge';
 import DatasetCountBadge from './DatasetCountBadge';
 import DatafileCountBadge from './DatafileCountBadge';
+import fetchExperimentData from './utils/FetchData';
 
+
+const ExperimentViewPageBadges = ({ experimentID }) => {
+  const [expData, setExpData] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  React.useEffect(() => {
+    fetchExperimentData(experimentID).then((data) => {
+      setExpData(data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    isLoading ? <span className="float-right spinner-grow spinner-grow-sm" role="status" aria-hidden="true" />
+      : (
+        <Fragment>
+          <span className="mr-2">
+            <DatasetCountBadge experimentData={expData} />
+          </span>
+          <span className="mr-2">
+            <DatafileCountBadge experimentData={expData} />
+          </span>
+          <span className="mr-2">
+            <ExperimentLastUpdatedBadge experimentData={expData} />
+          </span>
+          <span className="mr-2">
+            <PublicAccessBadge experimentData={expData} />
+          </span>
+        </Fragment>
+      )
+  );
+};
+
+ExperimentViewPageBadges.propTypes = {
+  experimentID: PropTypes.string.isRequired,
+};
 
 const elem = document.querySelector('.badges');
 const experimentID = elem.id.split('-')[1];
 ReactDOM.render(
-  <Fragment>
-    <span className="mr-2">
-      <DatasetCountBadge experimentID={experimentID} />
-    </span>
-    <span className="mr-2">
-      <DatafileCountBadge experimentID={experimentID} />
-    </span>
-    <span className="mr-2">
-      <ExperimentLastUpdatedBadge experimentID={experimentID} />
-    </span>
-    <span className="mr-2">
-      <PublicAccessBadge experimentID={experimentID} />
-    </span>
-  </Fragment>, elem,
+  <ExperimentViewPageBadges experimentID={experimentID} />, elem,
 );
