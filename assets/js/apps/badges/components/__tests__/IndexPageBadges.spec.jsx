921b55f0a9648e3b2e8ebbfa4782f672f6d6c6af
manishkumr <rishimanish123@gmail.com>
2020-01-28 17:26:21 +1100

    added few more tests

--- assets/js/apps/badges/components/__tests__/IndexPageBadges.spec.jsx
+++ assets/js/apps/badges/components/__tests__/IndexPageBadges.spec.jsx
@@ -5,7 +5,7 @@ import IndexPageBadges from '../IndexPageBadges';
 
 global.fetch = require('jest-fetch-mock');
 
-const fakeExperimentData = {
+let fakeExperimentData = {
   update_time: '2020-01-13T14:00:08.908600',
   dataset_count: 1,
   datafile_count: 100,
@@ -39,16 +39,77 @@ describe('renders badges on index page', () => {
     await act(async () => {
       ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
     });
-    expect(container.querySelector('ul').childNodes[1].firstChild.textContent.trim()).toEqual('1');
+    expect(container.querySelector('ul').childNodes[1].firstChild.textContent.trim())
+      .toEqual('1');
     expect(container.querySelector('ul').childNodes[1].firstChild.title).toEqual('1 dataset');
   });
   it('should render datafile count badge', async () => {
     await act(async () => {
       ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
     });
-    expect(container.querySelector('ul').childNodes[2].firstChild.textContent.trim()).toEqual('100');
+    expect(container.querySelector('ul').childNodes[2].firstChild.textContent.trim())
+      .toEqual('100');
     expect(container.querySelector('ul').childNodes[2].firstChild.title).toEqual('100 files');
   });
+  it('should render Public access badge', async () => {
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[3].firstChild.textContent.trim())
+      .toEqual('Private');
+    expect(container.querySelector('ul').childNodes[3].firstChild.title.trim())
+      .toEqual('No public access');
+  });
   // remove the mock to ensure tests are completely isolated
+  global.fetch.mockRestore();
+});
+
+describe('renders public access badge', () => {
+  it('should render badge as private', async () => {
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[3].firstChild.title.trim())
+      .toEqual('No public access');
+  });
+  it('should render badge as public', async () => {
+    fakeExperimentData = {
+      update_time: '2020-01-13T14:00:08.908600',
+      dataset_count: 1,
+      datafile_count: 100,
+      public_access: 100,
+    };
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[3].firstChild.title.trim())
+      .toEqual('All data is public');
+  });
+  it('should render badge as Metadata', async () => {
+    fakeExperimentData = {
+      update_time: '2020-01-13T14:00:08.908600',
+      dataset_count: 1,
+      datafile_count: 100,
+      public_access: 50,
+    };
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[3].firstChild.title.trim())
+      .toEqual('Only descriptions are public, not data');
+  });
+  it('should render badge as [PUBLICATION] Awaiting release', async () => {
+    fakeExperimentData = {
+      update_time: '2020-01-13T14:00:08.908600',
+      dataset_count: 1,
+      datafile_count: 100,
+      public_access: 25,
+    };
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[3].firstChild.title.trim())
+      .toEqual('Under embargo and awaiting release');
+  });
   global.fetch.mockRestore();
 });
