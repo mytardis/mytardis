4e6b7779ea04bcb286f1b1960a05650453b3ac6a
manishkumr <rishimanish123@gmail.com>
2020-01-28 16:04:16 +1100

    adding more tests

--- assets/js/apps/badges/components/__tests__/IndexPageBadges.spec.jsx
+++ assets/js/apps/badges/components/__tests__/IndexPageBadges.spec.jsx
@@ -5,9 +5,17 @@ import IndexPageBadges from '../IndexPageBadges';
 
 global.fetch = require('jest-fetch-mock');
 
-
+const fakeExperimentData = {
+  update_time: '2020-01-13T14:00:08.908600',
+  dataset_count: 1,
+  datafile_count: 100,
+  public_access: 1,
+};
 let container = null;
 beforeEach(() => {
+  jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve(fakeExperimentData),
+  }));
   container = document.createElement('div');
   document.body.appendChild(container);
 });
@@ -16,29 +24,31 @@ afterEach(() => {
   container.remove();
   container = null;
 });
-it('can render badges on index page', async () => {
-  const fakeExperimentData = {
-    update_time: '2020-01-13T14:00:08.908600',
-    dataset_count: 1,
-    datafile_count: 100,
-    public_access: 1,
-  };
-  jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
-    json: () => Promise.resolve(fakeExperimentData),
-  }));
-
-  await act(async () => {
-    ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+describe('renders badges on index page', () => {
+  it('should render last updated badge', async () => {
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.textContent).toContain('13th January 2020');
+    expect(container.querySelector('span').attributes.getNamedItem('title').value)
+      .toEqual('Last updated: Mon, Jan 13, 2020 2:00 PM');
+    expect(container.querySelector('span').attributes.getNamedItem('content').value)
+      .toEqual('2020-01-13T03:00:08.908Z');
   });
-  expect(container.textContent).toContain('13th January 2020');
-  expect(container.querySelector('span').attributes.getNamedItem('title').value)
-    .toEqual('Last updated: Mon, Jan 13, 2020 2:00 PM');
-  expect(container.querySelector('span').attributes.getNamedItem('content').value)
-    .toEqual('2020-01-13T03:00:08.908Z');
-  expect(container.querySelector('ul').childNodes[1].firstChild.textContent.trim()).toEqual('1');
-  expect(container.querySelector('ul').childNodes[1].firstChild.title).toEqual('1 dataset');
-  expect(container.querySelector('ul').childNodes[2].firstChild.textContent.trim()).toEqual('100');
-  expect(container.querySelector('ul').childNodes[2].firstChild.title).toEqual('100 files');
+  it('should render dataset count badge', async () => {
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[1].firstChild.textContent.trim()).toEqual('1');
+    expect(container.querySelector('ul').childNodes[1].firstChild.title).toEqual('1 dataset');
+  });
+  it('should render datafile count badge', async () => {
+    await act(async () => {
+      ReactDOM.render(<IndexPageBadges experimentID="123" />, container);
+    });
+    expect(container.querySelector('ul').childNodes[2].firstChild.textContent.trim()).toEqual('100');
+    expect(container.querySelector('ul').childNodes[2].firstChild.title).toEqual('100 files');
+  });
   // remove the mock to ensure tests are completely isolated
   global.fetch.mockRestore();
 });
