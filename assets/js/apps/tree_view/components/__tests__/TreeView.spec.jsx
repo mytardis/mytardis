055224988193e8b9363e1e490abc5a3914ff983b
manishkumr <rishimanish123@gmail.com>
2020-02-03 14:44:24 +1100

    adding test for treeview component

---
+++ assets/js/apps/tree_view/components/__tests__/TreeView.spec.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from '@testing-library/react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TreeView from '../TreeView';
+
+
+global.fetch = require('jest-fetch-mock');
+
+configure({ adapter: new Adapter() });
+
+const fakeTreeData = [{
+  name: 'parent_1',
+  path: 'parent_1',
+  children: [],
+},
+{
+  name: 'parent_2',
+  path: 'parent_2',
+  children: [],
+},
+{
+  name: 'Parent.txt',
+},
+{
+  name: 'STORM-6.jpg',
+}];
+const fakeChildData = [
+  {
+    name: 'child_1',
+    path: 'parent_1/child_1',
+    children: [],
+  },
+  {
+    name: 'child_2',
+    path: 'parent_1/child_2',
+    children: [],
+  },
+  {
+    name: 'Child_1.txt',
+  },
+  {
+    name: 'Child_2.txt',
+  }];
+
+describe('renders initial tree view on page load', () => {
+  let container = null;
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(fakeTreeData),
+    }));
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+  afterEach(() => {
+    // cleanup on exiting
+    container.remove();
+    container = null;
+  });
+  it('should render folder tree correctly', async () => {
+    await act(async () => {
+      ReactDOM.render(<TreeView datasetId="1234" modified="" />, container);
+    });
+    expect(container).toMatchSnapshot();
+  });
+  it('should render tree view with four child nodes', async () => {
+    await act(async () => {
+      ReactDOM.render(<TreeView datasetId="1234" modified="" />, container);
+    });
+    expect(container.querySelector('ul').children.length).toEqual(4);
+    expect(container.querySelector('ul').children[0].textContent).toEqual('parent_1');
+    expect(container.querySelector('ul').children[1].textContent).toEqual('parent_2');
+    expect(container.querySelector('ul').children[2].textContent).toEqual('Parent.txt');
+    expect(container.querySelector('ul').children[3].textContent).toEqual('STORM-6.jpg');
+  });
+});
+
+describe('renders child nodes when clicked on parent node', () => {
+  let container = null;
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(fakeTreeData),
+    }));
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+  afterEach(() => {
+    // cleanup on exiting
+    container.remove();
+    container = null;
+  });
+  it('should render child nodes when clicked on parent', async () => {
+    let component;
+    await act(async () => {
+      component = mount(<TreeView datasetId="1234" modified="" />);
+    });
+    component.update();
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(fakeChildData),
+    }));
+    await act(async () => {
+      component.find('NodeHeader').first().simulate('click');
+    });
+    component.update();
+    expect(component.find('NodeHeader')).toHaveLength(8);
+    expect(component.find('Header').get(0).props.iconClass).toEqual('folder-open');
+    expect(component.find('Header').get(0).props.node.children.length).toEqual(4);
+    expect(component.find('Header').get(0).props.node.children[0].name).toEqual('child_1');
+  });
+});
