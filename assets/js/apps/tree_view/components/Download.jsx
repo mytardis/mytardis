1a3869c5d8b3c7cad3002d981adb0276790cf705
manishkumr <rishimanish123@gmail.com>
2020-02-06 15:26:41 +1100

    adding button to nodes

--- assets/js/apps/tree_view/components/Download.jsx
+++ assets/js/apps/tree_view/components/Download.jsx
@@ -1,16 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const DownloadButton = ({ href }) => (
+const FileDownloadButton = ({ href }) => (
   <Fragment>
-    <a className="btn btn-sm" title="Download" href={href}>
+    <a className="btn-sm" style={{ color: 'Black' }} title="Download" href={href}>
       <i className="fa fa-download fa-sm" />
     </a>
   </Fragment>
 );
 
-DownloadButton.propTypes = {
-  href: PropTypes.string.isRequired,
+const TreeDownloadButton = ({ count }) => {
+  const [buttonText, setButtonText] = useState('Select All');
+  if (count > 0) {
+    setButtonText('Download selected');
+  }
+  return (
+    <Fragment>
+      <button
+        className="btn btn-outline-secondary"
+        style={{ marginBottom: '12px', fontWeight: 'bold' }}
+        title="Download"
+        type="submit"
+        count={count}
+      >
+        <i className="fa fa-download fa-sm mr-1 " />
+        {buttonText}
+      </button>
+    </Fragment>
+  );
 };
 
-export default DownloadButton;
+FileDownloadButton.propTypes = {
+  href: PropTypes.string.isRequired,
+};
+TreeDownloadButton.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+export { FileDownloadButton, TreeDownloadButton };
---
+++ assets/js/apps/tree_view/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FileDownloadButton } from './Download';
+
+const Header = ({
+  onSelect, node, style, iconClass,
+}) => {
+  const iconStyle = { marginRight: '5px', opacity: '0.6' };
+  return (
+    <div style={style.base}>
+      <div style={{ ...style.title }}>
+        <input
+          type="checkbox"
+          defaultChecked={node.selected}
+          className="datafile_checkbox"
+          style={{ marginRight: '5px' }}
+          value={`${node.name}`}
+          onClick={onSelect}
+        />
+        <i className={`fa fa-${iconClass}`} style={iconStyle} />
+        {node.name}
+        {iconClass === 'file-text' ? <FileDownloadButton href={`/api/v1/dataset_file/${node.id}/download/`} /> : ''}
+      </div>
+    </div>
+  );
+};
+
+
+Header.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+  node: PropTypes.object.isRequired,
+  style: PropTypes.object.isRequired,
+  iconClass: PropTypes.string.isRequired,
+};
+
+export default Header;
