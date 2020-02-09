b9bb2426f6b360fa76549a99d0e38738d99ee35f
Chris Seal <c.seal@auckland.ac.nz>
2020-02-09 21:08:39 +0000

    fix broken migrations - redux

--- tardis/tardis_portal/migrations/0017_uoa_experiment.py
+++ tardis/tardis_portal/migrations/0017_uoa_experiment.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='experiment',
             name='internal_id',
-            field=models.CharField(default=b'WUI-2019-03-02-13-03-50.499650', max_length=400, unique=True),
+            field=models.CharField(default=tardis.tardis_portal.models.experiment.experiment_internal_id_default, max_length=400, unique=True),
         ),
         migrations.AddField(
             model_name='experiment',
