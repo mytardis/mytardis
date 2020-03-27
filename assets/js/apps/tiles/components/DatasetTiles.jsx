43e421f9178e796b6ca48ba900b062628c327b5d
manishkumr <rishimanish123@gmail.com>
2020-03-27 14:21:46 +1100

    adding datasettiles

--- assets/js/apps/tiles/components/DatasetTiles.jsx
+++ assets/js/apps/tiles/components/DatasetTiles.jsx
@@ -1,8 +1,19 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
 
-const DatasetTiles = ({ experimentID }  ) => {
-
+const DatasetTiles = ({ experimentID }) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const datasetData = fetchExperimentDataset(experimentID);
+    setData(datasetData);
+  }, [experimentID]);
+  return (
+    <Fragment>
+      {data.map(
+        dataset => <DatasetTile data={dataset} />,
+      )}
+    </Fragment>
+  );
 };
 
 export default DatasetTiles;
