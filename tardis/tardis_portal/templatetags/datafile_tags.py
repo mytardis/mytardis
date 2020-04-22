b0d99cf4ef05ec02c6e98b1215be75e1f27c3f61
Mike Laverick <mike.laverick@auckland.ac.nz>
2020-04-23 11:24:00 +1200

    Typo fixes

--- tardis/tardis_portal/templatetags/datafile_tags.py
+++ tardis/tardis_portal/templatetags/datafile_tags.py
@@ -8,7 +8,7 @@ def is_datafile_downloadable(datafile_id, request):
     """
     Determines if Datafile is downloadable by User, respecting ACLs
     """
-    has_download_permissions = authz.has_datafile_download_access(request,datafile_id)
+    has_download_permissions = authz.has_datafile_download_access(request, datafile_id)
 
     return has_download_permissions
 
@@ -18,6 +18,6 @@ def is_datafile_writable(datafile_id, request):
     """
     Determines if Datafile is writable by User, respecting ACLs
     """
-    has_write_permissions = authz.has_datafile_write(request,datafile_id)
+    has_write_permissions = authz.has_datafile_write(request, datafile_id)
 
     return has_write_permissions
