988192bf2f5ac9fcd52cbfd39b22cf9afd6135e2
manishkumr <rishimanish123@gmail.com>
2020-04-27 16:08:34 +1000

    updating tests to mpount share list component

--- assets/js/apps/tiles/components/__tests__/DatasetTiles.spec.jsx
+++ assets/js/apps/tiles/components/__tests__/DatasetTiles.spec.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { act } from '@testing-library/react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import fetchMock from 'jest-fetch-mock';
 import DatasetTilesLists from '../DatasetTilesLists';
 
 const fakeData = [
@@ -40,39 +40,59 @@ const fakeData = [
     size_human_readable: '342.3 KB',
     show_dataset_thumbnails: true,
   }];
+const fakeExpList = [
+  {
+    id: 34,
+    title: 'My Test Exp',
+  },
+  {
+    id: 38,
+    title: 'My Test Exp 2',
+  }];
 
 global.fetch = require('jest-fetch-mock');
 
 configure({ adapter: new Adapter() });
 
 
-let container = null;
+let rootContainer = null;
 let shareContainer = null;
 let component = null;
+let mainContainer = null;
 beforeEach(async () => {
-  jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
-    json: () => Promise.resolve(fakeData),
-  }));
-  container = document.createElement('div');
-  container.setAttribute('id', 'main-list');
-  document.body.appendChild(container);
-  shareContainer = document.createElement('div');
-  shareContainer.setAttribute('id', 'share-list');
-  document.body.appendChild(shareContainer);
-  await act(async () => {
-    component = mount(<DatasetTilesLists experimentId="1234" shareContainer={shareContainer} />, { attachTo: container });
+  fetchMock.mockResponse((request) => {
+    if (request.url.endsWith('/dataset/')) {
+      return Promise.resolve(JSON.stringify(fakeData));
+    }
+    if (request.url.endsWith('/experiment_list/')) {
+      return Promise.resolve(JSON.stringify(fakeExpList));
+    }
+    return {
+      status: 404,
+      body: 'Not Found',
+    };
   });
-  component.update();
+
+  rootContainer = global.document.createElement('div');
+  rootContainer.setAttribute('id', 'root');
+  mainContainer = global.document.createElement('div');
+  mainContainer.setAttribute('id', 'datasets-pane');
+  rootContainer.appendChild(mainContainer);
+
+  shareContainer = global.document.createElement('div');
+  shareContainer.setAttribute('id', 'experiment-tab-transfer-datasets');
+  rootContainer.appendChild(shareContainer);
 });
 afterEach(() => {
   // cleanup on exiting
-  container.remove();
-  container = null;
-  component.unmount();
-  component.detach();
+
 });
 describe('render dataset tiles on page load', () => {
-  it('should match snapshot', () => {
-    expect(container).toMatchSnapshot();
+  it('should match snapshot', async () => {
+    await act(async () => {
+      component = mount(<DatasetTilesLists experimentId="1234" shareContainer={shareContainer} />, { attachTo: mainContainer });
+    });
+    component.update();
+    expect(rootContainer).toMatchSnapshot();
   });
 });
