721fdea73939edb16e5cc730ee6c860350a0db2b
Mike Laverick <mike.laverick@auckland.ac.nz>
2020-05-12 15:58:45 +1200

    Remove existing parameters from the "add parameter" dropdown box in parameter_edit v4

--- tardis/tardis_portal/views/parameters.py
+++ tardis/tardis_portal/views/parameters.py
@@ -52,7 +52,7 @@ def edit_parameters(request, parameterset, otype, view_sensitive=False):
     parameternames = ParameterName.objects.filter(
         schema__namespace=parameterset.schema.namespace)
     for parameter in parameterset.parameters:
-        parameternames.exclude(name=parameter.name.name)
+        parameternames = parameternames.exclude(id=parameter.name.id)
 
     success = False
     valid = True
