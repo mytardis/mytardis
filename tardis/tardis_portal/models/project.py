0a5af094cf4d2088ad2f35019e8c932ce713bc5a
Mike Laverick <mike.laverick@auckland.ac.nz>
2020-05-27 11:26:49 +1200

    Refactored Proj/Exp/Set/File managers into generic SafeManager

--- tardis/tardis_portal/models/project.py
+++ tardis/tardis_portal/models/project.py
@@ -13,7 +13,7 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.timezone import now as django_time_now
 from .institution import Institution
 # from ..models import DataManagementPlan # Hook in place for future proofing
-from ..managers import OracleSafeManager, ProjectManager
+from ..managers import OracleSafeManager, SafeManager
 
 from .access_control import ObjectACL
 
@@ -61,7 +61,7 @@ class Project(models.Model):
                           null=True, blank=True)
     institution = models.ManyToManyField(Institution,
                                          related_name='institutions')
-    safe = ProjectManager()
+    safe = SafeManager()
 
     #TODO Integrate DMPs into the project.
     #data_management_plan = models.ManyToManyField(DataManagementPlan,
