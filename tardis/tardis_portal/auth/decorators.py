471f4a34f5c89a015644cf2c6fed29807ceba8bf
Mike Laverick <mike.laverick@auckland.ac.nz>
2020-05-27 14:36:25 +1200

    Add project import to decorators

--- tardis/tardis_portal/auth/decorators.py
+++ tardis/tardis_portal/auth/decorators.py
@@ -36,7 +36,7 @@ from django.http import HttpResponseRedirect
 from django.db.models import Q
 from django.conf import settings
 
-from ..models import Experiment, Dataset, DataFile, GroupAdmin
+from ..models import Project, Experiment, Dataset, DataFile, GroupAdmin
 from ..shortcuts import return_response_error
 
 
