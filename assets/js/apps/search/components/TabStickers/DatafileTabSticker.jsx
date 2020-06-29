f7d43ab317b1b11d078a3e8f2fdbda758efc6f2d
warrickct <warrickct@gmail.com>
2020-06-30 11:31:33 +1200

    Renamed tabstickers component, changed sticker sizing to em from, split stickers into separate components.

---
+++ assets/js/apps/search/components/TabStickers/DatafileTabSticker.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import TabSticker from './TabSticker';
+export const DatafileTabSticker = () => {
+    return (<TabSticker initials="DF"></TabSticker>);
+};
+
+export default DatafileTabSticker;
--- assets/js/apps/search/components/TabStickers/TabSticker.jsx
+++ assets/js/apps/search/components/TabStickers/TabSticker.jsx
@@ -37,3 +37,5 @@ export default function TabSticker(props) {
         </div>
     )
 }
+
+
