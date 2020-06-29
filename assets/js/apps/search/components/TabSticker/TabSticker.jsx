0926904e8f3ed0fa1ca01e74b97a4ce7aa0dac7c
warrickct <warrickct@gmail.com>
2020-06-29 15:34:35 +1200

    background colour change based on initials

--- assets/js/apps/search/components/TabSticker/TabSticker.jsx
+++ assets/js/apps/search/components/TabSticker/TabSticker.jsx
@@ -3,8 +3,26 @@ import React from 'react';
 import './TabSticker.css'
 
 export default function TabSticker(props) {
+    let backgroundColor = "#fafafa";
+    switch (props.initials) {
+        case "P":
+            backgroundColor = "#f7db8d";
+            break;
+        case "E":
+            backgroundColor = "#abe2ff";
+            break;
+        case "DS":
+            backgroundColor = "#ffcce8";
+            break;
+        case "DF":
+            backgroundColor = "#70ffae";
+    }
+
     return (
-        <div className="tab-sticker">
+        <div 
+            className="tab-sticker"
+            style={{'background-color': backgroundColor}}
+        >
             <div className="content">
                 {props.initials}
             </div>
