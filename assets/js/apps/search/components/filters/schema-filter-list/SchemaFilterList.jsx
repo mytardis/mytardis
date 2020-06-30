9d09ffa61670c6843862c15eb2405373caf30bee
Noel <noel.zeng@auckland.ac.nz>
2020-06-30 16:05:03 +1200

    Change filter list to expect filters by ID

--- assets/js/apps/search/components/filters/schema-filter-list/SchemaFilterList.jsx
+++ assets/js/apps/search/components/filters/schema-filter-list/SchemaFilterList.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import PropTypes from "prop-types";
 import Accordion from 'react-bootstrap/Accordion';
+import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import TextFilter from "../text-filter/TextFilter";
 
-const FilterList = ({ filters, activeFilters,   }) => {
+const FilterList = ({ filters, activeFilters   }) => {
+    
+    /*
     const filtersComponents = filters.map((filter) => {
         switch (filter.data_type) {
             case "STRING":
@@ -16,29 +19,84 @@ const FilterList = ({ filters, activeFilters,   }) => {
             {}
         </div>
     )
+    */
 }
 
-const SchemaFilterList = ({ schemas }) => {
+const SchemaFilterList = ({ value, options, onValueChange }) => {
+    // Generate a list of schemas based on the objects
+    const schemasAsList = useMemo(() => (
+        Object.keys(options.schemas)
+              .map(id => options.schemas[id])
+    ),[options.schemas]);
+    const handleSchemaToggle = (schemaId,e) => {
+        if (value.includes(schemaId)) {
+            if (value.length == 1) {
+                // Prevent switching off all schemas.
+                return;
+            }
+            const newValue = value.filter(schema => schema !== schemaId);
+            onValueChange(newValue);
+        } else {
+            const newValue = value.concat(schemaId);
+            onValueChange(newValue);
+        }
+    }
 
+    const getCheckValue = (id) => {
+        return value.includes(id);
+    }
+    
     return (
-        <Accordion>
-            {schemas.map(({ schema_name, parameters }) => (
-                <Card key={schema_name}>
-                    <Accordion.Toggle as={Card.Header} eventKey={schema_name}>
-                        {schema_name}
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey={schema_name}>
-                        <Card.Body>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            ))}
-        </Accordion>
+        <div>
+            <Form.Group>
+                <Form.Label>Show me</Form.Label>
+                {
+                    
+                    schemasAsList.map(
+                        ({schema_name,id}) => (
+                            <Form.Check 
+                                key={id}
+                                id={"schemaCheck-"+id}
+                                type="checkbox" 
+                                label={schema_name} 
+                                checked={getCheckValue(id)} 
+                                onChange={handleSchemaToggle.bind(this,id)} 
+                            />
+                        )
+                    )
+                }
+            </Form.Group>
+            <Accordion>
+                {schemasAsList.map(({ id, schema_name, parameters }) => {
+                    console.log(value);
+                    if (!value.includes(id)) {
+                        // If schema is not selected, don't show filters for the schema.
+                        return null;
+                    }
+                    return (
+                        <Card key={schema_name}>
+                            <Accordion.Toggle as={Card.Header} eventKey={schema_name}>
+                                {/* <FilterList filters  */}
+                                {schema_name}
+                            </Accordion.Toggle>
+                            <Accordion.Collapse eventKey={schema_name}>
+                                <Card.Body>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    );
+                })}
+            </Accordion>
+        </div>
     );
 };
 
 SchemaFilterList.propTypes = {
-    schemas: PropTypes.array.isRequired
+    value: PropTypes.arrayOf(PropTypes.string).isRequired,
+    options: PropTypes.shape({
+        schemas: PropTypes.object
+    }),
+    onValueChange: PropTypes.func.isRequired
 };
 
 export default SchemaFilterList;
--- assets/js/apps/search/components/filters/schema-filter-list/SchemaFilterList.stories.jsx
+++ assets/js/apps/search/components/filters/schema-filter-list/SchemaFilterList.stories.jsx
@@ -5,35 +5,72 @@ import { action } from '@storybook/addon-actions';
 export default {
   component: SchemaFilterList,
   title: 'Schema filter list',
-  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+  decorators: [story => <div style={{ padding: '3rem', width:'400px' }}>{story()}</div>],
   excludeStories: /.*Data$/,
 };
 
 export const schemaData = {
-    schemas: [
-        {
-          "parameters": [
-            {
-              "data_type": "STRING",
-              "name":"param1",
-              "full_name": "Parameter 1"
-            },
-            {
-              "data_type": "STRING",
-              "name":"param2",
-              "full_name": "Parameter 2"
-            },
-            {
-              "data_type": "STRING",
-              "name":"sensitive_param",
-              "full_name": "Sensitive Parameter"
-            }
-          ],
-          "schema_name": "Schema_ACL_dataset"
+    value: [
+      "1",
+      "2"
+    ],
+    onValueChange: action('active schema changed'),
+    options: {
+      schemas: {
+        "1":{
+            "parameters": [
+              {
+                "data_type": "STRING",
+                "name":"param1",
+                "full_name": "Parameter 1"
+              },
+              {
+                "data_type": "STRING",
+                "name":"param2",
+                "full_name": "Parameter 2"
+              },
+              {
+                "data_type": "STRING",
+                "name":"sensitive_param",
+                "full_name": "Sensitive Parameter"
+              }
+            ],
+            "schema_name": "Raw data",
+            "id":"1"
+          },
+        "2":{
+            "parameters": [
+              {
+                "data_type": "STRING",
+                "name":"param1",
+                "full_name": "Another parameter 1"
+              },
+              {
+                "data_type": "STRING",
+                "name":"param2",
+                "full_name": "Another parameter 2"
+              },
+              {
+                "data_type": "STRING",
+                "name":"sensitive_param",
+                "full_name": "Sensitive Parameter"
+              }
+            ],
+            "schema_name": "Biopipeline Output",
+            "id":"2"
         }
-      ]
+      }
+    }
 }
+
+export const onlyOneSchemaSelectedData = Object.assign({},schemaData,{
+  value: ["1"]
+});
 
 export const Default = () => (
     <SchemaFilterList {...schemaData} />
-)
+);
+
+export const OnlyOneSelected = () => (
+  <SchemaFilterList {...onlyOneSchemaSelectedData} />
+);
