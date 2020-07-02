0c6d6c8481b96a2c94621b48a0af5124fb0e7f88
Noel <noel.zeng@auckland.ac.nz>
2020-07-02 16:36:15 +1200

    Map tab stickers to object types

--- assets/js/apps/search/components/TabStickers/DatafileTabSticker.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TabSticker from './TabSticker';
-export const DatafileTabSticker = () => {
-    return (<TabSticker initials="DF"></TabSticker>);
-};
-
-export default DatafileTabSticker;
