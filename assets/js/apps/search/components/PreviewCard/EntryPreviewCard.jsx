e514a9d91431d4579770fbeaeaf5b47c45a6fc59
warrickct <warrickct@gmail.com>
2020-07-15 10:48:13 +1200

    Initial files and skeleton for entry preview card.

---
+++ assets/js/apps/search/components/PreviewCard/EntryPreviewCard.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import './EntryPreviewCard.css'
+
+export default function EntryPreviewCard(props) {
+    return (
+        <h1>Entry preview card</h1>
+    )
+}
---
+++ assets/js/apps/search/components/PreviewCard/EntryPreviewCard.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions';
+import { EntryPreviewCard } from './EntryPreviewCard';
+
+export default {
+  component: EntryPreviewCard,
+  title: 'EntryPreviewCard',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+  excludeStories: /.*Data$/,
+};
+
+export const Default = () => (
+  <EntryPreviewCard />
+);
