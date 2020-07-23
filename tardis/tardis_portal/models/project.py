ebd3dbe97eea9ff39263d9b3ae37e4e346687727
Mike Laverick <mike.laverick@auckland.ac.nz>
2020-07-23 15:17:17 +1200

    Adjust 'empty' value detection

--- tardis/tardis_portal/models/project.py
+++ tardis/tardis_portal/models/project.py
@@ -105,7 +105,7 @@ class Project(models.Model):
                 param_dict = {}
                 type_idx = 0
                 for idx, value in enumerate(sublist[1:-1]):
-                    if value is not None:
+                    if value is not None or value != "":
                         param_dict['pn_id'] = str(PN_id)
                         if sublist[-1]:
                             param_dict['sensitive'] = True
