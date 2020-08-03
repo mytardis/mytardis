eb18d5c4fcb3a42d62c43494937803e55f8fdd86
Chris <c.seal@auckland.ac.nz>
2020-08-03 14:39:35 +1200

    Logging to see what is going on with GroupAdmin creation

--- tardis/tardis_portal/api.py
+++ tardis/tardis_portal/api.py
@@ -1191,9 +1191,11 @@ class ProjectResource(MyTardisModelResource):
                             user.groups.add(download_group)
                 bundle.data.pop('members')
         for group in project_groups:
+            logger.error(f'Creating group admin for {group}')
             group_admin, _ = GroupAdmin.objects.get_or_create(user=bundle.request.user.id,
                                                               group=group.id,
                                                               admin_groups=project_admin_groups)
+            logger.error(group_admin)
         return super().hydrate_m2m(bundle)
 
     def obj_create(self, bundle, **kwargs):
