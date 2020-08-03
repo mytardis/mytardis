f8b76ba293caecebc0ef07d48c045627a5732587
Chris <c.seal@auckland.ac.nz>
2020-08-03 14:26:05 +1200

    Typos in LDAP function

--- tardis/tardis_portal/api.py
+++ tardis/tardis_portal/api.py
@@ -140,12 +140,12 @@ def get_user_from_upi(upi):
                           search_filter,
                           attributes=['*'])
         if len(connection.entries) > 1:
-            error_message = f'More than one person with {search_action}: {value} has been found in the LDAP'
+            error_message = f'More than one person with {settings.LDAP_USER_LOGIN_ATTR}: {upi} has been found in the LDAP'
             if logger:
                 logger.error(error_message)
             raise Exception(error_message)
         elif len(connection.entries) == 0:
-            error_message = f'No one with {search_action}: {value} has been found in the LDAP'
+            error_message = f'No one with {settings.LDAP_USER_LOGIN_ATTR}: {upi} has been found in the LDAP'
             if logger:
                 logger.warning(error_message)
             return None
