3cb0cb202db11510391db8c968aa5756f8881f01
mike-ivs <mike.laverick@auckland.ac.nz>
2020-08-05 10:40:29 +1200

    Refactor "has_xxx_access" functions for speed

--- tardis/tardis_portal/auth/decorators.py
+++ tardis/tardis_portal/auth/decorators.py
@@ -196,57 +196,25 @@ def has_write(request, obj_id, ct_type):
 def has_download_access(request, obj_id, ct_type):
     if ct_type == 'project':
         obj = Project.objects.get(id=obj_id)
-        if obj.locked:
-            return False
     if ct_type == 'experiment':
         obj = Experiment.objects.get(id=obj_id)
-        if obj.locked:
-            return False
     if ct_type == 'dataset':
         obj = Dataset.objects.get(id=obj_id)
-        if obj.immutable:
-            return False
     if ct_type == 'datafile':
         obj = DataFile.objects.get(id=obj_id)
     return request.user.has_perm('tardis_acls.download_'+ct_type, obj)
 
-"""
-def has_download_access(request, obj_id, ct_type):
-    if ct_type == 'project':
-        return Project.safe.owned_and_shared(request.user, downloadable=True
-                                             ).filter(id=obj_id).exists()
-    if ct_type == 'experiment': # Retain public functionality for now
-        if Experiment.safe.owned_and_shared(request.user, downloadable=True
-                                                ).filter(id=obj_id).exists():
-            return True
-        else:
-            exp = Experiment.objects.get(id=obj_id)
-            return Experiment.public_access_implies_distribution(exp.public_access)
-    if ct_type == 'dataset':
-        return Dataset.safe.owned_and_shared(request.user, downloadable=True
-                                             ).filter(id=obj_id).exists()
-    if ct_type == 'datafile':
-        return DataFile.safe.owned_and_shared(request.user, downloadable=True
-                                              ).filter(id=obj_id).exists()
-"""
 
 def has_sensitive_access(request, obj_id, ct_type):
     if ct_type == 'project':
-        return Project.safe.owned_and_shared(request.user, viewsensitive=True
-                                             ).filter(id=obj_id).exists()
-    if ct_type == 'experiment': # Retain public functionality for now
-        if Experiment.safe.owned_and_shared(request.user, viewsensitive=True
-                                             ).filter(id=obj_id).exists():
-            return True
-        else:
-            exp = Experiment.objects.get(id=obj_id)
-            return Experiment.public_access_implies_distribution(exp.public_access)
+        obj = Project.objects.get(id=obj_id)
+    if ct_type == 'experiment':
+        obj = Experiment.objects.get(id=obj_id)
     if ct_type == 'dataset':
-        return Dataset.safe.owned_and_shared(request.user, viewsensitive=True
-                                             ).filter(id=obj_id).exists()
+        obj = Dataset.objects.get(id=obj_id)
     if ct_type == 'datafile':
-        return DataFile.safe.owned_and_shared(request.user, viewsensitive=True
-                                             ).filter(id=obj_id).exists()
+        obj = DataFile.objects.get(id=obj_id)
+    return request.user.has_perm('tardis_acls.sensitive_'+ct_type, obj)
 
 
 def has_read_or_owner_ACL(request, obj_id, ct_type):
