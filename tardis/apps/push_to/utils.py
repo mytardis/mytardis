c302d312c6f6cebc0fc8497ca27a53ef1bc18050
Dmitry Yakhnov <dmitry@yakhnov.info>
2021-03-16 14:29:48 +1100

    PushTo upgrade (#2910)
    * PushTo updates
    
    * Remove class
    
    * Close connections
    
    * Close connections
    
    * Remove disk space check, fix RW permissions check
    
    * Remove unused models
    
    * (fix) C0411 Pylint
    
    * Squashed commit of the following:
    
    commit 253d47462eb08b6a927d804e5cd01ee3607108c7
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 15:29:38 2021 +1100
    
        Update Django
    
    commit 9dde61100b4ed8d59ec697b9fc14759b331cf8a3
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 15:24:57 2021 +1100
    
        Update boto3 from 1.17.11 to 1.17.12 (#2869)
    
        Co-authored-by: pyup-bot <github-bot@pyup.io>
    
    commit 9c8f61a6c04dc97009610f0caec3a7dfd1490798
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 14:53:30 2021 +1100
    
        Update Pylint
    
    commit 2f27980f0a0d4e62a1a265909915f29cf4f9bfe7
    Author: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Date:   Mon Feb 22 14:48:58 2021 +1100
    
        Bump boto3 from 1.17.9 to 1.17.11 (#2866)
    
        Bumps [boto3](https://github.com/boto/boto3) from 1.17.9 to 1.17.11.
        - [Release notes](https://github.com/boto/boto3/releases)
        - [Changelog](https://github.com/boto/boto3/blob/develop/CHANGELOG.rst)
        - [Commits](https://github.com/boto/boto3/compare/1.17.9...1.17.11)
    
        Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
        Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Squashed commit of the following:
    
    commit 81d2118cae4d2b5f296ca0ca47fd0c5c0b09f9d0
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 17:48:35 2021 +1100
    
        Pyup update django 2.2.18 to 2.2.19 (#2870)
    
        * Update django from 2.2.18 to 2.2.19
    
        * (fix) use-a-generator
    
        * (fix) Use a generator instead
    
        * Fix
    
        * Fix
    
        * Fix
    
        * Fix Pylint
    
        * (fix) Use a generator instead
    
        Co-authored-by: pyup-bot <github-bot@pyup.io>
    
    commit ea9c4b6f49854a5249a41abc3779256eeea1aa6e
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 15:55:33 2021 +1100
    
        Update django from 2.2.18 to 2.2.19 (#2868)
    
        Co-authored-by: pyup-bot <github-bot@pyup.io>
    
    commit 253d47462eb08b6a927d804e5cd01ee3607108c7
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 15:29:38 2021 +1100
    
        Update Django
    
    commit 9dde61100b4ed8d59ec697b9fc14759b331cf8a3
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 15:24:57 2021 +1100
    
        Update boto3 from 1.17.11 to 1.17.12 (#2869)
    
        Co-authored-by: pyup-bot <github-bot@pyup.io>
    
    commit 9c8f61a6c04dc97009610f0caec3a7dfd1490798
    Author: Dmitry Yakhnov <dmitry@yakhnov.info>
    Date:   Mon Feb 22 14:53:30 2021 +1100
    
        Update Pylint
    
    commit 2f27980f0a0d4e62a1a265909915f29cf4f9bfe7
    Author: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Date:   Mon Feb 22 14:48:58 2021 +1100
    
        Bump boto3 from 1.17.9 to 1.17.11 (#2866)
    
        Bumps [boto3](https://github.com/boto/boto3) from 1.17.9 to 1.17.11.
        - [Release notes](https://github.com/boto/boto3/releases)
        - [Changelog](https://github.com/boto/boto3/blob/develop/CHANGELOG.rst)
        - [Commits](https://github.com/boto/boto3/compare/1.17.9...1.17.11)
    
        Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
        Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * (fix) NoneType is not iterable
    
    * Maintenance task
    
    * (fix) got an unexpected keyword argument 'priority'
    
    * (fix) can't compare offset-naive and offset-aware datetimes
    
    * Change import position
    
    * Handle expired auth
    
    * Handle expired auth
    
    * Handle expired auth
    
    (cherry picked from commit 9e6289ef372e4d56195273e91e58c5687a446cbd)

--- tardis/apps/push_to/utils.py
+++ tardis/apps/push_to/utils.py
@@ -1,41 +1,23 @@
-import random
+import uuid
+from io import BytesIO
 
 from paramiko import SFTPError
-
-from tardis.tardis_portal.models import Experiment, Dataset, DataFile
 
 
 def shell_escape(s):
     return "'" + s.replace("'", "'\\''") + "'"
 
 
-def bytes_available(ssh_client, path):
-    _, stdout, _ = ssh_client.exec_command(
-        'df -k %s | tail -n 1' % shell_escape(path))
-    cmd_output = stdout.read().split()
-
-    # df can output the size in a couple of ways; try to detect which
-    # and return the bytes available
+def is_directory(sftp, path):
+    sftp.chdir(".")
+    cwd = sftp.getcwd()
     try:
-        if len(cmd_output) == 5:
-            return int(cmd_output[2]) * 1024
-        assert len(cmd_output) == 6
-        return int(cmd_output[3]) * 1024
-    except ValueError:
-        pass
-    return "unknown"
-
-
-def is_directory(sftp_client, path):
-    sftp_client.chdir('.')
-    cwd = sftp_client.getcwd()
-    try:
-        sftp_client.chdir(path)
+        sftp.chdir(path)
         return True
     except (SFTPError, IOError):
         return False
     finally:
-        sftp_client.chdir(cwd)
+        sftp.chdir(cwd)
 
 
 def list_subdirectories(sftp_client, path, show_hidden=False):
@@ -47,35 +29,19 @@ def list_subdirectories(sftp_client, path, show_hidden=False):
 
 
 def get_default_push_location(sftp_client):
-    sftp_client.chdir('.')
+    sftp_client.chdir(".")
     return sftp_client.getcwd()
 
 
-def get_object_size(type, id):
-    for obj_type in [Experiment, Dataset, DataFile]:
-        if obj_type.__name__.lower() == type.lower():
-            return obj_type.objects.get(pk=id).get_size()
-    raise TypeError("Object of type %s does not exist" % type)
-
-
-def can_copy(ssh_client, object_type, object_id, path):
-    if not is_directory(ssh_client.open_sftp(), path):
+def can_copy(sftp, object_type, object_id, path):
+    if not is_directory(sftp, path):
         return False, "Directory does not exist."
 
-    # check if destination is writable by touching a file and deleting it
-    chan = ssh_client.get_transport().open_session()
-    test_file_name = "%032x" % random.getrandbits(128)
-    chan.exec_command(
-        "touch {destination}/{test_file_name} && "
-        "rm {destination}/{test_file_name}".format(
-            destination=path, test_file_name=test_file_name))
-    if chan.recv_exit_status() != 0:
-        return False, "Destination is not writable"
-
     try:
-        if bytes_available(ssh_client, path) > get_object_size(object_type,
-                                                               object_id):
-            return True, ''
-        return False, 'Insufficient disk space'
-    except (Experiment.DoesNotExist, Dataset.DoesNotExist, TypeError):
-        return False, ''
+        filename = "{}/.probe".format(path)
+        sftp.putfo(BytesIO(str(uuid.uuid4()).encode()), filename)
+        sftp.remove(filename)
+    except:
+        return False, "Destination is not writable."
+
+    return True, ""
