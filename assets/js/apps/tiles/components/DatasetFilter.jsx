ec282febe5ec145de9f9dd95c48c550ef98d464e
Dmitry Yakhnov <dmitry@yakhnov.info>
2021-07-17 18:29:40 +1000

    Fix eslint

--- assets/js/apps/tiles/components/DatasetFilter.jsx
+++ assets/js/apps/tiles/components/DatasetFilter.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const DatasetFilter = ({ onFilter, onSort }) => {
-  const dropDOwnStyle = {
+  const dropDownStyle = {
     minWidth: 'max-content',
     transform: 'translate3d(0px 38px 0px)',
   };
@@ -18,78 +18,70 @@ const DatasetFilter = ({ onFilter, onSort }) => {
   };
   return (
     <Fragment>
-    <div className="row mb-3">
-      <div className="col-md-8">
-        <div className="has-search">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Start typing to filter datasets based on description"
-            onKeyUp={onFilter}
-          />
+      <div className="row mb-3">
+        <div className="col-md-8">
+          <div className="has-search">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Start typing to filter datasets based on description"
+              onKeyUp={onFilter}
+            />
+          </div>
         </div>
-      </div>
-      <div className="col-md-4">
-        <div className="btn-group">
-          <button type="button" className="btn btn-outline-secondary float-end">Sort by</button>
-          <button
-            type="button"
-            className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Toggle Dropdown</span>
-          </button>
-          <div className="dropdown-menu" style={dropDOwnStyle} ref={(node) => { if (node) { node.style.setProperty('transform', 'translate3d(0px 38px 0px)', '!important'); } }}>
-            <div className="row">
-              <div className="col col-md-8">
-                <span className="dropdown-item" href="#">
-                  Description
-                </span>
+        <div className="col-md-4">
+          <div className="btn-group">
+            <button type="button" className="btn btn-outline-secondary float-end">Sort by</button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Toggle Dropdown</span>
+            </button>
+            <div className="dropdown-menu" style={dropDownStyle} ref={(node) => { if (node) { node.style.setProperty('transform', 'translate3d(0px 38px 0px)', '!important'); } }}>
+              <div className="row">
+                <div className="col col-md-8">
+                  <span className="dropdown-item" href="#">Description</span>
+                </div>
+                <div className="col col-md-4">
+                  <button className="fa fa-sort-alpha-asc me-2 mt-2" style={sortButtonStyle} type="button" onClick={() => onSort('description', 'asc')} />
+                  <button className="fa fa-sort-alpha-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('description', 'desc')} />
+                </div>
               </div>
-              <div className="col col-md-4">
-                <button className="fa fa-sort-alpha-asc me-2 mt-2" style={sortButtonStyle} type="button" onClick={() => onSort('description', 'asc')} />
-                <button className="fa fa-sort-alpha-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('description', 'desc')} />
+              <div className="row">
+                <div className="col col-md-8">
+                  <span className="dropdown-item" href="#">Files count</span>
+                </div>
+                <div className="col col-md-4">
+                  <button className="fa fa-sort-numeric-asc me-2 mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datafileCount', 'asc')} />
+                  <button className="fa fa-sort-numeric-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datafileCount', 'desc')} />
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col col-md-8">
-                <span className="dropdown-item" href="#">
-                  Files count
-                </span>
+              <div className="row">
+                <div className="col col-md-8">
+                  <span className="dropdown-item" href="#">Size</span>
+                </div>
+                <div className="col col-md-4">
+                  <button className="fa fa-sort-numeric-asc me-2 mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetSize', 'asc')} />
+                  <button className="fa fa-sort-numeric-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetSize', 'desc')} />
+                </div>
               </div>
-              <div className="col col-md-4">
-                <button className="fa fa-sort-numeric-asc me-2 mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datafileCount', 'asc')} />
-                <button className="fa fa-sort-numeric-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datafileCount', 'desc')} />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col col-md-8">
-                <span className="dropdown-item" href="#">
-                  Size
-                </span>
-              </div>
-              <div className="col col-md-4">
-                <button className="fa fa-sort-numeric-asc me-2 mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetSize', 'asc')} />
-                <button className="fa fa-sort-numeric-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetSize', 'desc')} />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col col-md-8">
-                <span className="dropdown-item" href="#">
-                  Last modified
-                </span>
-              </div>
-              <div className="col col-md-4">
-                <button className="fa fa-sort-numeric-asc me-2 mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetTime', 'asc')} />
-                <button className="fa fa-sort-numeric-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetTime', 'desc')} />
+              <div className="row">
+                <div className="col col-md-8">
+                  <span className="dropdown-item" href="#">Last modified</span>
+                </div>
+                <div className="col col-md-4">
+                  <button className="fa fa-sort-numeric-asc me-2 mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetTime', 'asc')} />
+                  <button className="fa fa-sort-numeric-desc mt-2" type="button" style={sortButtonStyle} onClick={() => onSort('datasetTime', 'desc')} />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </Fragment>
   );
 };
