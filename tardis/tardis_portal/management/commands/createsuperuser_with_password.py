1a7039b8c30ed22da4a6ef59ab5c5bf7b0b63e7d
Chris Seal <c.seal@auckland.ac.nz>
2021-07-21 15:17:12 +1200

    Tidy up for pylint to pass tests

--- tardis/tardis_portal/management/commands/createsuperuser_with_password.py
+++ tardis/tardis_portal/management/commands/createsuperuser_with_password.py
@@ -6,7 +6,7 @@ class Command(createsuperuser.Command):
     help = 'Create a superuser, and allow password to be provided'
 
     def add_arguments(self, parser):
-        super(Command, self).add_arguments(parser)
+        super().add_arguments(parser)
         parser.add_argument(
             '--password', dest='password', default=None,
             help='Specifies the password for the superuser.',
@@ -30,7 +30,7 @@ class Command(createsuperuser.Command):
                 self.stdout.write("User exists, exiting normally due to --preserve")
                 return
 
-        super(Command, self).handle(*args, **options)
+        super().handle(*args, **options)
 
         if password:
             user = self.UserModel._default_manager.db_manager(database).get(username=username)
