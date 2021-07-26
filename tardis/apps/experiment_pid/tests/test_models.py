1c147db4c3a03b7a4bc39c3fc43ed582ec6fa376
Chris Seal <c.seal@auckland.ac.nz>
2021-07-26 12:58:55 +1200

    Add import for IntegrityError

--- tardis/apps/experiment_pid/tests/test_models.py
+++ tardis/apps/experiment_pid/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 from tardis.tardis_portal.models.experiment import Experiment
 
 
