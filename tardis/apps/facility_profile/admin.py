f02ad11f4fb66b8c9d2b89173e68aed51e1dcdda
Chris Seal <c.seal@auckland.ac.nz>
2021-07-29 10:50:23 +1200

    Remove reference to Institution model in Facility Profile admin

--- tardis/apps/facility_profile/admin.py
+++ tardis/apps/facility_profile/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from tardis.apps.facility_profile.models import Institution, FacilityProfile
+from tardis.apps.facility_profile.models import FacilityProfile
 
 # Register PID models in admin site
-admin.site.register(Institution)
 admin.site.register(FacilityProfile)
