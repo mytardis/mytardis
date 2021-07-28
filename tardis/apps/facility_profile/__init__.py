5c97d7d19ebac8440dd09864419d4661f1c08fe6
Chris Seal <c.seal@auckland.ac.nz>
2021-07-29 10:40:04 +1200

    Fix relative imports in __init__.py

--- tardis/apps/facility_profile/__init__.py
+++ tardis/apps/facility_profile/__init__.py
@@ -1,4 +1,4 @@
 # pylint: disable=trailing-whitespace
 __author__ = "Chris Seal <c.seal@auckland.ac.nz"
 
-from models import *
+from .models import *
--- tardis/apps/institution/__init__.py
+++ tardis/apps/institution/__init__.py
@@ -1,4 +1,4 @@
 # pylint: disable=trailing-whitesapce
 __author__ = "Chris Seal <c.seal@auckland.ac.nz"
 
-from models import *
+from .models import *
