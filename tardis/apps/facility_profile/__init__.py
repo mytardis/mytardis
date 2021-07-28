f9240cb872d7a8d9a1d6effad8690fa68b54c37a
Chris Seal <c.seal@auckland.ac.nz>
2021-07-29 10:44:38 +1200

    more __init__.py issues

--- tardis/apps/facility_profile/__init__.py
+++ tardis/apps/facility_profile/__init__.py
@@ -1,4 +1,2 @@
 # pylint: disable=trailing-whitespace
 __author__ = "Chris Seal <c.seal@auckland.ac.nz"
-
-from .models import *
--- tardis/apps/institution/__init__.py
+++ tardis/apps/institution/__init__.py
@@ -1,4 +1,2 @@
 # pylint: disable=trailing-whitesapce
 __author__ = "Chris Seal <c.seal@auckland.ac.nz"
-
-from .models import *
