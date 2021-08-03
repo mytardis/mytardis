cc276df0a4ff601ad7cdec85d16724af84e282bb
Chris Seal <c.seal@auckland.ac.nz>
2021-08-03 15:12:03 +1200

    Initial commit of modified views

--- tardis/apps/dataset_pid/models.py
+++ tardis/apps/dataset_pid/models.py
@@ -22,9 +22,6 @@ class DatasetPID(models.Model):
     )
     pid = models.CharField(max_length=400, null=True, blank=True, unique=True)
 
-    # class Meta:
-    #    app_label = "tardis_apps"
-
     def __str__(self):
         return self.pid
 
