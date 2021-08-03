9b4a40892f29d224c4e41380df040c279746ecae
Chris Seal <c.seal@auckland.ac.nz>
2021-08-04 11:49:25 +1200

    Fixed Typo

--- tardis/apps/facilityprofile/admin.py
+++ tardis/apps/facilityprofile/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from tardis.apps.facility_profile.models import FacilityProfile
+from tardis.apps.facilityprofile.models import FacilityProfile
 
 # Register PID models in admin site
 admin.site.register(FacilityProfile)
