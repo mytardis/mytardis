2a45299f8e2bd4c114174b7622d8aaf7810b1ab4
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-10-15 11:44:56 +1300

    Fix experiment_recent_datasets to provide datasets directly to template

--- tardis/tardis_portal/views/ajax_pages.py
+++ tardis/tardis_portal/views/ajax_pages.py
@@ -108,7 +108,12 @@ def experiment_latest_dataset(request, experiment_id):
 @never_cache
 @authz.experiment_access_required
 def experiment_recent_datasets(request, experiment_id):
-    context = dict(exp=Experiment.objects.get(id=experiment_id))
+    if settings.ONLY_EXPERIMENT_ACLS:
+        context = dict(datasets=Dataset.objects.prefetch_related(
+                       "experiments").filter(experiments__id=experiment_id))
+    else:
+        context = dict(datasets=Dataset.safe.all(request.user).filter(
+                       experiments__id=experiment_id))
     return render_response_index(
         request,
         'tardis_portal/ajax/experiment_recent_datasets.html',
