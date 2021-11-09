8ed8357221685daaea5956d722c7f3ff751cc929
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-11-09 15:02:09 +1300

    Add in utility decorator to measure DB performance of functions

--- tardis/tardis_portal/util.py
+++ tardis/tardis_portal/util.py
@@ -1,11 +1,15 @@
+import logging
 import os
-
+import time
+import functools
 from urllib.parse import quote
 
+from django.conf import settings
+from django.db import connection, reset_queries
 import pytz
 
-from django.conf import settings
 
+logger = logging.getLogger(__name__)
 LOCAL_TZ = pytz.timezone(settings.TIME_ZONE)
 
 
@@ -103,3 +107,24 @@ def get_filesystem_safe_experiment_name(experiment):
         exp_title, safe=settings.SAFE_FILESYSTEM_CHARACTERS)
 
     return expt_filename
+
+
+def query_debugger(func):
+
+    @functools.wraps(func)
+    def inner_func(*args, **kwargs):
+
+        reset_queries()
+        start_queries = len(connection.queries)
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        end_queries = len(connection.queries)
+
+        logger.info(f"Function : {func.__name__}")
+        logger.info(f"Number of Queries : {end_queries - start_queries}")
+        logger.info(f"Finished in : {(end - start):.2f}s")
+
+        return result
+
+    return inner_func
