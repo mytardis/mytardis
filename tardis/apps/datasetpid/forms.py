cc9a23a3ff3a900065fade6ccd9a189e66a6ea59
Chris Seal <c.seal@auckland.ac.nz>
2021-11-10 14:31:03 +1300

    update dataset form to account for multiple identifers

--- tardis/apps/datasetpid/forms.py
+++ tardis/apps/datasetpid/forms.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from django import forms
 
@@ -10,3 +11,12 @@ logger = logging.getLogger(__name__)
 class DatasetPIDForm(DatasetForm):
 
     pid = forms.CharField(max_length=400, required=False)
+    alternative = forms.CharField(widget=forms.Textarea)
+
+    def clean_alternative(self):
+        rawdata = self.cleaned_data["alternative"]
+        cleaned_alternative = []
+        rawdata = re.split(",|;|\n", rawdata)
+        for identifier in rawdata:
+            cleaned_alternative.append(identifier.strip())
+        return cleaned_alternative
