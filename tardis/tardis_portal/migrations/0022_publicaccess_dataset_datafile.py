7e89372844dbf0320f40fa0870735143a55f0326
Mike Laverick <mike.laverick@auckland.ac.nz>
2021-11-17 10:56:14 +1300

    Migration file: change ExperimentACL.SYSTEM_OWNED to 2

--- tardis/tardis_portal/migrations/0022_publicaccess_dataset_datafile.py
+++ tardis/tardis_portal/migrations/0022_publicaccess_dataset_datafile.py
@@ -17,7 +17,7 @@ def create_public_user(apps, schema_editor):
     # PUBLIC_USER
     for exp in EXPERIMENT.objects.filter(public_access__gt=25).iterator():
         EXPERIMENTACL.objects.create(canRead = True,
-                                     aclOwnershipType = EXPERIMENTACL.SYSTEM_OWNED,
+                                     aclOwnershipType = 2,# = .SYSTEM_OWNED,
                                      experiment = exp,
                                      user = PUBLIC_USER)
 
