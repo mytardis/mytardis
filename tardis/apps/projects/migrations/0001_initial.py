92904acec8de7a1c1fa0573418da7c85c95fa082
Mike Laverick <mike.laverick@auckland.ac.nz>
2022-01-07 16:05:29 +1300

    Update Projects App initial migration to hold Schema Type changes

--- tardis/apps/projects/migrations/0001_initial.py
+++ tardis/apps/projects/migrations/0001_initial.py
@@ -19,6 +19,21 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name="schema",
+            name="type",
+            field=models.IntegerField(
+                choices=[
+                    (1, "Experiment schema"),
+                    (2, "Dataset schema"),
+                    (3, "Datafile schema"),
+                    (4, "None"),
+                    (5, "Instrument schema"),
+                    (6, "Project schema"),
+                ],
+                default=1,
+            ),
+        ),
         migrations.CreateModel(
             name="DefaultInstitutionProfile",
             fields=[
