cc1f980265719c9076141d7d901cd054049323bc
Mike Laverick <mike.laverick@auckland.ac.nz>
2022-01-25 14:44:47 +1300

    Add ajax to project urls

--- tardis/apps/projects/urls.py
+++ tardis/apps/projects/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 
-from .views import ProjectView, create_project, edit_project, my_projects
+from .views import (
+    ProjectView,
+    create_project,
+    edit_project,
+    my_projects,
+    retrieve_owned_proj_list,
+)
 
 
 # TODO point these to alterntive pages when they are developed
@@ -18,4 +24,9 @@ project_urls = [
     ),
     url(r"^create/$", create_project, name="tardis.apps.projects.create_project"),
     url(r"^myprojects/$", my_projects, name="tardis.apps.projects.views.my_projects"),
+    url(
+        r"^owned_proj_list/$",
+        retrieve_owned_proj_list,
+        name="tardis.apps.projects.retrieve_owned_proj_list",
+    ),
 ]
