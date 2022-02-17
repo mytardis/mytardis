1c7ce2a803c1a62c0f19846bac752a5a65f0d78d
Mike Laverick <mike.laverick@auckland.ac.nz>
2022-02-17 17:06:04 +1300

    Remove upload URLs if settings disable creation forms

--- tardis/apps/filepicker/urls.py
+++ tardis/apps/filepicker/urls.py
@@ -1,11 +1,21 @@
+from django.conf import settings
 from django.conf.urls import url
 
 from .views import upload_button
 from .views import fpupload
 
-urlpatterns = [
-    url(r'^upload_button/(?P<dataset_id>\d+)/$', upload_button,
-        name='tardis.apps.filepicker.views.upload_button'),
-    url(r'^fpupload/(?P<dataset_id>\d+)/$', fpupload,
-        name='tardis.apps.filepicker.views.fpupload'),
-]
+urlpatterns = []
+
+if not settings.DISABLE_CREATION_FORMS:
+    urlpatterns += [
+        url(
+            r"^upload_button/(?P<dataset_id>\d+)/$",
+            upload_button,
+            name="tardis.apps.filepicker.views.upload_button",
+        ),
+        url(
+            r"^fpupload/(?P<dataset_id>\d+)/$",
+            fpupload,
+            name="tardis.apps.filepicker.views.fpupload",
+        ),
+    ]
