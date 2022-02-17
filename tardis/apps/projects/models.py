64e12c6aab1f3a64ffc41f4c7628ae8f1a24e9ff
Mike Laverick <mike.laverick@auckland.ac.nz>
2022-02-17 14:50:23 +1300

    Remove unused import

--- tardis/apps/projects/models.py
+++ tardis/apps/projects/models.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models import Prefetch
 from django.db.models.signals import post_save
 from django.urls import reverse
 from django.utils.timezone import now as django_time_now
