2cc4e319d1bf3fe1cdd3a567b6c5e4eaeb9dea35
Chris Seal <c.seal@auckland.ac.nz>
2022-02-18 13:16:49 +1300

    Add OBJECTS_WITH_IDENTIFIERS field to test_settings.py

--- tardis/test_settings.py
+++ tardis/test_settings.py
@@ -1,81 +1,85 @@
 # pylint: disable=wildcard-import,unused-wildcard-import
 from __future__ import absolute_import
 
-from os import path
+import logging  # pylint: disable=wrong-import-order
 from glob import glob
+from os import path
 
-from celery import Celery  # pylint: disable=import-error
 from django.apps import apps  # pylint: disable=wrong-import-order
 
-from .default_settings import *  # noqa # pylint: disable=W0401,W0614
-import logging  # pylint: disable=wrong-import-order
+from celery import Celery  # pylint: disable=import-error
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+from .default_settings import *  # noqa # pylint: disable=W0401,W0614
+
+TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
 
 DEBUG = True
 
 DATABASES = {
-    'default': {
+    "default": {
         # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.sqlite3',
+        "ENGINE": "django.db.backends.sqlite3",
         # Name of the database to use. For SQLite, it's the full path.
-        'NAME': ':memory:',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
 # During testing it's always eager
 CELERY_ALWAYS_EAGER = True
-BROKER_URL = 'memory://'
+BROKER_URL = "memory://"
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
-tardis_app = Celery('tardis')
-tardis_app.config_from_object('django.conf:settings')
+tardis_app = Celery("tardis")
+tardis_app.config_from_object("django.conf:settings")
 tardis_app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
-TEMPLATES[0]['DIRS'].append(
-    path.join(path.dirname(__file__),
-    'tardis_portal/tests/rifcs').replace('\\', '/'))
-TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+TEMPLATES[0]["DIRS"].append(
+    path.join(path.dirname(__file__), "tardis_portal/tests/rifcs").replace("\\", "/")
+)
+TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
 
 del STATICFILES_STORAGE  # noqa
 
-DEFAULT_STORAGE_BASE_DIR = path.abspath(path.join(path.dirname(__file__),
-                                        '../var/store/')).replace('\\', '/')
+DEFAULT_STORAGE_BASE_DIR = path.abspath(
+    path.join(path.dirname(__file__), "../var/store/")
+).replace("\\", "/")
 
-AUTH_PROVIDERS = (('localdb', 'Local DB',
-                  'tardis.tardis_portal.auth.localdb_auth.DjangoAuthBackend'),
-                  ('ldap', 'LDAP',
-                   'tardis.tardis_portal.auth.ldap_auth.ldap_auth'))
+AUTH_PROVIDERS = (
+    ("localdb", "Local DB", "tardis.tardis_portal.auth.localdb_auth.DjangoAuthBackend"),
+    ("ldap", "LDAP", "tardis.tardis_portal.auth.ldap_auth.ldap_auth"),
+)
 
-#if (optional) ldap doesn't exist then don't enable ldap auth
+# if (optional) ldap doesn't exist then don't enable ldap auth
 try:
     import ldap  # noqa # pylint: disable=C0411,C0413,W0611
-    AUTH_PROVIDERS += (('ldap', 'LDAP',
-                        'tardis.tardis_portal.auth.ldap_auth.ldap_auth'),)
+
+    AUTH_PROVIDERS += (
+        ("ldap", "LDAP", "tardis.tardis_portal.auth.ldap_auth.ldap_auth"),
+    )
 except ImportError:
     pass
 
-NEW_USER_INITIAL_GROUPS = ['test-group']
+NEW_USER_INITIAL_GROUPS = ["test-group"]
 
 
 def get_all_tardis_apps():
-    base_dir = path.normpath(path.join(path.dirname(__file__), '..'))
-    tardis_app_dir = path.join(base_dir, *TARDIS_APP_ROOT.split('.'))
-    contents = glob(path.join(tardis_app_dir, '*'))
+    base_dir = path.normpath(path.join(path.dirname(__file__), ".."))
+    tardis_app_dir = path.join(base_dir, *TARDIS_APP_ROOT.split("."))
+    contents = glob(path.join(tardis_app_dir, "*"))
     apps = [
-        'tardis.apps.%s' % path.basename(item)
+        "tardis.apps.%s" % path.basename(item)
         for item in contents
-        if (path.isdir(item)
-            and path.basename(item) != '__pycache__')
+        if (path.isdir(item) and path.basename(item) != "__pycache__")
     ]
     return tuple(sorted(apps))
 
+
 INSTALLED_APPS += get_all_tardis_apps() + (
-    'django_nose',
-    'behave_django',
+    "django_nose",
+    "behave_django",
 )
 
 DEDUP_INSTALLED_APPS = []
@@ -99,42 +103,40 @@ LDAP_USER_ATTR_MAP = {"givenName": "first_name", "sn": "last_name", "mail": "ema
 LDAP_GROUP_ID_ATTR = "cn"
 LDAP_GROUP_ATTR_MAP = {"description": "display"}
 
-#LDAP_ADMIN_USER = ''
-#LDAP_ADMIN_PASSWORD = ''
-LDAP_BASE = 'dc=example, dc=com'
-LDAP_USER_BASE = 'ou=People, ' + LDAP_BASE
-LDAP_GROUP_BASE = 'ou=Group, ' + LDAP_BASE
+# LDAP_ADMIN_USER = ''
+# LDAP_ADMIN_PASSWORD = ''
+LDAP_BASE = "dc=example, dc=com"
+LDAP_USER_BASE = "ou=People, " + LDAP_BASE
+LDAP_GROUP_BASE = "ou=Group, " + LDAP_BASE
 
 SYSTEM_LOG_LEVEL = logging.DEBUG
 MODULE_LOG_LEVEL = logging.DEBUG
 
-SYSTEM_LOG_FILENAME = 'request-test.log'
-MODULE_LOG_FILENAME = 'tardis-test.log'
+SYSTEM_LOG_FILENAME = "request-test.log"
+MODULE_LOG_FILENAME = "tardis-test.log"
 
 # RIF-CS Settings
-OAI_DOCS_PATH = '/tmp/mytardis/rifcs'
-RIFCS_TEMPLATE_DIR = 'tardis/tardis_portal/tests/rifcs/'
-RIFCS_GROUP = 'MyTardis Test Group'
+OAI_DOCS_PATH = "/tmp/mytardis/rifcs"
+RIFCS_TEMPLATE_DIR = "tardis/tardis_portal/tests/rifcs/"
+RIFCS_GROUP = "MyTardis Test Group"
 RIFCS_KEY = "keydomain.test.example"
 
 # tardis.apps.sync
-MYTARDIS_SITES_URL = 'http://localhost:8080/mytardis-sites.xml/'
-MYTARDIS_SITE_URL = 'http://localhost:8080/'
-SYNC_MANAGER = 'managers.default_manager'
+MYTARDIS_SITES_URL = "http://localhost:8080/mytardis-sites.xml/"
+MYTARDIS_SITE_URL = "http://localhost:8080/"
+SYNC_MANAGER = "managers.default_manager"
 
-SYNC_CLIENT_KEYS = (
-    ('127.0.0.1', 'valid_client_key'),
-        )
+SYNC_CLIENT_KEYS = (("127.0.0.1", "valid_client_key"),)
 
-SYNC_CLIENT_KEY = 'valid_client_key'
+SYNC_CLIENT_KEY = "valid_client_key"
 
-SYNC_ADMINS = ('syncadmin@localhost', )
-SERVER_EMAIL = 'transfers@localhost'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SYNC_ADMINS = ("syncadmin@localhost",)
+SERVER_EMAIL = "transfers@localhost"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 REMOTE_SERVER_CREDENTIALS = [
     # Just one server for tests
-    ('http://localhost:4272/', 'username', 'password')
+    ("http://localhost:4272/", "username", "password")
 ]
 
 REQUIRE_DATAFILE_CHECKSUMS = True
@@ -142,21 +144,21 @@ REQUIRE_DATAFILE_SIZES = True
 REQUIRE_VALIDATION_ON_INGESTION = True
 
 ARCHIVE_FILE_MAPPERS = {
-    'deep-storage': (
-        'tardis.apps.deep_storage_download_mapper.mapper.deep_storage_mapper',
+    "deep-storage": (
+        "tardis.apps.deep_storage_download_mapper.mapper.deep_storage_mapper",
     ),
 }
 
 # Site's default archive organization (i.e. path structure)
-DEFAULT_ARCHIVE_ORGANIZATION = 'deep-storage'
+DEFAULT_ARCHIVE_ORGANIZATION = "deep-storage"
 
-DEFAULT_ARCHIVE_FORMATS = ['tar']
+DEFAULT_ARCHIVE_FORMATS = ["tar"]
 
 AUTOGENERATE_API_KEY = True
 
-SECRET_KEY = 'ij!%7-el^^rptw$b=iol%78okl10ee7zql-()z1r6e)gbxd3gl'
+SECRET_KEY = "ij!%7-el^^rptw$b=iol%78okl10ee7zql-()z1r6e)gbxd3gl"
 
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+SESSION_SERIALIZER = "django.contrib.sessions.serializers.PickleSerializer"
 USE_TZ = True  # apparently sqlite has issues with timezones?
 
 # Only for automated testing - don't use this in production:
@@ -175,12 +177,12 @@ SFTP_HOST_KEY = (
     "QBfGeFt1VGAy+YTLYLzTfnGnoFQcv7+2i9ZXnn/Gs9N8M+/lekdBFYgzoKN0y4pG\n"
     "2+Q+Tlr2aNlAmrHtkT13+wJAJVgZATPI5X3UO0Wdf24f/w9+OY+QxKGl86tTQXzE\n"
     "4bwvYtUGufMIHiNeWP66i6fYCucXCMYtx6Xgu2hpdZZpFw==\n"
-    "-----END RSA PRIVATE KEY-----\n")
+    "-----END RSA PRIVATE KEY-----\n"
+)
 
 # tardis.apps.s3utils will be in INSTALLED_APPS for unit tests:
 CALCULATE_CHECKSUMS_METHODS = {
-    'storages.backends.s3boto3.S3Boto3Storage':
-        'tardis.apps.s3utils.utils.calculate_checksums'
+    "storages.backends.s3boto3.S3Boto3Storage": "tardis.apps.s3utils.utils.calculate_checksums"
 }
 
 from tardis.default_settings.apps_default_settings import *  # noqa # pylint: disable=C0411,C0413
@@ -189,6 +191,13 @@ CHUNK_STORAGE = path.join(DEFAULT_STORAGE_BASE_DIR, "chunks")
 
 # For testing hsm app:
 RECALL_URI_TEMPLATES = {
-  'tardis.apps.hsm.storage.HsmFileSystemStorage':
-  '/api/v1/hsm_replica/{dfo_id}/recall/'
+    "tardis.apps.hsm.storage.HsmFileSystemStorage": "/api/v1/hsm_replica/{dfo_id}/recall/"
 }
+
+OBJECTS_WITH_IDENTIFIERS = [
+    "dataset",
+    "experiment",
+    "facility",
+    "instrument",
+    "project",
+]
