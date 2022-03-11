017ceeb225600b9a267d10499e2eacd459c9102c
Melvin Luong <>
2022-03-11 11:33:26 +1100

    Removed OpenID references from social_auth

--- tardis/apps/social_auth/auth/social_auth.py
+++ tardis/apps/social_auth/auth/social_auth.py
@@ -13,9 +13,7 @@ from celery import shared_task
 from tardis.tardis_portal.models import UserAuthentication
 from tardis.tardis_portal.auth.utils import configure_user
 
-from tardis.apps.openid_migration.models import OpenidUserMigration
 from tardis.apps.social_auth import default_settings as social_auth_default_settings
-from tardis.apps.openid_migration import default_settings as openid_migration_default_settings
 from tardis import default_settings
 
 logger = logging.getLogger(__name__)
@@ -95,19 +93,6 @@ def add_user_permissions(**kwargs):
     return kwargs
 
 
-def add_migration_permission(**kwargs):
-    """
-    Adds permission to migrate account for OpenID authenticated user
-    """
-    user = kwargs.get('user')
-    # Check if migration has been performed and add permission only if it is false
-    is_account_migrated = OpenidUserMigration.objects.filter(new_user=user)
-    if user and not is_account_migrated:
-        user.user_permissions.add(Permission.objects.get(codename='add_openidusermigration'))
-
-    return kwargs
-
-
 def approve_user_auth(**kwargs):
     """
     Sets approved status to True in user authentication
@@ -168,12 +153,6 @@ def send_account_approved_email(user_id, authMethod):
     """Sends user email once account is approved by admin"""
     user = User.objects.get(id=user_id)
     site_title = getattr(settings, 'SITE_TITLE', 'MyTardis')
-    # get instruction link from settings
-    account_migration_instructions_links = getattr(
-        settings, 'ACCOUNT_MIGRATION_INSTRUCTIONS_LINKS',
-        openid_migration_default_settings.ACCOUNT_MIGRATION_INSTRUCTIONS_LINKS)
-    # get authenticated user backend
-    account_migration_instructions_link = account_migration_instructions_links[authMethod]
 
     subject = '[%s] User account Approved' % site_title
     message = (
@@ -187,8 +166,7 @@ def send_account_approved_email(user_id, authMethod):
         "Thanks,\n"
         "%s Team\n"
         % (user.first_name, user.last_name, site_title, 'Google Account',
-           site_title, site_title, account_migration_instructions_link,
-           site_title))
+           site_title, site_title))
     try:
         from_email = getattr(settings, 'DEFAULT_FROM_EMAIL', default_settings.DEFAULT_FROM_EMAIL)
         user.email_user(
@@ -196,58 +174,6 @@ def send_account_approved_email(user_id, authMethod):
 
     except Exception as e:
         logger.error("There was an error sending mail: %s ", e)
-
-
-def migrate_user_message(**kwargs):
-    """
-    Automatically detects if a user has an account with the same email address
-    and prompts user to perform migration.
-    """
-    # We don't need to provide any message if openid_migration app is not enabled
-    if not is_openid_migration_enabled:
-        return kwargs
-    # Check if user accounts exist with the same email address
-    user = kwargs.get('user')
-    current_user_email = user.email
-    users = User.objects.filter(email=current_user_email)
-    if not users.count() > 1:
-        return kwargs
-    # Check if migration has been performed
-    is_account_migrated = OpenidUserMigration.objects.filter(new_user=user)
-    if is_account_migrated:
-        return kwargs
-    # check if account is not approved
-    backend = kwargs.get('backend')
-    authenticatedBackendName = type(backend).__name__
-    # get auth method from backend
-    authMethod = get_auth_method(authenticatedBackendName)
-    user_auth = UserAuthentication.objects.get(userProfile=user.userprofile,
-                       username=user.username,
-                       authenticationMethod=authMethod,)
-
-    if not user_auth or not user_auth.approved:
-        return kwargs
-
-    if not is_account_migrated:
-        # update message
-        request = kwargs.get('request')
-        messages.add_message(request, messages.WARNING,
-                             'We have found an existing account with your current email address. '
-                             'Please migrate data from your old account by clicking '
-                             '<a href="%s">Migrate My Account</a>'
-                             % reverse('tardis.apps.openid_migration.views.migrate_accounts'),
-                             extra_tags='safe')
-
-    return kwargs
-
-
-def is_openid_migration_enabled():
-    try:
-        if 'tardis.apps.openid_migration' in settings.INSTALLED_APPS:
-            return getattr(settings, 'OPENID_MIGRATION_ENABLED', True)
-    except AttributeError:
-        pass
-    return False
 
 
 def requires_admin_approval(authenticationBackend):
