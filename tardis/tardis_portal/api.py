34defbcf62ba553e113fdd36f5276c5b88909b3d
Mike Laverick <mikelaverick@btinternet.com>
2022-05-06 14:24:58 +1200

    Update api.py

--- tardis/tardis_portal/api.py
+++ tardis/tardis_portal/api.py
@@ -13,8 +13,7 @@ from itertools import chain
 
 from django.conf import settings
 from django.conf.urls import url
-from django.contrib.auth.models import AnonymousUser
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import AnonymousUser, User, Group
 from django.core.paginator import EmptyPage, InvalidPage, Paginator
 from django.db import IntegrityError, transaction
 from django.db.models import Q
