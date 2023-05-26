d131dec19f6d6b4b73550be3bdd1d978a643ecbf
Mike Laverick <mike.laverick@auckland.ac.nz>
2023-05-26 14:18:41 +1200

    Get PSs using query not model function

--- tardis/apps/projects/views.py
+++ tardis/apps/projects/views.py
@@ -92,7 +92,7 @@ class ProjectView(TemplateView):
         if request.user.is_authenticated:
             c["is_owner"] = authz.has_ownership(request, project.id, "project")
 
-        c["parametersets"]: project.getParameterSets().exclude(schema__hidden=True)
+        c["parametersets"]: project.projectparameterset_set.exclude(schema__hidden=True)
 
         # _add_protocols_and_organizations(request, project, c)
         return c
