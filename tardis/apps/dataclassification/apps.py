4929bb0e71beafa99c28c8deab1e9f7dccd1ceec
Chris Seal <c.seal@auckland.ac.nz>
2023-08-01 16:27:58 +1200

    Fix type in apps

--- tardis/apps/dataclassification/apps.py
+++ tardis/apps/dataclassification/apps.py
@@ -2,5 +2,5 @@ from tardis.app_config import AbstractTardisAppConfig
 
 
 class DataClassificationConfig(AbstractTardisAppConfig):
-    name = "tardis.apps.data_classification"
-    verbose_name = "data_classification"
+    name = "tardis.apps.dataclassification"
+    verbose_name = "dataclassification"
