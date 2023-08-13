19696f9e96088ce560c1fe3a7e10753c0eee8743
Chris Seal <c.seal@auckland.ac.nz>
2023-08-14 10:11:08 +1200

    Test

--- tardis/tardis_portal/api.py
+++ tardis/tardis_portal/api.py
@@ -974,12 +974,12 @@ class ExperimentResource(MyTardisModelResource):
             related_name="experiments",
             full=True,
         )
-    if "tardis.apps.dataclassification" in settings.INSTALLED_APPS:
+    """if "tardis.apps.dataclassification" in settings.INSTALLED_APPS:
         classification = fields.IntegerField(
             "tardis.apps.dataclassification.ExperimentDataClassification",
             "dataclassification_classification",
             null=True,
-        )
+        )"""
     # tags = fields.ListField()
 
     # Custom filter for identifiers module based on code example from
