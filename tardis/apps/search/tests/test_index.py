b8940c8d2e866f68beea69f2185f97780a2bac51
mike-ivs <mike.laverick@auckland.ac.nz>
2023-10-06 11:00:48 +1300

    quick test for ES detection in GH actions

--- tardis/apps/search/tests/test_index.py
+++ tardis/apps/search/tests/test_index.py
@@ -15,78 +15,84 @@ from tardis.apps.search.documents import (
 from tardis.tardis_portal.models import DataFile, Dataset, Experiment
 
 
-@override_settings(SINGLE_SEARCH_ENABLED=True)
-@modify_settings(INSTALLED_APPS={
-    'append': 'django_elasticsearch_dsl'
-})
-@override_settings(ELASTICSEARCH_DSL={
-        'default': {
-            'hosts': os.environ.get('ELASTICSEARCH_URL', None)
-        },
-})
-@unittest.skipUnless(
-        os.environ.get('ELASTICSEARCH_URL', None),
-        "--elasticsearch not set"
-    )
-class IndexExperimentTestCase(TestCase):
+from django_elasticsearch_dsl.test import is_es_online
 
+
+# @override_settings(SINGLE_SEARCH_ENABLED=True)
+# @modify_settings(INSTALLED_APPS={
+#    'append': 'django_elasticsearch_dsl'
+# })
+# @override_settings(ELASTICSEARCH_DSL={
+#        'default': {
+#            'hosts': os.environ.get('ELASTICSEARCH_URL', None)
+#        },
+# })
+# @unittest.skipUnless(
+#    os.environ.get("ELASTICSEARCH_URL", None), "--elasticsearch not set"
+# )
+class IndexExperimentTestCase(TestCase):
     def setUp(self):
-        user = 'tardis_user1'
-        pwd = 'secret'
-        email = ''
+        print("Elasticsearch is online?", is_es_online())
+
+        user = "tardis_user1"
+        pwd = "secret"
+        email = ""
         self.user = User.objects.create_user(user, email, pwd)
         self.out = StringIO()
-        call_command('search_index', stdout=self.out,
-                     action='delete', force=True)
+        call_command("search_index", stdout=self.out, action="delete", force=True)
 
     def test_create_index(self):
-        self.exp1 = Experiment(title='test exp1',
-                               institution_name='monash',
-                               description='Test Description',
-                               created_by=self.user)
-        self.exp2 = Experiment(title='test exp2',
-                               institution_name='monash',
-                               description='Test Description',
-                               created_by=self.user)
+        self.exp1 = Experiment(
+            title="test exp1",
+            institution_name="monash",
+            description="Test Description",
+            created_by=self.user,
+        )
+        self.exp2 = Experiment(
+            title="test exp2",
+            institution_name="monash",
+            description="Test Description",
+            created_by=self.user,
+        )
         self.exp1.save()
         self.exp2.save()
         # get search instance
         search = ExperimentDocument.search()
         # query for title(exact matching)
-        query = search.query("match", title='test exp1')
+        query = search.query("match", title="test exp1")
         result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].title, 'test exp1')
+        self.assertEqual(result.hits[0].title, "test exp1")
         # query for description
-        query = search.query("match", description='Test Description')
+        query = search.query("match", description="Test Description")
         result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].description, 'Test Description')
+        self.assertEqual(result.hits[0].description, "Test Description")
         # query for created_time
         query = search.query("match", created_time=self.exp1.created_time)
         result = query.execute(ignore_cache=True)
         self.assertEqual(result.hits[0].created_time, self.exp1.created_time)
         # dataset
         # dataset1 belongs to experiment1
-        self.dataset1 = Dataset(description='test_dataset')
+        self.dataset1 = Dataset(description="test_dataset")
         self.dataset1.save()
         self.dataset1.experiments.add(self.exp1)
         self.dataset1.save()
 
         # dataset2 belongs to experiment2
-        self.dataset2 = Dataset(description='test_dataset2')
+        self.dataset2 = Dataset(description="test_dataset2")
         self.dataset2.save()
         self.dataset2.experiments.add(self.exp2)
         self.dataset2.save()
         # search on dataset
         search = DatasetDocument.search()
-        query = search.query("match", description='test_dataset')
+        query = search.query("match", description="test_dataset")
         result = query.execute(ignore_cache=True)
         self.assertEqual(result.hits.total.value, 1)
         # search on datafile
         settings.REQUIRE_DATAFILE_SIZES = False
         settings.REQUIRE_DATAFILE_CHECKSUMS = False
-        self.datafile = DataFile(dataset=self.dataset1, filename='test.txt')
+        self.datafile = DataFile(dataset=self.dataset1, filename="test.txt")
         self.datafile.save()
         search = DataFileDocument.search()
-        query = search.query("match", filename='test.txt')
+        query = search.query("match", filename="test.txt")
         result = query.execute(ignore_cache=True)
         self.assertEqual(result.hits[0].filename, self.datafile.filename)
--- tardis/test_settings.py
+++ tardis/test_settings.py
@@ -80,6 +80,7 @@ def get_all_tardis_apps():
 INSTALLED_APPS += get_all_tardis_apps() + (
     #'django_nose',
     "behave_django",
+    "django_elasticsearch_dsl",
 )
 
 print(INSTALLED_APPS)
@@ -204,3 +205,15 @@ OBJECTS_WITH_IDENTIFIERS = [
     "instrument",
     "project",
 ]
+
+SINGLE_SEARCH_ENABLED = True
+ELASTICSEARCH_DSL = {
+    "default": {
+        "hosts": [
+            "127.0.0.1:9200",
+        ]
+    },
+}
+
+ELASTICSEARCH_DSL_AUTOSYNC = False
+ELASTICSEARCH_DSL_INDEX_SETTINGS = {"number_of_shards": 10}
