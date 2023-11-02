ce0b85a88c3342536aebde161276e7d39dc36c11
mike-ivs <mike.laverick@auckland.ac.nz>
2023-11-03 09:39:00 +1300

    rejig the search-test structure for better testing

--- tardis/apps/search/tests/test_index.py
+++ tardis/apps/search/tests/test_index.py
@@ -34,7 +34,7 @@ TODO improve these tests to include the following:
 
 @unittest.skipUnless(is_es_online(), "Elasticsearch is offline")
 @override_settings(ELASTICSEARCH_DSL_AUTOSYNC=True)
-class IndexExperimentTestCase(TestCase):
+class IndexTestCase(TestCase):
     def setUp(self):
         user = "tardis_user1"
         pwd = "secret"
@@ -57,119 +57,73 @@ class IndexExperimentTestCase(TestCase):
         self.out = StringIO()
         call_command("search_index", stdout=self.out, action="delete", force=True)
 
-    def test_create_index(self):
-        # Create project objects, triggering their indexing, then search for them
-        self.proj1 = Project(
-            name="Test Project 1",
+        # Create project object
+        self.proj = Project(
+            name="Test Project",
             description="This is a test.",
             principal_investigator=self.user,
             created_by=self.user,
         )
         self.proj1.save()
         # Explicit ACL creation for owner
-        acl = ProjectACL(
-            user=self.user,
-            project=self.proj1,
-            canRead=True,
-            canSensitive=True,
-            canWrite=True,
-            isOwner=True,
-            aclOwnershipType=ProjectACL.OWNER_OWNED,
-        )
-        acl.save()
-        self.proj2 = Project(
-            name="Test Project 2",
-            description="This is also a test.",
-            principal_investigator=self.user,
-            created_by=self.user,
-        )
-        self.proj2.save()
-        # Explicit ACL creation for owner
-        acl = ProjectACL(
-            user=self.user,
-            project=self.proj2,
-            canRead=True,
-            canSensitive=True,
-            canWrite=True,
-            isOwner=True,
-            aclOwnershipType=ProjectACL.OWNER_OWNED,
-        )
-        acl.save()
-        # Explicit ACL creation for group and token
-        acl = ProjectACL(
-            group=self.group,
-            project=self.proj2,
-            canRead=True,
-            aclOwnershipType=ProjectACL.OWNER_OWNED,
-        )
-        acl.save()
-        acl = ProjectACL(
-            token=self.token,
-            project=self.proj2,
-            canRead=True,
-            aclOwnershipType=ProjectACL.OWNER_OWNED,
-        )
-        acl.save()
-        search = ProjectDocument.search()
-        # query for title(exact matching)
-        query = search.query("match", name="Test Project 1")
-        result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].name, "Test Project 1")
-        # query for description
-        query = search.query("match", description="This is also a test.")
-        result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].description, "This is also a test.")
+        # acl = ProjectACL(
+        #    user=self.user,
+        #    project=self.proj1,
+        #    canRead=True,
+        #    canSensitive=True,
+        #    canWrite=True,
+        #    isOwner=True,
+        #    aclOwnershipType=ProjectACL.OWNER_OWNED,
+        # )
+        # acl.save()
 
+        # create experiment object
         self.exp1 = Experiment(
             title="test exp1",
             institution_name="monash",
             description="Test Description",
             created_by=self.user,
         )
-        self.exp2 = Experiment(
-            title="test exp2",
-            institution_name="monash",
-            description="Test Description",
-            created_by=self.user,
-        )
         self.exp1.save()
-        self.exp2.save()
-        # get search instance
-        search = ExperimentDocument.search()
-        # query for title(exact matching)
-        query = search.query("match", title="test exp1")
-        result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].title, "test exp1")
-        # query for description
-        query = search.query("match", description="Test Description")
-        result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].description, "Test Description")
-        # query for created_time
-        query = search.query("match", created_time=self.exp1.created_time)
-        result = query.execute(ignore_cache=True)
-        self.assertEqual(result.hits[0].created_time, self.exp1.created_time)
-        # dataset
+
+        # create dataset object
         # dataset1 belongs to experiment1
         self.dataset1 = Dataset(description="test_dataset")
         self.dataset1.save()
         self.dataset1.experiments.add(self.exp1)
         self.dataset1.save()
 
-        # dataset2 belongs to experiment2
-        self.dataset2 = Dataset(description="test_dataset2")
-        self.dataset2.save()
-        self.dataset2.experiments.add(self.exp2)
-        self.dataset2.save()
+        # create datafile object
+        settings.REQUIRE_DATAFILE_SIZES = False
+        settings.REQUIRE_DATAFILE_CHECKSUMS = False
+        self.datafile = DataFile(dataset=self.dataset1, filename="test.txt")
+        self.datafile.save()
+
+    def test_create_index(self):
+        """
+        Test the existance of document objects
+        """
+        # search on project
+        search = ProjectDocument.search()
+        # query for title(exact matching)
+        query = search.query("match", name="Test Project 1")
+        result = query.execute(ignore_cache=True)
+        self.assertEqual(result.hits[0].name, "Test Project 1")
+
+        # search on experiment
+        search = ExperimentDocument.search()
+        # query for title(exact matching)
+        query = search.query("match", title="test exp1")
+        result = query.execute(ignore_cache=True)
+        self.assertEqual(result.hits[0].title, "test exp1")
+
         # search on dataset
         search = DatasetDocument.search()
         query = search.query("match", description="test_dataset")
         result = query.execute(ignore_cache=True)
         self.assertEqual(result.hits.total.value, 1)
+
         # search on datafile
-        settings.REQUIRE_DATAFILE_SIZES = False
-        settings.REQUIRE_DATAFILE_CHECKSUMS = False
-        self.datafile = DataFile(dataset=self.dataset1, filename="test.txt")
-        self.datafile.save()
         search = DataFileDocument.search()
         query = search.query("match", filename="test.txt")
         result = query.execute(ignore_cache=True)
