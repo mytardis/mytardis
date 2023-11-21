66d0fd3f23484b7323ead9c0fe4fb6f16605a587
mike-ivs <mike.laverick@auckland.ac.nz>
2023-11-21 16:51:13 +1300

    test server address again

--- tardis/test_settings_microacls.py
+++ tardis/test_settings_microacls.py
@@ -209,7 +209,7 @@ SINGLE_SEARCH_ENABLED = True
 ELASTICSEARCH_DSL = {
     "default": {
         "hosts": [
-            "https://es1:9200",
+            "https://127.0.0.1:9200",
         ]
     },
 }
