21138015c88070c2c4515f01d22edcb981774900
Chris Seal <c.seal@auckland.ac.nz>
2024-03-04 11:50:59 +1300

    More typing to fix up

--- tardis/tardis_portal/api.py
+++ tardis/tardis_portal/api.py
@@ -11,7 +11,7 @@ import json
 import logging
 import re
 from itertools import chain
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 from urllib.parse import quote
 from wsgiref.util import FileWrapper
 
