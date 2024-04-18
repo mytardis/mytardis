568098757f0e4678480b3bb5bcccb47d8e1fcfb2
MyTardis Dev Webserver 2 <mytardis-dev-2@auckland.ac.nz>
2024-04-18 14:38:13 +1200

    Update .gitignore and pick up app changes hidden by .gitignore

---
+++ tardis/apps/yaml_dump/views/idwyaml.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponse
+import logging
+from  ruamel.yaml import YAML
+from rest_framework import generics
+from rest_framework.authentication import RemoteUserAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from tardis.apps.projects.models import Project
+from tardis.tardis_portal.api_v2.serializers.project import ProjectSerializer
+logger = logging.getLogger('__name__')
+
+class IDWYAMLView(generics.ListAPIView):
+    #serializer_class = ProjectSerializer
+    #authentication_classes = [RemoteUserAuthentication]
+    permission_classes = [IsAuthenticated]
+    project_serialiser_class = ProjectSerializer
+    experiment_serialiser_class = ProjectSerializer
+
+    #def get_serializer_context(self):
+    #    context = super().get_serializer_context()
+    #    context["request"] = self.request
+    #    return context
+
+    #def get_queryset(self):
+    #    return Project.safe.all(user=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        logger.debug(request.user)
+        project_queryset = Project.safe.all(user=request.user)
+        #experiment_queryset = Project.safe.all(user=request.user)
+
+        project_serialiser = self.project_serialiser_class(project_queryset, context={'request': request}, many=True)
+        #experiment_serialiser = self.experiment_serialiser_class(experiment_queryset, context={'request': request}, many=True)
+
+        response_results = {
+            "projects": project_serialiser.data,
+            #"experiments": experiment_serialiser.data,
+        }
+
+        logger.debug(response_results)
+
+        #response = Response(response_results)
+
+        # Convert response data to YAML
+        #data = yaml.dump(response.data)
+
+        # Create a YAML file
+        response = HttpResponse(data, content_type="application/yaml")
+        response["Content-Disposition"] = 'attachment; filename="data.yaml"'
+        return response
+
+    #def wrangle_projects(self, projects):
+    #    for project in projects:
