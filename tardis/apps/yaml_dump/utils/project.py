082f23d3ca181496440c1051319183fa0ec94240
Chris Seal <c.seal@auckland.ac.nz>
2024-04-23 15:22:47 +1200

    Migration to IDW models

--- tardis/apps/yaml_dump/utils/project.py
+++ tardis/apps/yaml_dump/utils/project.py
@@ -2,27 +2,19 @@
 
 A set of functions to wrangle the data into an appropriate format for export via YAML
 """
-from dataclasses import dataclass, asdict
-from typing import Any, Dict, Optional, List
+
+from typing import Any, Dict
 
 from django.conf import settings
 
+from tardis.apps.yaml_dump.models.datastatus import DataStatus
+from tardis.apps.yaml_dump.models.project import Project
+from tardis.apps.yaml_dump.models.username import Username
 
-INGESTED = 5
-
-@dataclass
-class ProjectYAMLDataclass:
-    data_status: int
-    description: str
-    name: str
-    object_schema: str
-    principal_investigator: str
-    metadata: Optional[List[Dict[str, Any]]] = None
-    data_classification: Optional[int] = None
-    identifiers: Optional[List[str]] = None
+INGESTED = DataStatus.INGESTED
 
 
-def wrangle_project_into_IDW_YAML(project: Dict[str, Any]) -> ProjectYAMLDataclass:
+def wrangle_project_into_IDW_YAML(project: Dict[str, Any]) -> Project:
     """Flatten the structure data into the format suited to dump in a YAML
 
     Args:
@@ -31,24 +23,26 @@ def wrangle_project_into_IDW_YAML(project: Dict[str, Any]) -> ProjectYAMLDatacla
     Returns:
         ProjectYAMLDataclass: The flattened dictionary
     """
-    return_dc = ProjectYAMLDataclass(
-        data_status = INGESTED,  # INGESTED flag is set since we can get the data from MyTardis
-        description = project["description"],
-        name = project["name"],
-        object_schema = project["projectparameterset_set"][0]["schema"]["namespace"],
-        principal_investigator = f'{project["principal_investigator"]["username"]}',
+    return_dc = Project(
+        data_status=INGESTED,  # INGESTED flag is set since we can get the data from MyTardis
+        description=project["description"],
+        name=project["name"],
+        object_schema=project["projectparameterset_set"][0]["schema"]["namespace"],
+        principal_investigator=Username(
+            f'{project["principal_investigator"]["username"]}',
+        ),
     )
-    if project['projectparameterset_set'][0]['parameters']:
-        metadata = []
-        for parameter in project['projectparameterset_set'][0]['parameters']:
+    if project["projectparameterset_set"][0]["parameters"]:
+        metadata = {}
+        for parameter in project["projectparameterset_set"][0]["parameters"]:
             value = None
-            if parameter['numerical_value']:
-                value = parameter['numerical_value']
-            if parameter['string_value']:
-                value = parameter['string_value']
-            if parameter['datetime_value']:
-                value = parameter['datetime_value']
-            metadata.append({parameter['name']['name']:value})
+            if parameter["numerical_value"]:
+                value = parameter["numerical_value"]
+            if parameter["string_value"]:
+                value = parameter["string_value"]
+            if parameter["datetime_value"]:
+                value = parameter["datetime_value"]
+            metadata[parameter["name"]["name"]] = value
         return_dc.metadata = metadata
 
     if "tardis.apps.dataclassification" in settings.INSTALLED_APPS:
@@ -60,20 +54,5 @@ def wrangle_project_into_IDW_YAML(project: Dict[str, Any]) -> ProjectYAMLDatacla
         identifiers = [value["identifier"] for value in project["identifiers"]]
         return_dc.identifiers = identifiers
     return return_dc
-        
-def project_representer(dumper, data):
-    return dumper.represent_mapping("!Project", asdict(data))
 
-"""
-    !Project
-data_classification: null
-data_status: null
-description: ''
-groups: []
-identifiers:
-- helen-project
-metadata: null
-name: Helen's project
-object_schema: ''
-principal_investigator: !Username 'szen012'
-users: []"""
+
