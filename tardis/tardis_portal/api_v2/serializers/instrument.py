e30ca1f727c977a0890bba7483d73892674d763b
Chris Seal <c.seal@auckland.ac.nz>
2024-04-24 15:31:06 +1200

    Test UserACLs

--- tardis/tardis_portal/api_v2/serializers/instrument.py
+++ tardis/tardis_portal/api_v2/serializers/instrument.py
@@ -14,7 +14,7 @@ from django.conf import settings
 from rest_framework import serializers
 
 from tardis.apps.identifiers.models import InstrumentID
-from tardis.tardis_portal.api.serializers.facility import FacilitySerializer
+from tardis.tardis_portal.api_v2.serializers.facility import FacilitySerializer
 from tardis.tardis_portal.models.instrument import Instrument
 from tardis.tardis_portal.models.parameters import (
     InstrumentParameter,
