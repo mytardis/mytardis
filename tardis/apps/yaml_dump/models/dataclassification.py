cc0b2b0bb045e6a698769e339bed3a4b29c7403e
Chris Seal <c.seal@auckland.ac.nz>
2024-04-26 14:52:37 +1200

    Fix broken enum

--- tardis/apps/yaml_dump/models/dataclassification.py
+++ tardis/apps/yaml_dump/models/dataclassification.py
@@ -1,18 +1,5 @@
 from dataclasses import dataclass
-from enum import Enum
 from typing import Optional
-
-class DataClassification(Enum):
-    """An enumerator for data classification.
-    Gaps have been left deliberately in the enumeration to allow for intermediate
-    classifications of data that may arise. The larger the integer that the classification
-    resolves to, the less sensitive the data is.
-    """
-
-    RESTRICTED = 1
-    SENSITIVE = 25
-    INTERNAL = 100
-    PUBLIC = 100
 
 
 @dataclass
@@ -21,4 +8,4 @@ class IDataClassification:
     Common interface for MyTardis models with data classification labels.
     """
 
-    data_classification: Optional[DataClassification] = None
+    data_classification: Optional[int] = None
