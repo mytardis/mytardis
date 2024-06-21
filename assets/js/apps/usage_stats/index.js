016fc10a21772a5a01048ac40d13cf6e0555d3db
am-nash <amnash243@gmail.com>
2024-06-21 11:56:01 +1000

    Change the logic for counters for float values

--- assets/js/apps/usage_stats/index.js
+++ assets/js/apps/usage_stats/index.js
@@ -2,17 +2,22 @@ function runCounters() {
     const counters = document.querySelectorAll(".stat-counter");
 
     counters.forEach((counter) => {
-        console.log(counter);
         counter.innerHTML = 0;
 
         updateCounter = (counter) => {
-            const target = parseFloat(counter.getAttribute('data-target'));
-            const current = parseFloat(counter.getAttribute('data-current'));
+            const isInt = counter.hasAttribute('data-counter-is-int');
+            const target = parseFloat(counter.getAttribute('data-counter-target'));
+            const current = parseFloat(counter.getAttribute('data-counter-current'));
             const increment = target / 200;
-            
+
             if (current < target) {
-                counter.innerHTML = `${Math.ceil(current + increment).toLocaleString()}`;
-                counter.setAttribute('data-current', current + increment);
+                if (isInt) {
+                    counter.innerHTML = `${Math.floor(current + increment).toLocaleString()}`;
+                } else {
+                    counter.innerHTML = `${(Math.round((current + increment) * 100) / 100).toLocaleString()}`;
+                }
+
+                counter.setAttribute('data-counter-current', current + increment);
 
                 setTimeout(() => updateCounter(counter), 1)
             } else {
