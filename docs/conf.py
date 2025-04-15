5bb901ea3c880155491ed56460a437c2733504cd
simonyuau <xm.yuau@gmail.com>
2025-04-15 14:45:03 +1000

    Update conf.py
    added the 'edit it on github' link

--- docs/conf.py
+++ docs/conf.py
@@ -88,6 +88,13 @@ pygments_style = 'sphinx'
 # Sphinx theme
 html_theme = "sphinx_rtd_theme"
 
+html_context = {
+    "display_github": True, # Integrate GitHub 
+    "github_repo": "mytardis/mytardis", # Repo name
+    "github_version": "stable", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
+
 # API doc generation
 # ------------------
 
